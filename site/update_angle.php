c98f1f7dce453202a3a88d41ff1020b1e4a7b254
fm <github-fm@actilis.net>
2008-12-10 21:45:56 +0000

    Appel de update_angle en POST + controle de saisie update_angle (pilotmode)

--- site/update_angle.php
+++ site/update_angle.php
@@ -1,30 +1,25 @@
 <?php
-//cookie for expert/beginner mode
-/*
-$expertcookie=htmlentities($_GET['expertcookie']);
-if ($expertcookie == "yes")
-{
-  setcookie("expertcookie", 'yes', time()+3600*24*365); //expire in one year
-
-}
-else
-{
-  setcookie("expertcookie", 'no', time()+3600*24*365); //expire in one year
-}
-*/
-//must be after cookies creation
 include_once("header.inc");
 include_once("config.php");
 
+// Controle de saisie / soumission
+$pilotmodes=array("autopilot", "windangle", "orthodromic", "bestvmg", "bestspeed");
+if ( ! in_array($pilotmode, $pilotmodes) ) {
+      printf ("<H1>PILOTMPDE = $pilotmode IS UNSUPPORTED.");
+      printf ("<H2>Your IP address is : " . $_SERVER["REMOTE_ADDR"] . "</H2>\n");
+      printf ("<H2>It has been logged. Don't try this again.</H2>\n");
+      sleep (1);
+      exit;
+} 
 
-if ( strspn($pilotmode, "-+.0123456789") != strlen($pilotmode)  ||
+if (
      strspn($boatheading, "-+.0123456789") != strlen($boatheading)  ||
      strspn($pilotparameter, "-+.0123456789") != strlen($pilotparameter)  ) {
 
-      printf ("<h1>This is not a nice thing to try this.</h1>");
-      printf ("<h2>Your IP address is : " . $_SERVER["REMOTE_ADDR"] . "</h2>\n");
+      printf ("<H1>This is not a nice thing to try this.</H1>");
+      printf ("<H2>Your IP address is : " . $_SERVER["REMOTE_ADDR"] . "</H2>\n");
       sleep (1);
-      printf ("<h2>It has been logged. Don't try this again.</h2>\n");
+      printf ("<H2>It has been logged. Don't try this again.</H2>\n");
       exit;
 }
 
@@ -32,6 +27,7 @@ if ($idusers != 0 )
 {
 
   if ( $idusers ==  getLoginId() ) {
+
       if ( $boatheading >= 360 ) $boatheading -= 360;
       if ( $boatheading < 0 ) $boatheading += 360;
 
@@ -44,12 +40,12 @@ if ($idusers != 0 )
       $fullUsersObj->writeNewheading($pilotmode, $boatheading, $pilotparameter);
 
 
-      echo "<h1 align=\"center\">&nbsp;<br />&nbsp;<br />" . $strings[$lang]["angleupdated"] . "<br />&nbsp;<br />&nbsp;</h1>";
+      echo "<H1 align=center>&nbsp;<BR>&nbsp;<BR>" . $strings[$lang]["angleupdated"] . "<BR>&nbsp;<BR>&nbsp;</H1>";
   } else {
-      printf ("<h1>This is not a nice thing to try this.</h1>");
-      printf ("<h2>Your IP address is : " . $_SERVER["REMOTE_ADDR"] . "</h2>\n");
+      printf ("<H1>This is not a nice thing to try this.</H1>");
+      printf ("<H2>Your IP address is : " . $_SERVER["REMOTE_ADDR"] . "</H2>\n");
       sleep (1);
-      printf ("<h2>It has been logged. Don't try this again.</h2>\n");
+      printf ("<H2>It has been logged. Don't try this again.</H2>\n");
   }
  
 }
