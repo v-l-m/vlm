75903047b073f0cd4b843e70490f22b4eebf3f1f
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2008-12-12 16:30:05 +0000

    fonctions loxo, distance et cap entre deux points

--- trunk/lib/vlm-c/loxo.h
+++ trunk/lib/vlm-c/loxo.h
@@ -1,5 +1,5 @@
 /**
- * $Id: loxo.h,v 1.5 2008/05/24 14:54:31 ylafon Exp $
+ * $Id: loxo.h,v 1.7 2008-12-12 16:27:06 ylafon Exp $
  *
  * (c) 2008 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -24,12 +24,19 @@
 #include "defs.h"
 #include "types.h"
 
-void move_boat_loxo           PARAM1(boat *);
-void estimate_boat_loxo       PARAM5(boat *, int, double, double *, double *);
-int  estimate_boat_loxo_coast PARAM5(boat *, int, double, double *, double *);
-void set_heading_loxo         PARAM1(boat *);
-void set_heading_direct       PARAM2(boat *, double);
-void set_heading_constant     PARAM1(boat *);
-void set_heading_wind_angle   PARAM1(boat *);
+void move_boat_loxo                 PARAM1(boat *);
+void estimate_boat_loxo             PARAM5(boat *, int, double, 
+					   double *, double *);
+int  estimate_boat_loxo_coast       PARAM5(boat *, int, double, 
+					   double *, double *);
+void set_heading_loxo               PARAM1(boat *);
+void set_heading_direct             PARAM2(boat *, double);
+void set_heading_constant           PARAM1(boat *);
+void set_heading_wind_angle         PARAM1(boat *);
+void get_loxo_coord_from_dist_angle PARAM6(double, double, double, double,
+					   double *, double *);
+
+void loxo_distance_angle            PARAM6(double, double, double, double,
+					   double *, double *);
 
 #endif /* _LOXO_H_ */
