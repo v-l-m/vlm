29ceee93150eba4ab364a2fd7bdc3970e4c1dc56
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2008-12-16 20:21:02 +0000

    regle un probleme de chemin pour le style (ref #16) validation (ref #18)

--- trunk/site/palmares.php
+++ trunk/site/palmares.php
@@ -1,21 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php 
+$lang=htmlentities(quote_smart($_REQUEST['lang']));
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<? echo $lang ?>">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="styles/new-style.css" />
-<link rel="shortcut icon" type="image/png" href="images/site/favicon.png" />
-<title>Virtual Loup-De-Mer </title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="style/new-style.css" />
+  <link rel="shortcut icon" type="image/png" href="images/site/favicon.png" />
+  <title>Virtual Loup-De-Mer </title>
+</head>
+<body>
 <?php
-//include_once("includes/header.inc");
 include_once("config.php");
 include_once("includes/strings.inc");
 
 $lang=htmlentities(quote_smart($_REQUEST['lang']));
 if ( htmlentities(quote_smart($_REQUEST['type'])) == 'palmares' ) {
-        $idusers=htmlentities(quote_smart($_REQUEST['idusers']));
-	echo "<H4>" ; printf ($strings[$lang]["palmares"],$idusers); echo "</H4>";
-	displayPalmares($idusers);
-	echo "<INPUT TYPE=BUTTON VALUE=\"Close\" ONCLICK=\"javascript:self.close();\">";
-}
-
-//include_once("includes/footer.inc");
-
+  $idusers=htmlentities(quote_smart($_REQUEST['idusers']));
+  echo "<h4>" ; printf ($strings[$lang]["palmares"],$idusers); echo "</h4>";
+  displayPalmares($idusers);
 ?>
+<form>
+  <input type="button" value="Close" onclick="javascript:self.close();" />
+</form>
+<?php
+}
+//include_once("includes/footer.inc");
+?>
+</body>
+</html>
