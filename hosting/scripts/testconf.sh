84d00eb155c7a75226a926008bf99ce6794022fe
paparazzia <paparazzia@gmail.com>
2008-12-24 08:51:20 +0000

    * ajout du script d'install * misc

--- hosting/scripts/testconf.sh
+++ hosting/scripts/testconf.sh
@@ -3,14 +3,13 @@
 
 #pour tester la configuration
 if test "$VLMRACINE" = ""; then
-    echo "ERREUR : Veuillez définir la variable d'environnement VLMRACINE"
-    echo "HINT : Par exemple dans votre .bashrc"
+    echo "!!! ERREUR : Veuillez définir la variable d'environnement VLMRACINE"
+    echo "!!! HINT : Par exemple dans votre .bashrc"
     exit 1
 fi
 
 source $VLMRACINE/conf/conf_script || { echo "ERREUR: votre fichier de configuration n'est pas disponible"; exit 1;}
 
-echo "Création des répertoires si nécessaire"
-
+echo "Création des répertoires $VLMJEUROOT et $VLMTEMP si nécessaire"
 mkdir -p $VLMJEUROOT
 mkdir -p $VLMTEMP
