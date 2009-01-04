5defe54db70ed6537976a9c1b373003787c02952
paparazzia <paparazzia@gmail.com>
2009-01-04 11:43:31 +0000

    refs #11 and #43 : script de création de l'user et de la bdd associée

--- base/scripts/dump.sh
+++ base/scripts/dump.sh
@@ -2,4 +2,4 @@
 
 source $VLMRACINE/conf/conf_base
 
-mysqldump -h $DBSERVER -u $DBUSER --password=$DBPASSWORD $DBNAME > vlmdump.sql
+mysqldump -h $DBSERVER -u $DBUSER --password=$DBPASSWORD --add-drop-table $DBNAME > vlmdump.sql
