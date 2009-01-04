6e0f194f5a3c873c79703226daba7bf6f31515c4
paparazzia <paparazzia@gmail.com>
2009-01-04 16:34:25 +0000

    refs #43 : * load de l'extension dans la ligne de commande du moteur (au cas ou le php.ini est différent pour php.cli) * nettoyage du cron du moteur qui présuppose VLMRACINE

--- moteur/cron-vlm.sh
+++ moteur/cron-vlm.sh
@@ -4,7 +4,7 @@ touch /tmp/cronvlm.$1.lock
 
 LOG=/tmp/$(date +%Y%m%d_%H:%M)-$1-cronvlm.log
 
-VLMRACINE=/home/commun
+#VLMRACINE=/base/de/vlm #A configurer normalement fans le crontab
 source $VLMRACINE/conf/conf_script || exit 1
 
 cd $VLMJEUROOT/moteur
@@ -14,6 +14,8 @@ date >> $LOG
 echo "************************************************************" >> $LOG
 nice -10 $VLMPHPPATH moteur.php $* >> $LOG 2>&1
 
+#maupiti & r16212.ovh.net sont les serveurs ou le moteur est susceptible d'envoyer un mail
+#FIXME : faire une option de conf ?
 [ $(hostname) != "maupiti.actilis" -a $(hostname) != "r16212.ovh.net" ] && grep CROSSED $LOG | sed 's/.*player //g' | sed 's/ CROSSED.*$//g' | while read idusers ; do
         sed -n "/user $idusers:/,/DONE/p"  $LOG >/tmp/CC-$idusers.log
         mail -s "COAST CROSSING : BOAT $idusers" vlm@virtual-winds.com -- -F "VLM-ENGINE" -f "vlm@virtual-winds.com" </tmp/CC-$idusers.log
