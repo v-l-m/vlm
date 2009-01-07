e5f97be6cc8cc88ccd25035cdc3735e80746034a
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-01-07 05:44:16 +0000

    * bugfix

--- trunk/grib/windserver.sh
+++ trunk/grib/windserver.sh
@@ -13,16 +13,19 @@ LATEST=latest.grb
 
 case "$1" in
   start)
-	windserver $GRIBPATH/$LATEST
-	exit 0
+    windserver $GRIBPATH/$LATEST
+    exit 0
+  ;;
   stop)
-	ipcrm -S 0x2cc6ccad
-	ipcrm -M 0x2cc6acb9
-	ipcrm -M 0x2cc69e42
-	exit 0
+    ipcrm -S 0x2cc6ccad
+    ipcrm -M 0x2cc6acb9
+    ipcrm -M 0x2cc69e42
+    exit 0
+  ;;
   *)
-	echo "Usage: $0 start|stop"
-	exit 1
+  echo "Usage: $0 start|stop"
+  exit 1
+  ;;
 esac
 
 exit 0
