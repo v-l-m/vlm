8a977da8a40789966d41742a95b7e7204c78bf37
paparazzia <paparazzia@gmail.com>
2009-01-18 16:00:01 +0000

    merge (last one) from trunk

--- moteur/cron-vlm.sh
+++ moteur/cron-vlm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-[ -f /tmp/cronvlm.$1.lock ] && exit
-touch /tmp/cronvlm.$1.lock
-
-LOG=/tmp/$(date +%Y%m%d_%H:%M)-$1-cronvlm.log
-
 #VLMRACINE=/base/de/vlm #A configurer normalement fans le crontab
 source $VLMRACINE/conf/conf_script || exit 1
+
+[ -f $VLMTEMP/cronvlm.$1.lock ] && exit
+touch $VLMTEMP/cronvlm.$1.lock
+
+LOG=$VLMTEMP/$(date +%Y%m%d_%H:%M)-$1-cronvlm.log
 
 cd $VLMJEUROOT/moteur
 echo -e "\n" >> $LOG
@@ -17,11 +17,11 @@ nice -10 $VLMPHPPATH moteur.php $* >> $LOG 2>&1
 #maupiti & r16212.ovh.net sont les serveurs ou le moteur est susceptible d'envoyer un mail
 #FIXME : faire une option de conf ?
 [ $(hostname) != "maupiti.actilis" -a $(hostname) != "r16212.ovh.net" ] && grep CROSSED $LOG | sed 's/.*player //g' | sed 's/ CROSSED.*$//g' | while read idusers ; do
-        sed -n "/user $idusers:/,/DONE/p"  $LOG >/tmp/CC-$idusers.log
-        mail -s "COAST CROSSING : BOAT $idusers" vlm@virtual-winds.com -- -F "VLM-ENGINE" -f "vlm@virtual-winds.com" </tmp/CC-$idusers.log
-        rm -f /tmp/CC-$idusers.log
+        sed -n "/user $idusers:/,/DONE/p"  $LOG >$VLMTEMP/CC-$idusers.log
+        mail -s "COAST CROSSING : BOAT $idusers" vlm@virtual-winds.com -- -F "VLM-ENGINE" -f "vlm@virtual-winds.com" <$VLMTEMP/CC-$idusers.log
+        rm -f $VLMTEMP/CC-$idusers.log
 done
 
-rm -f /tmp/cronvlm.$1.lock
+rm -f $VLMTEMP/cronvlm.$1.lock
 gzip -9 $LOG
 
