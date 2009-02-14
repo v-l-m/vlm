fea878b45f04b28b446c0a53e6c6d2cdee63e74e
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-02-14 12:55:37 +0000

    * lien depuis le classement vers les IC (refs #23)

--- trunk/site/ics.php
+++ trunk/site/ics.php
@@ -9,9 +9,9 @@ include_once("config.php");
 function outputIC($fullRacesObj) {
     foreach($fullRacesObj->races->ics as $ic) {
         if ($ic['flag'] & IC_FLAG_VISIBLE) {
-            echo "<div class=\"ic\">";
+            echo "<div class=\"ic\">\n";
             echo nl2br($ic['instructions']);
-            echo "</div>";
+            echo "\n</div>\n";
         }
     }
 
@@ -25,7 +25,7 @@ function outputRaceMap($fullRacesObj, $alttemplate) {
 
     $href = "images/racemaps/regate".$fullRacesObj->races->idraces.".jpg";
     if ( file_exists($href) ) {
-        echo "<img src=\"$ref\" alt=\"" .$alttemplate. "\" />";
+        echo "<img src=\"$href\" alt=\"" .$alttemplate. "\" />\n";
     }
 }
 
@@ -36,18 +36,19 @@ function outputRaceTitle($fullRacesObj, $titletemplate = "%s / %s") {
 if ($idraces != 0) {
 
     $fullRacesObj = new fullRaces($idraces);
-    echo "<div id=\"raceheader\">";
+    echo "<div id=\"raceheader\">\n";
         outputRaceTitle($fullRacesObj, $strings[$lang]["racestarted"]);
-    echo "</div>";
+    echo "</div>\n";
 
     // Carte de la course
-    echo "<div id=\"racemap\">";
+    echo "<div id=\"racemap\">\n";
         outputRaceMap($fullRacesObj, $strings[$lang]["racemap"]);
-    echo "</div>";
+    echo "</div>\n";
 
-    echo "<div id=\"ic\">";
+    echo "<div id=\"ic\">\n";
+        echo "<h3>".$strings[$lang]["ic"]."</h3>
         outputIC($fullRacesObj);
-    echo "</div>";    
+    echo "</div>\n";    
 }
 
 include_once("includes/footer.inc");
