79f511f68883be38bd4ce2a7bfa2a18d1555ef77
paparazzia <paparazzia@gmail.com>
2009-02-14 12:40:43 +0000

    * bugfix

--- site/ics.php
+++ site/ics.php
@@ -6,45 +6,47 @@ include_once("config.php");
 /*
  output des IC
  */
-function outputIC($fullRacesObj, $lang="fr") {
+function outputIC($fullRacesObj) {
     foreach($fullRacesObj->races->ics as $ic) {
         if ($ic['flag'] & IC_FLAG_VISIBLE) {
-            echo "<div class="ic">";
+            echo "<div class=\"ic\">";
             echo nl2br($ic['instructions']);
             echo "</div>";
         }
     }
 
+}
+
 /*
  output de la racemap
  */
 
-function outputRaceMap($fullRacesObj, $lang="fr") {
+function outputRaceMap($fullRacesObj, $alttemplate) {
 
     $href = "images/racemaps/regate".$fullRacesObj->races->idraces.".jpg";
     if ( file_exists($href) ) {
-        echo "<img src=\"$ref\" alt=\"" .$strings[$lang]["racemap"]. "\" />";
+        echo "<img src=\"$ref\" alt=\"" .$alttemplate. "\" />";
     }
 }
 
-function outputRaceTitle($fullRacesObj, $lang="fr") {
-    printf("<h3>".$strings[$lang]["racestarted"]."</h3>", $fullRacesObj->races->racename, gmdate("Y/m/d H:i:s", $fullRacesObj->races->deptime));     
+function outputRaceTitle($fullRacesObj, $titletemplate = "%s / %s") {
+    printf("<h3>".$titletemplate."</h3>", $fullRacesObj->races->racename, gmdate("Y/m/d H:i:s", $fullRacesObj->races->deptime));     
 }
 
 if ($idraces != 0) {
 
     $fullRacesObj = new fullRaces($idraces);
     echo "<div id=\"raceheader\">";
-        outputRaceTitle($fullRacesObj, $lang);
+        outputRaceTitle($fullRacesObj, $strings[$lang]["racestarted"]);
     echo "</div>";
 
     // Carte de la course
     echo "<div id=\"racemap\">";
-        outputRaceMap($fullRacesObj, $lang);
+        outputRaceMap($fullRacesObj, $strings[$lang]["racemap"]);
     echo "</div>";
 
     echo "<div id=\"ic\">";
-        outputIC($fullRacesObj, $lang);
+        outputIC($fullRacesObj);
     echo "</div>";    
 }
 
