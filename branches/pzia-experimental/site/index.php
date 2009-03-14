36c3551d930ea4f7cf3301f1e3253d3ffa44934e
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-03-14 21:26:21 +0000

    * test if we are connected before displying the abandon box

--- branches/pzia-experimental/site/index.php
+++ branches/pzia-experimental/site/index.php
@@ -18,8 +18,9 @@ if ( isLoggedIn() ) {
 echo "<h1 class=\"info\" align=\"center\">" . $strings[$lang]["one-one-one"] . "</h1>";
 echo "<hr />";
 
-include("abandon_race.php");
-
+if (isLoggedIn()) {
+    include("abandon_race.php");
+}
 
 include "includes/raceslist.inc";
 
