0170f55bbec9a0783ce99016ef98f2d85a93a21c
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-03-15 18:04:10 +0000

    * merge from pzia

--- trunk/site/faq.php
+++ trunk/site/faq.php
@@ -1,23 +1,20 @@
-<?
-include_once("includes/header.inc");
-include_once("config.php");
+<?php
+    include_once("includes/header.inc");
+    include_once("config.php");
 ?>
 
-<h1>
-<?echo $strings[$lang]["faqtitle"]?>
-</h1>
+    <h1><?php echo $strings[$lang]["faqtitle"]; ?></h1>
 
-<?for ($i =1; $i<20; $i++)
-{?>
-<h3>
-   <?echo $strings[$lang]["q".$i];?>
-</h3>
+<?php
+    for ($i =1; $i<20; $i++) {
+?>
 
-<p>
-   <?echo $strings[$lang]["a".$i];?>
-</p>
+        <h3><?php echo $strings[$lang]["q".$i]; ?></h3>
+        <p><?php echo $strings[$lang]["a".$i]; ?></p>
 
-<?
-   }
-include_once("includes/footer.inc");
+<?php
+    }
+
+    include_once("includes/footer.inc");
+
 ?>
