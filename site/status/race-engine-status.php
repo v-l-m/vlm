391e68243595415b20cf4a1851bcdea1685e73c9
paparazzia <paparazzia@gmail.com>
2009-04-12 17:32:41 +0000

    * refs #133

--- site/status/race-engine-status.php
+++ site/status/race-engine-status.php
@@ -28,6 +28,9 @@ header('Cache-Control: max-age=1');
        .hidden {
      display: none;
        }
+             td {
+                 text-align: center;
+                 }
          ]]>
     </style>
   </head>
@@ -42,6 +45,7 @@ header('Cache-Control: max-age=1');
     </p>
     </div>
     <div id="racesdetailstatus">
+    <h2>Race detailled status</h2>
     <ul>
 <?php
 
@@ -56,18 +60,45 @@ header('Cache-Control: max-age=1');
         $result2 = wrapper_mysql_db_query(DBNAME,$query2) or die("Query [$query2] failed \n");
         $row2 = mysql_fetch_assoc($result2);
         $delay = $current_time - (int)$row2['time'];
-        //echo $vacfreq."hhh".$delay;
         if ($delay > 60*(int)$vacfreq) {
             $cssklass = "maybelate";
         } else {
             $cssklass = "intime";
         }
-        echo "<li>($idraces) $racename - Last update : ".gmdate("Y-m-d \T H:i:s", $row2['time'])." - ";
-        echo "<span  class=\"$cssklass\">$delay seconds ago</span></li>\n";
+        echo "<li>\n";
+        echo "<span  class=\"$cssklass\">".sprintf("%03d", $delay)." seconds ago</span>\n";
+        echo " - LUP : ".gmdate("H:i:s", $row2['time'])." - (<b>$idraces</b>) $racename\n";
+        echo "</li>\n";
     } // Foreach race
 
 ?>
   </ul>
   </div>
+  <hr />
+  <div id="updatedetailstatus">
+    <h2>Last 10 update status</h2>
+    <table>
+    <tr><th>Time (GMT)</th><th>Duration (s)</th><th>Races</th><th>Boats</th><th>b/s</th></tr>
+    <?php
+      $query2 = "SELECT `time`,races,boats,duration,update_comment FROM updates ORDER BY `time` DESC LIMIT 10";
+      $result2 = wrapper_mysql_db_query(DBNAME,$query2) or die("Query [$query2] failed \n");
+      while($row2 = mysql_fetch_assoc($result2)) {
+          echo "<tr>\n";
+          $lastupdate = $row2['time'];
+          $races = $row2['races'];
+          $boats = $row2['boats'];
+          $duration = max($row2['duration'],1);
+          $update_comment = $row2['update_comment'];
+          printf("<td>%s</td>\n", gmdate('Y-m-d \T H:i:s', time()) );
+          printf("<td>%d</td>", $duration );
+          printf("<td title=\"%s\">%d</td>", $update_comment, $races );
+          printf("<td>%d</td>", $boats );
+          printf("<td>%.3f</td>", (float)$boats/$duration );          
+          echo "</tr>\n";
+      }     
+     ?>
+    </table>
+  </div>
+
   </body>
 </html>
