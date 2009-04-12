0536c2487b0ef856b0b012609dbe1de6468a1766
paparazzia <paparazzia@gmail.com>
2009-04-12 19:21:03 +0000

    refs #133 : ajout menu entre les status

--- site/status/windtable-status.php
+++ site/status/windtable-status.php
@@ -16,40 +16,15 @@
 #
 # Contact: <yves@raubacapeu.net>
 
-include("vlmc.php");
-
-$global_vlmc_context = new vlmc_context();
-global_vlmc_context_set($global_vlmc_context);
-
-$current_time = time();
-header('Content-Type: application/xhtml+xml; charset=UTF-8');
-header('Cache-Control: max-age=1');
+  include("vlmc.php");
+  
+  $global_vlmc_context = new vlmc_context();
+  global_vlmc_context_set($global_vlmc_context);
+  
+  $current_time = time();
+  $PAGETITLE="Windtable status check";
+  include("../includes/header-status.inc");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title>VLM-C windtable status (GRIB using shared memory)</title>
-    <style type="text/css">
-      <![CDATA[
-             .after {
-                 background-color: #99FF99;
-                 }
-             .before {
-                 background-color: #FFFF99;
-                 }
-             li, li > p {
-                 margin-top: 0px;
-                 margin-bottom: 0px;
-             }
-       .hidden {
-     display: none;
-       }
-         ]]>
-    </style>
-  </head>
-  <body>
-    <h1>Windtable status check</h1>
     <p id="currenttimeblurb">
       Current time: <span id="currenttime">
       <?php echo gmdate("Y-m-d \T H:i:s", $current_time) ?> GMT
