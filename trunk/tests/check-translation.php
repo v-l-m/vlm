35698c43fcf413f5d5e9d246b5b37bbd8f9370a9
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-04-16 20:14:21 +0000

    * refs #32 : translations' checking tools update

--- trunk/tests/check-translation.php
+++ trunk/tests/check-translation.php
@@ -17,11 +17,16 @@ $false_errors = Array(
       'faq', 'tchat', 'skipper', 'nautics', 'knots', 'days', 'mapsize', 'mercator', 'tracks', 'degrees', 'a12', 'dateClassificationFormat',
       'loch',
       ),
+    'en' => Array(
+      'images', 'faq', 'doc', 'forum', 'tchat', 'taverne', 'a12', 'contact', 'version', 'position', 'loch', 'estime', 'mapsize', 'proj', 'mercator', 'lambert', 'degrees',
+      ),
     );
 
 $langs= array_keys($strings);
 
 foreach ($langs as $lg) {
+    $missing = Array();
+    $untranslated = Array();
     if ($lg == "en") {
         $base = "fr"; //swap for en & fr, main languages of VLM.
     } else {
@@ -36,9 +41,17 @@ foreach ($langs as $lg) {
     foreach ($enkeys as $k) {
         if (!array_key_exists($k, $strings[$lg])) {
             echo "$lg : the KEY '$k' is MISSING : What is the translation of \"".$strings[$base][$k]."\" in $lg ?\n";
+            $missing[] = $k;
         } else if ($strings[$base][$k] == $strings[$lg][$k] and !in_array($k, $false_errors[$lg])) {
             echo "$lg : the KEY '$k' is UNTRANSLATED ($base==$lg): Is \"".$strings[$base][$k]."\" the same in $lg ?\n";
+            $untranslated[] = $k;
         }
     }
+    echo "\n---Summary :";
+    echo "\nMissing : '";
+    echo implode("', '", $missing);
+    echo "'\nUntranslated : '";
+    echo implode("', '", $untranslated);
+    echo "'\n\n";
 }
     
