114c27f57332fb415245649eaf9ebfb2ffbad602
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-05-08 12:35:07 +0000

    sync

--- branches/spf-experimental/maps/gshhs/init-gshhs.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-gshhsfile=gshhs_1.10.zip
-urlgshhs=http://www.ngdc.noaa.gov/mgg/shorelines/data/gshhs/version1.10/$gshhsfile
-
-echo "Ready to download gshhs files and install here"
-
-wget $urlgshhs
-unzip -j $gshhsfile -d ./
