2f27264d48437c999d6f1a0b134fec5baa908e0a
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-05-20 16:27:39 +0000

    new wind function (ported from spf-experimental) (refs #148)

--- trunk/lib/vlm-c/winds.c
+++ trunk/lib/vlm-c/winds.c
@@ -1,5 +1,5 @@
 /**
- * $Id: winds.c,v 1.23 2009-01-29 23:42:23 ylafon Exp $
+ * $Id: winds.c,v 1.24 2009-05-12 22:10:47 ylafon Exp $
  *
  * (c) 2008 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -478,6 +478,275 @@ wind_info *get_wind_info_latlong_TWSA(double latitude, double longitude,
     _check_angle_interp(angle);
     v = vprev + (angle) * t_ratio;
     _positive_angle(v);
+  } else {
+    u = uprev;
+    v = vprev;
+  }
+#ifdef DEBUG
+  printf("Speed %.3f, angle %.3f\n", u, radToDeg(v));
+#endif /* DEBUG */
+  wind->speed = u;
+  wind->angle = v;
+  return wind;
+}
+
+/* same as above, but with interpolation using True Wind Speed and Angle 
+ In selective mode (trying to avoid diverging rotations) */
+wind_info *get_wind_info_latlong_selective_TWSA(double latitude, 
+						double longitude,
+						time_t vac_time, 
+						wind_info *wind) {
+  winds *prev, *next;
+  int i, t_long, t_lat;
+  double u0prev, u0next, v0prev, v0next;
+  double u1prev, u1next, v1prev, v1next;
+  double u2prev, u2next, v2prev, v2next;
+  double u3prev, u3next, v3prev, v3next;
+  double u01next, u23next, u01prev, u23prev;
+  double v01next, v23next, v01prev, v23prev;
+  double uprev, unext, vprev, vnext;
+  double u,v;
+  double d_long, d_lat, t_ratio, angle;
+  int rot_step1a, rot_step1b, rot_step2a, rot_step2b;
+#ifdef OLD_C_COMPILER
+  double t_speed;
+#else
+  double complex c, c01, c23;
+#endif /* OLD_C_COMPILER */
+  winds_prev *windtable;
+#ifdef DEBUG
+  char buff[64];
+#endif /* DEBUG */
+
+  windtable = &global_vlmc_context->windtable;
+  /* if the windtable is not there, return NULL */
+  if (windtable->wind == NULL) {
+    wind->speed = 0.0;
+    wind->angle = 0.0;
+    return NULL;
+  }
+
+  d_long = radToDeg(longitude); /* is there a +180 drift? see grib */
+  if (d_long < 0) {
+    d_long += 360;
+  } else if (d_long >= 360) {
+    d_long -= 360;
+  }
+  d_lat = radToDeg(latitude) + 90; /* is there a +90 drift? see grib*/
+    
+  prev = next = NULL;
+
+  /* correct the grib time, currently variable in VLM */
+  vac_time -= windtable->time_offset;
+
+  for (i=0; i< windtable->nb_prevs; i++) {
+    if (windtable->wind[i]->prevision_time > vac_time) {
+      if (i) {
+	next = windtable->wind[i];
+	prev = windtable->wind[i-1];
+      } else {
+	prev = windtable->wind[i];
+      }
+      break;
+    }
+  }
+  /* none found the two are the last ones */
+  if (!next && !prev) {
+    prev = windtable->wind[windtable->nb_prevs-1];
+  } 
+#ifdef GRIB_RESOLUTION_0_5
+  d_long = d_long*2.0;
+  d_lat = d_lat*2.0;
+#endif /* GRID_RESOLUTION_0_5 */
+  t_long = (int)floor(d_long);
+  t_lat = (int)floor(d_lat);
+
+  u0prev = prev->wind_u[t_long][t_lat];
+  v0prev = prev->wind_v[t_long][t_lat];
+  u1prev = prev->wind_u[t_long][t_lat+1];
+  v1prev = prev->wind_v[t_long][t_lat+1];
+  u2prev = prev->wind_u[(t_long+1)%WIND_GRID_LONG][t_lat];
+  v2prev = prev->wind_v[(t_long+1)%WIND_GRID_LONG][t_lat];
+  u3prev = prev->wind_u[(t_long+1)%WIND_GRID_LONG][t_lat+1];
+  v3prev = prev->wind_v[(t_long+1)%WIND_GRID_LONG][t_lat+1];    
+
+  _transform_u_v(u0prev, v0prev);
+  _transform_u_v(u1prev, v1prev);
+  _transform_u_v(u2prev, v2prev);
+  _transform_u_v(u3prev, v3prev);
+  
+#ifdef DEBUG
+  printf("u0prev: %.2f kts, %.2f deg\n", u0prev, radToDeg(v0prev));
+  printf("u1prev: %.2f kts, %.2f deg\n", u1prev, radToDeg(v1prev));
+  printf("u2prev: %.2f kts, %.2f deg\n", u2prev, radToDeg(v2prev));
+  printf("u3prev: %.2f kts, %.2f deg\n", u3prev, radToDeg(v3prev));
+#endif /* DEBUG */  
+  /* speed interpolation */
+  u01prev = u0prev + (u1prev - u0prev) * (d_lat - floor(d_lat));
+  u23prev = u2prev + (u3prev - u2prev) * (d_lat - floor(d_lat));
+
+  angle = (v1prev - v0prev);
+  _check_angle_interp(angle);
+  rot_step1a = (angle > 0.0);
+  v01prev = v0prev + (angle) * (d_lat - floor(d_lat));
+  _positive_angle(v01prev);
+  
+  angle =  (v3prev - v2prev);
+  _check_angle_interp(angle);
+  rot_step1b = (angle > 0.0);
+  v23prev = v2prev + (angle) * (d_lat - floor(d_lat));
+  _positive_angle(v23prev);
+
+#ifdef OLD_C_COMPILER
+# define _transform_back_u_v(a,b)		\
+  t_speed = a * sin(b);				\
+  a = a * cos(b);				\
+  b = t_speed;
+#else
+# define _transform_back_u_v(a,b,c)		\
+  c = a * cos(b) + _Complex_I * a * sin(b)
+#endif /* OLD_C_COMPILER */
+
+  if (rot_step1a == rot_step1b) {
+    uprev = u01prev + (u23prev - u01prev) * (d_long - floor(d_long));
+
+    angle = (v23prev - v01prev);
+    _check_angle_interp(angle);
+    rot_step2a = (angle > 0.0);
+    vprev = v01prev + (angle) * (d_long - floor(d_long));
+    _positive_angle(vprev);
+  } else {
+    rot_step2a = -1;
+    /* rotations are in contrary motion, let's use UV for this */
+#ifdef OLD_C_COMPILER
+    _transform_back_u_v(u01prev, v01prev);
+    _transform_back_u_v(u23prev, v23prev);
+    uprev = u01prev + (u23prev - u01prev) * (d_long - floor(d_long));
+    vprev = v01prev + (v23prev - v01prev) * (d_long - floor(d_long));
+    _transform_u_v(uprev, vprev);
+#else
+    _transform_back_u_v(u01prev, v01prev, c01);
+    _transform_back_u_v(u23prev, v23prev, c23);
+    c = c01 + (c23 - c01) * (d_long - floor(d_long));
+    uprev = cabs(c);
+    vprev = carg(c);
+#endif /* OLD_C_COMPILER */
+  }
+
+#ifdef DEBUG
+  printf("-> u01prev: %.2f kts, %.2f deg\n", u01prev, radToDeg(v01prev));
+  printf("-> u23prev: %.2f kts, %.2f deg\n", u23prev, radToDeg(v23prev));
+  printf("=>   uprev: %.2f kts, %.2f deg\n", uprev, radToDeg(vprev));
+#endif /* DEBUG */  
+
+  if (next) {
+    u0next = next->wind_u[t_long][t_lat];
+    v0next = next->wind_v[t_long][t_lat];      
+    u1next = next->wind_u[t_long][t_lat+1];
+    v1next = next->wind_v[t_long][t_lat+1];
+    u2next = next->wind_u[(t_long+1)%WIND_GRID_LONG][t_lat];
+    v2next = next->wind_v[(t_long+1)%WIND_GRID_LONG][t_lat];      
+    u3next = next->wind_u[(t_long+1)%WIND_GRID_LONG][t_lat+1];
+    v3next = next->wind_v[(t_long+1)%WIND_GRID_LONG][t_lat+1];
+      
+    _transform_u_v(u0next, v0next);
+    _transform_u_v(u1next, v1next);
+    _transform_u_v(u2next, v2next);
+    _transform_u_v(u3next, v3next);
+
+#ifdef DEBUG
+    printf("u0next: %.2f kts, %.2f deg\n", u0next, radToDeg(v0next));
+    printf("u1next: %.2f kts, %.2f deg\n", u1next, radToDeg(v1next));
+    printf("u2next: %.2f kts, %.2f deg\n", u2next, radToDeg(v2next));
+    printf("u3next: %.2f kts, %.2f deg\n", u3next, radToDeg(v3next));
+#endif /* DEBUG */  
+
+    /* simple bilinear interpolation, we might factor the cos(lat) in
+       the computation to tackle the shape of the pseudo square */
+      
+    u01next = u0next + (u1next - u0next) * (d_lat - floor(d_lat));
+    u23next = u2next + (u3next - u2next) * (d_lat - floor(d_lat));
+
+    angle = (v1next - v0next);
+    _check_angle_interp(angle);
+    rot_step1a = (angle > 0.0);
+    v01next = v0next + (angle) * (d_lat - floor(d_lat));
+    _positive_angle(v01next);
+    
+    angle =  (v3next - v2next);
+    _check_angle_interp(angle);
+    rot_step1b = (angle > 0.0);
+    v23next = v2next + (angle) * (d_lat - floor(d_lat));
+    _positive_angle(v23next);
+
+    if (rot_step1a == rot_step1b) {
+      unext = u01next + (u23next - u01next) * (d_long - floor(d_long));
+      
+      angle = (v23next - v01next);
+      _check_angle_interp(angle);
+      rot_step2b = (angle > 0.0);
+      vnext = v01next + (angle) * (d_long - floor(d_long));
+      _positive_angle(vnext);
+    } else {
+    rot_step2b = -1;
+    /* rotations are in contrary motion, let's use UV for this */
+#ifdef OLD_C_COMPILER
+    _transform_back_u_v(u01next, v01next);
+    _transform_back_u_v(u23next, v23next);
+    unext = u01next + (u23next - u01next) * (d_long - floor(d_long));
+    vnext = v01next + (v23next - v01next) * (d_long - floor(d_long));
+    _transform_u_v(unext, vnext);
+#else
+    _transform_back_u_v(u01next, v01next, c01);
+    _transform_back_u_v(u23next, v23next, c23);
+    c = c01 + (c23 - c01) * (d_long - floor(d_long));
+    unext = cabs(c);
+    vnext = carg(c);
+#endif /* OLD_C_COMPILER */
+    }    
+#ifdef DEBUG
+    printf("-> u01next: %.2f kts, %.2f deg\n", u01next, radToDeg(v01next));
+    printf("-> u23next: %.2f kts, %.2f deg\n", u23next, radToDeg(v23next));
+    printf("=>   unext: %.2f kts, %.2f deg\n", unext, radToDeg(vnext));
+#endif /* DEBUG */  
+   
+#ifdef DEBUG
+    printf("vac_time %ld, prev prevision time %ld, next prevision time %ld\n", vac_time,
+	   prev->prevision_time, next->prevision_time);
+    ctime_r(&vac_time, buff);
+    printf("vac_time %s", buff);
+    ctime_r(&prev->prevision_time, buff);
+    printf(" prev_time %s", buff);
+    ctime_r(&next->prevision_time, buff);
+    printf(" next_time %s\n", buff);
+    printf("diff num %ld, diff_denom %ld\n", vac_time - prev->prevision_time,
+	   next->prevision_time - prev->prevision_time);
+#endif /* DEBUG */
+    t_ratio = ((double)(vac_time - prev->prevision_time)) / 
+      ((double)(next->prevision_time - prev->prevision_time));
+      
+    if ((rot_step2a == rot_step2b) || (rot_step2a < 0) ||
+	(rot_step2b < 0)) {
+      u = uprev + (unext - uprev) * t_ratio;
+      angle = (vnext - vprev);
+      _check_angle_interp(angle);
+      v = vprev + (angle) * t_ratio;
+      _positive_angle(v);
+    } else {
+#ifdef OLD_C_COMPILER
+      _transform_back_u_v(uprev, vprev);
+      _transform_back_u_v(unext, vnext);
+      u = uprev + (unext - uprev) * t_ratio;
+      v = vprev + (vnext - vprev) * t_ratio;
+      _transform_u_v(u, v);
+#else
+      _transform_back_u_v(uprev, vprev, c01);
+      _transform_back_u_v(unext, vnext, c23);
+      c = c01 + (c23 - c01) * t_ratio;
+      u = cabs(c);
+      v = carg(c);
+#endif /* OLD_C_COMPILER */
+    }
   } else {
     u = uprev;
     v = vprev;
