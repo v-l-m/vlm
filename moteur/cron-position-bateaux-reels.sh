aeaf6de7751bc5c5597b9b58868d848e6bf1cf36
fm <github-fm@actilis.net>
2009-05-20 20:15:31 +0000

    fake pour path de "php"

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Ce script doit etre executable.
+if [ -x /opt/php/bin/php ];
+   PHP=/opt/php/bin/php
+else 
+   PHP=php
+fi
 
 # Appel aux scripts de recuperation des positions des reels
 cd $VLMRACINE/vlmcode/lib/phpcommon
 
 # VOR
-php $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
+$PHP $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
 
 
