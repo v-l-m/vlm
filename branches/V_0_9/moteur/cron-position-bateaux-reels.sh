f033224d3ef26fd4d8d637b1a112e8bf41fa6872
fm <fm@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-08-26 07:16:14 +0000

    Position Pym et Ben

--- branches/V_0_9/moteur/cron-position-bateaux-reels.sh
+++ branches/V_0_9/moteur/cron-position-bateaux-reels.sh
@@ -11,7 +11,7 @@ fi
 # Appel aux scripts de recuperation des positions des reels
 cd $VLMRACINE/vlmcode/lib/phpcommon
 
-# VOR : finished
-# $PHP $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
+# Pym et Benoit
+$PHP $VLMRACINE/vlmcode/moteur/posscripts/oeposNyLo.php
 
 
