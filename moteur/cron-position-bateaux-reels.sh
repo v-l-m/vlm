41b4fa0b19862d01da06d73fe275815de88b19ba
fm <github-fm@actilis.net>
2009-09-14 21:24:53 +0000

    Merge from V_0_10

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -5,14 +5,14 @@ source $VLMRACINE/conf/conf_script || exit 1
 
 
 # Ce script doit etre executable.
-if [ -x /opt/php/bin/php ];
+if [ -x /opt/php/bin/php ]; then
     PHP=/opt/php/bin/php
 else
     PHP=php
 fi
 
 
-=======
+#=======
 # VOR : finished
 # $PHP $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
 
