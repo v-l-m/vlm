48710e3ada92f7d39b2223bf211abc31e775a8fe
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-09-27 08:13:04 +0000

    * merge too messy, better start again from clean copy

--- branches/pzia-experimental/moteur/cron-position-bateaux-reels.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Appel aux scripts de recuperation des positions des reels
-cd $VLMRACINE/vlmcode/lib/phpcommon
-
-# VOR
-php $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
-
-
--- branches/pzia-experimental/site/admin/adminfooter.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Now important call to phpMyEdit
-require_once('pme/extensions/phpMyEdit-mce-cal.class.php');
-new phpMyEdit_mce_cal($opts);
-?>
-
-</body>
-</html>
