85347e769ee9458e3468cf6d94615ce13199c1bb
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-09-27 19:34:07 +0000

    * basic layout

--- branches/pzia-experimental/site/admin/uploadracemap.php
+++ branches/pzia-experimental/site/admin/uploadracemap.php
@@ -1,30 +1,28 @@
 <?php
-
+    $PAGETITLE = "Racemap upload";
     include ("htmlstart.php");
     include_once ("functions.php");
     
+    $idnewrace = $_REQUEST['idnewrace'] ;   
+    
     if ($_REQUEST["action"] == "upload") {
-        echo "<h3>Image reçue</h3>";
-        echo "<br />";
-        print_r($_REQUEST);
-        echo "<br />";
-        print_r($_FILES);
+        echo "<h3>Image reçue pour la course $idnewrace.</h3>";
         //FIXME: tests here
         $img_blob = file_get_contents ($_FILES['fic']['tmp_name']);
         $req = "REPLACE INTO racesmap ( idraces, racemap ".
                   ") VALUES ( ".
-                  "".$_REQUEST['idnewrace']." , ".
+                  "".$idnewrace." , ".
                   "'".addslashes($img_blob)."') ";
         $ret = wrapper_mysql_db_query (DBNAME, $req) or die (mysql_error ());
-        
+        echo "<h3>OK</h3>";
     } else {
 ?>
         <h3>Envoi d'une image</h3>
         <form enctype="multipart/form-data" action="#" method="post">
             <input type="hidden" name="MAX_FILE_SIZE" value="2500000" />
             <input type="hidden" name="action" value="upload" />
-            <input type="text" name="idnewrace" size="12" />
-            <input type="file" name="fic" maxlength="250" size="50" />
+            Idraces:&nbsp;<input type="text" name="idnewrace" size="12" /><br />
+            Fichier jpeg:&nbsp;<input type="file" name="fic" maxlength="250" size="50" /><br />
             <input type="submit" value="Envoyer" />
         </form>
 <?php
