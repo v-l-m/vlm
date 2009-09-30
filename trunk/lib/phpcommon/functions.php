3c0dbd4e4d712be40e595328bdc412bc04448c87
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-09-30 21:48:01 +0000

    more instrumenting in trace mode

--- trunk/lib/phpcommon/functions.php
+++ trunk/lib/phpcommon/functions.php
@@ -13,7 +13,12 @@ include_once("vlmc.php");
 
 function wrapper_mysql_db_query($cmd) {
   if (defined('MOTEUR') && defined('TRACE_SQL_QUERIES')) {
-    echo "*** DB ACCESS ".$cmd."\n";
+    echo "*** DB ACCESS ".$cmd;
+    $sql_start_time=microtime(1);
+    $res = mysql_query($cmd);
+    $sql_end_time=microtime(1);
+    echo " : ".($sql_end_time-$sql_start_time)."s\n";
+    return $res;
   }
   return mysql_query($cmd);
 }
