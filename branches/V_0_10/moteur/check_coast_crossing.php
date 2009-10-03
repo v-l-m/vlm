440f2d62483db8b096f92ba45a671a5f2875333a
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-10-03 19:29:35 +0000

    merge form trunk, safer computing of boat position in case of coast crossing

--- branches/V_0_10/moteur/check_coast_crossing.php
+++ branches/V_0_10/moteur/check_coast_crossing.php
@@ -9,7 +9,8 @@ echo "\n\tChecking for coast crossing (vlm-c)\n ";
 echo "\tUsing: ".GSHHS_FILENAME."\n ";
 
 $crosses_the_coast = VLM_check_cross_coast($latAvant, $lonAvant, $latApres, $lonApres, 
-             $coast_xinglat, $coast_xinglong, $coast_xingratio);
+					   $coast_xinglat, $coast_xinglong, 
+					   $coast_xingratio);
 
 if ($crosses_the_coast) {
   echo "\t*** YES player " . $fullUsersObj->users->idusers . " CROSSED (vlmc), ";
@@ -17,13 +18,13 @@ if ($crosses_the_coast) {
   printf ("BOAT : %f,%f <----> %f,%f",
     $latAvant/1000,$lonAvant/1000 , $latApres/1000,$lonApres/1000);
   
-  $encounter_lat  = doublep_value($coast_xinglat)/1000;
-  $encounter_long = doublep_value($coast_xinglong)/1000;
+  $encounter_lat  = doublep_value($coast_xinglat);
+  $encounter_long = doublep_value($coast_xinglong);
 
   echo "\n\t\t\tEncounterCoordinates " . 
-    $encounter_lat . ", " . $encounter_long . 
+    $encounter_lat/1000 . ", " . $encounter_long/1000 . 
     "\n\nGoogleMap http://maps.google.fr/maps?f=q&hl=fr&geocode=&q=".
-    $encounter_lat . "," . $encounter_long .
+    $encounter_lat/1000 . "," . $encounter_long/1000 .
     "&ie=UTF8&spn=0.0191,0.082998&t=p&z=11&iwloc=addr. \n";
   
   /* NOTE the encounter coordinates are the real ones */
@@ -35,7 +36,7 @@ if ($crosses_the_coast) {
   echo "&list=" . $fullUsersObj->users->idusers ;
   echo "&x=1000&y=600&proj=mercator&text=right"; /* ahem, we have a point instead of the segment now :) */
   echo "&seg1=".$latAvant/1000 . "," . $lonAvant/1000 . ":" . $latApres/1000 . "," . $lonApres/1000;
-  echo "&seg2=".$encounter_lat . "," . $encounter_long . ":" . $encounter_lat . "," . $encounter_long;
+  echo "&seg2=".$encounter_lat/1000 . "," . $encounter_long/1000 . ":" . $encounter_lat/1000 . "," . $encounter_long/1000;
   echo "\n\n";
   /*
     echo "\n\t ==> Position Avant " . 
@@ -55,7 +56,23 @@ if ($crosses_the_coast) {
   // compute the real endpoint (using linear interpolation)
   // We can tune the 0.9 to whatever we want, to avoid putting the boat on the line
   // and have rounding errors having fun with us.
-  $latApres = $latAvant + ($latApres - $latAvant) * 0.9 * doublep_value($coast_xingratio);
-  $lonApres = $lonAvant + ($lonApres - $lonAvant) * 0.9 * doublep_value($coast_xingratio);
+  $latApres = $latAvant + ($encounter_lat - $latAvant) * 0.9;
+  $lonApres = $lonAvant + ($encounter_long - $lonAvant) * 0.9;
+
+  // ok we got a new point, check it's not crossing the coast because of roundings.
+  // new point coast check
+  for ( $coast_ratio = 8; $coast_ratio > 0; $coast_ratio--) {
+    $npcc = VLM_check_cross_coast($latAvant, $lonAvant, $latApres, $lonApres, 
+				  $coast_xinglat, $coast_xinglong, 
+				  $coast_xingratio);
+    if ($npcc) {
+      echo "*** Safety engaged for new point, moving to ".$coast_ratio."0%\n";
+      $latApres = $latAvant + ($encounter_lat - $latAvant) * ($coast_ratio / 10.);
+      $lonApres = $lonAvant + ($encounter_long - $lonAvant) * ($coast_ratio / 10.);
+    } else {
+      break;
+    }
+  }
+  // if things go wrong again, it is hopeless ;)
 }
 ?>
