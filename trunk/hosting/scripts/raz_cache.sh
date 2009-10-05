ea4fad66b80cbf0a0e0a1c6f447f03e21c0b2a88
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-10-05 20:56:21 +0000

    * dodo

--- trunk/hosting/scripts/raz_cache.sh
+++ trunk/hosting/scripts/raz_cache.sh
@@ -2,7 +2,7 @@
 #Commun aux scripts
 source $VLMRACINE/conf/conf_script || exit 1
 
-rm -Rf $VLMJEUROOT=/medias/images/racemaps/*
-rm -Rf $VLMJEUROOT=/medias/images/minimaps/*
-rm -Rf $VLMJEUROOT=/medias/images/pavillons/*
+rm -Rf $VLMJEUROOT/medias/images/racemaps/*
+rm -Rf $VLMJEUROOT/medias/images/minimaps/*
+rm -Rf $VLMJEUROOT/medias/images/pavillons/*
 
