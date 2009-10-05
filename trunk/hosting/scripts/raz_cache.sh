d23260d33d63ebd6c9df351cd5f256a4defac3aa
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-10-05 19:58:13 +0000

    * script pour vider le cache des images

---
+++ trunk/hosting/scripts/raz_cache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Commun aux scripts
+source $VLMRACINE/conf/conf_script || exit 1
+
+rm -Rf $VLMRACINE/medias/images/racemaps/*
+rm -Rf $VLMRACINE/medias/images/minimaps/*
+rm -Rf $VLMRACINE/medias/images/pavillons/*
+
