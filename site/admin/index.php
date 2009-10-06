848a9ff922eecf5a5dfba29e427addc45298010f
paparazzia <paparazzia@gmail.com>
2009-10-06 20:33:50 +0000

    * fix #204

--- site/admin/index.php
+++ site/admin/index.php
@@ -2,35 +2,37 @@
     $PAGETITLE = "VLM Management";
     include("htmlstart.php");
 
+    function adminlistbox($list, $title) {
+        echo "<div class=\"adminbox\">";
+        echo "<h3>$title</h3>";
+        echo "<ul>";
+        foreach ($list as $page => $pagedef) {
+            echo "<li>";
+            echo "<a href=\"$page\">$pagedef</a>";
+            echo "</li>";
+        }
+        echo "</ul></div>";
+    }
+
     $tablepages = Array(
-        "races.php" => "Races definitions",
+        "races.php" => "Races definitions [Experimental]",
         "races_instructions.php" => "Races instructions",
         );
+    adminlistbox($tablepages, "Table administration");
 
-    echo "<div class=\"adminbox\">";
-    echo "<h3>Table administration</h3>";
-    echo "<ul>";
-    foreach ($tablepages as $page => $pagedef) {
-        echo "<li>";
-        echo "<a href=\"$page\">$pagedef</a>";
-        echo "</li>";
-        }
-    echo "</ul></div>";
 
     $uploadpages = Array(
         "uploadracemap.php" => "Race map upload",
         "uploadflag.php" => "Flag upload",
         );
 
-    echo "<div class=\"adminbox\">";
-    echo "<h3>Upload tools</h3>";
-    echo "<ul>";
-    foreach ($uploadpages as $page => $pagedef) {
-        echo "<li>";
-        echo "<a href=\"$page\">$pagedef</a>";
-        echo "</li>";
-        }
-    echo "</ul></div>";
+    adminlistbox($uploadpages, "Table administration");
+
+    $strangepages = Array(
+        "strange_engaged_in_unknown.php" => "Engaged in unknown race.",
+        );
+
+    adminlistbox($strangepages, "Strangeness reports [Use with caution]");
 
     
     include("htmlend.php");
---
+++ site/admin/strange_engaged_in_unknown.php
@@ -0,0 +1,19 @@
+<?php
+    $PAGETITLE = "Strange case : Boat engaged in unknown race";
+    include ("htmlstart.php");
+    include_once ("functions.php");
+        
+    if ($_REQUEST["action"] == "go" and $_REQUEST['confirm'] == "on" ) {
+        wrapper_mysql_db_query("update users SET engaged=0 where engaged != 0 and engaged not in (select distinct idraces from races)");
+        echo "<h3>Done, following results should be empty.</h3>";
+    }
+    htmlQuery("select idusers, username, boatname, class, engaged, from_unixtime(lastchange) as lastchange from users where engaged != 0 and engaged not in (select distinct idraces from races)");
+?>
+        <form action="#" method="post">
+            <input type="hidden" name="action" value="go" />
+            <input type="checkbox" name="confirm">
+            <input type="submit" value="Clean it ?" />
+        </form>
+<?php
+    include ("htmlend.php");
+?>
