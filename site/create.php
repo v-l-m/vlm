084d9658e773308053073a7c9b1c64cdd15f4cf9
paparazzia <paparazzia@gmail.com>
2009-10-08 20:19:11 +0000

    fix #82 : correction du js en specifiant uniquement a-zA-Z0-9_ comme lettres ad-hoc   (le reste du ticket est trop flou)

--- site/create.php
+++ site/create.php
@@ -9,22 +9,26 @@ include_once("config.php");
 <script language="javascript" type="text/javascript">
     function submitbutton() {
       var form = document.myboat;
-      var r = new RegExp("[\<|\>|\"|\'|\%|\;|\(|\)|\&|\+|\-]", "i");
+      var r = new RegExp("[^a-zA-Z0-9_]", "i");
 
       // do field validation
       if (form.pseudo.value == "") {
         alert( "Vous devez entrer un pseudo !" ); // voir ensuite pour le choix lang - translation !
+        return false;
       } else if (r.exec(form.pseudo.value) || form.pseudo.value.length < 3) {
         alert( "caractere non-autorise ou pseudo trop court (mini 4)" ); // voir ensuite pour le choix lang - translation !
+        return false;
       // SkYDuST : Je pense qu'il faudrait ajouter un champ email pour pouvoir envoyer des communications (et aussi pour la securite)
       //} else if (form.email.value == "") {
       //  alert( "vous devez entrer votre mail" );
-      } else if ((form.password.value != "") {
+      } else if (form.password.value == "") {
         alert( "Mot de passe obligatoire !" ); // voir ensuite pour le choix lang - translation !
+        return false;
       } else if (r.exec(form.password.value)) {
         alert( "votre mot de passe doit contenir des caracteres non-autorises" ); // voir ensuite pour le choix lang - translation !
+        return false;
       } else {
-        form.submit();
+        return true;
       }
     }
   </script>
@@ -37,7 +41,7 @@ echo "<p>";
 
  echo $strings[$lang]["chooseaccount"];?>
 
-<form action="myboat.php" method="post" name="myboat"> <!-- modif SkYDuST -->
+<form onSubmit="return submitbutton();"  action="myboat.php" method="post" name="myboat"> <!-- modif SkYDuST -->
 <?echo $strings[$lang]["login_name"]?><br/>
 <input size="15" maxlength="15" name="pseudo"/><br/>
 <?echo $strings[$lang]["password"]?><br/>
