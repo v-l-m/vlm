28647477189ceddd0f86b0b63051cd8d810ae1ee
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-10-12 18:39:43 +0000

    * cleaning boat_ tables

--- branches/V_0_11/base/scripts/upgrade-v0.11.sql
+++ branches/V_0_11/base/scripts/upgrade-v0.11.sql
@@ -1,3 +1,25 @@
+#cleaning...
+DROP TABLE IF EXISTS `boat_A35`;
+DROP TABLE IF EXISTS `boat_C5`;
+DROP TABLE IF EXISTS `boat_C5v2`;
+DROP TABLE IF EXISTS `boat_Class40`;
+DROP TABLE IF EXISTS `boat_Imoca`;
+DROP TABLE IF EXISTS `boat_Imoca2007`;
+DROP TABLE IF EXISTS `boat_Imoca2008`;
+DROP TABLE IF EXISTS `boat_Mono650`;
+DROP TABLE IF EXISTS `boat_OEv2009`;
+DROP TABLE IF EXISTS `boat_OceanExpress`;
+DROP TABLE IF EXISTS `boat_VLM70`;
+DROP TABLE IF EXISTS `boat_cigale14`;
+DROP TABLE IF EXISTS `boat_debug`;
+DROP TABLE IF EXISTS `boat_dnf`;
+DROP TABLE IF EXISTS `boat_figaro`;
+DROP TABLE IF EXISTS `boat_figaro2`;
+DROP TABLE IF EXISTS `boat_hi5`;
+DROP TABLE IF EXISTS `boat_imoca60`;
+DROP TABLE IF EXISTS `boat_maxicata`;
+
+
 #Status table for tracking module upgrades, anticipating v0.12
 CREATE TABLE IF NOT EXISTS modules_status (
   `autoid`     bigint NOT NULL AUTO_INCREMENT,
