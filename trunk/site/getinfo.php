93d1354b0587e3f1a378fb11fe777397134594e7
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-10-12 07:30:09 +0000

    don't recompute a race when it's already there in fullUsers.

--- trunk/site/getinfo.php
+++ trunk/site/getinfo.php
@@ -5,12 +5,12 @@ header("content-type: text/plain; charset=UTF-8");
 
 
   $idu=htmlentities(quote_smart($_REQUEST['idu']));
-  if (  round($idu) == 0 ) {
-     echo "usage : http://virtual-loup-de-mer.org/getinfo.php?idu=X&pseudo=xxxx&password=pppp\n";
-     echo "\nX = numero de votre bateau";
-     echo "\nxxxxx = votre nom d'utilisateur";
-     echo "\nppppp = votre mot de passe";
-     echo "\nVariables = \n
+  if (round($idu) == 0) {
+    echo "usage : http://virtual-loup-de-mer.org/getinfo.php?idu=X&pseudo=xxxx&password=pppp\n";
+    echo "\nX = numero de votre bateau";
+    echo "\nxxxxx = votre nom d'utilisateur";
+    echo "\nppppp = votre mot de passe";
+    echo "\nVariables = \n
     #* WPL : liste de Waypoints (liste)
     #* RAC : numéro de la course (string)
     #* IDB : nom du bateau (string)
@@ -71,133 +71,127 @@ header("content-type: text/plain; charset=UTF-8");
     exit;
   }
 
-  $pseudo=quote_smart($_REQUEST['pseudo']);
-  $password=quote_smart($_REQUEST['password']);
+$pseudo=quote_smart($_REQUEST['pseudo']);
+$password=quote_smart($_REQUEST['password']);
 
-  // Les clients Linux modernes utilisent l'UTF-8, alors que les clients Windows subsistent en IDO88591
-  // On teste donc checkaccount en conversion ISO8859-1   *ET*  en UTF-8
-  if ( checkAccount(htmlentities($pseudo,ENT_COMPAT)        , htmlentities($password, ENT_COMPAT)) != $idu 
-    && checkAccount(htmlentities($pseudo,ENT_COMPAT,"UTF-8"), htmlentities($password, ENT_COMPAT,"UTF-8")) != $idu  ) {
-    echo "You should not do that.";
-    exit;
-  }
+// Les clients Linux modernes utilisent l'UTF-8, alors que les clients Windows subsistent en IDO88591
+// On teste donc checkaccount en conversion ISO8859-1   *ET*  en UTF-8
+if ( checkAccount(htmlentities($pseudo,ENT_COMPAT), 
+		  htmlentities($password, ENT_COMPAT)) != $idu 
+     && checkAccount(htmlentities($pseudo,ENT_COMPAT,"UTF-8"),
+		     htmlentities($password, ENT_COMPAT,"UTF-8")) != $idu  ) {
+  echo "You should not do that.";
+  exit;
+}
 
-  $usersObj = new fullUsers($idu);
-  printf ("IDU=%d\n", $usersObj->users->idusers) ;
-  printf ("IDB=%s\n", $usersObj->users->boatname) ;
-  printf ("EML=%s\n", $usersObj->users->email) ;
-  printf ("COL=%s\n", $usersObj->users->color) ;
-  printf ("CNT=%s\n", $usersObj->users->country) ;
-  printf ("POL=%s\n", $usersObj->users->boattype) ;
+$usersObj = new fullUsers($idu);
+printf ("IDU=%d\n", $usersObj->users->idusers) ;
+printf ("IDB=%s\n", $usersObj->users->boatname) ;
+printf ("EML=%s\n", $usersObj->users->email) ;
+printf ("COL=%s\n", $usersObj->users->color) ;
+printf ("CNT=%s\n", $usersObj->users->country) ;
+printf ("POL=%s\n", $usersObj->users->boattype) ;
 
-  if ( $usersObj->users->engaged == 0 ) {
+if ( $usersObj->users->engaged == 0 ) {
+  // Race is 0
+  echo "RAC=0\nno-data:not engaged on any race";
+} else {
+  printf ("RAC=%d\n", $usersObj->users->engaged) ;
+  $racesObj = &$usersObj->races;
+  printf ("RAN=%s\n", $racesObj->racename) ;
+  printf ("LAT=%s\n", $usersObj->lastPositions->lat) ;
+  printf ("LON=%s\n", $usersObj->lastPositions->long) ;
+  printf ("BSP=%2.2f\n", round($usersObj->boatspeed, 2));
+  printf ("HDG=%05.2f\n" , $usersObj->users->boatheading );
+  printf ("NWP=%02d\n" , $usersObj->users->nwp );
+  printf ("DNM=%4.2f\n", round($usersObj->distancefromend,2)) ;
+  printf ("ORT=%03.2f\n" , $usersObj->orthoangletoend );
+  printf ("LOX=%03.2f\n" , $usersObj->loxoangletoend ) ;
+  printf ("VMG=%2.2f\n", round($usersObj->VMGortho, 2));
 
-     // Race is 0
-     echo "RAC=0\nno-data:not engaged on any race";
-
+  if ( $usersObj->VMGortho != 0 ) {
+    $_timetogo=60 * 60 * $usersObj->distancefromend / $usersObj->VMGortho;
+    if ( $_timetogo < 0 ) {
+      $ETA=-1;
+    } else {
+      $ETA=sprintf(gmdate('Y-m-d H:i:s', time() + $_timetogo )); 
+    }
   } else {
-
-     printf ("RAC=%d\n", $usersObj->users->engaged) ;
-     $racesObj = new races($usersObj->users->engaged);
-     printf ("RAN=%s\n", $racesObj->racename) ;
-     printf ("LAT=%s\n", $usersObj->lastPositions->lat) ;
-     printf ("LON=%s\n", $usersObj->lastPositions->long) ;
-     printf ("BSP=%2.2f\n", round($usersObj->boatspeed, 2));
-     printf ("HDG=%05.2f\n" , $usersObj->users->boatheading );
-     printf ("NWP=%02d\n" , $usersObj->users->nwp );
-     printf ("DNM=%4.2f\n", round($usersObj->distancefromend,2)) ;
-     printf ("ORT=%03.2f\n" , $usersObj->orthoangletoend );
-     printf ("LOX=%03.2f\n" , $usersObj->loxoangletoend ) ;
-     printf ("VMG=%2.2f\n", round($usersObj->VMGortho, 2));
-
-     if ( $usersObj->VMGortho != 0 ) {
-         $_timetogo=60 * 60 * $usersObj->distancefromend / $usersObj->VMGortho;
-         if ( $_timetogo < 0 ) {
-            $ETA=-1;
-         } else {
-            $ETA=sprintf(gmdate('Y-m-d H:i:s', time() + $_timetogo )); 
-         }
-     } else {
-         $ETA=-1;
-     }
-     printf ("ETA=%s\n", $ETA);
-     $twd=$usersObj->wheading + 180; 
-     while ( $twd > 360 ) { $twd-=360; }
-     printf ("TWD=%05.2f\n" , round($twd,2) ) ;
-
-     printf ("TWS=%5.2f\n" , $usersObj->wspeed ) ;
-
-     // Calcul du TWA signé
-     $twa=round($usersObj->boatanglewithwind,2);
-     printf ("TWA=%5.2f\n" , $twa );
-
-     printf ("PIM=%d\n" , $usersObj->users->pilotmode );
-     switch ( $usersObj->users->pilotmode ) {
-        case 1: 
-          printf ("PIP=%5.2f\n", $usersObj->users->boatheading );
-          break;
-        case 2:
-          printf ("PIP=%5.2f\n", $usersObj->users->pilotparameter );
-          break;
-        case 3:
-        case 4:
-          if ( $usersObj->users->targetlat == 0 && $usersObj->users->targetlong == 0 ) {
-              printf ("PIP=%5.4f,%5.4f\n", $usersObj->users->LatNM, $usersObj->users->LonNM );
-          } else {
-              printf ("PIP=%5.4f,%5.4f@%d\n", $usersObj->users->targetlat, $usersObj->users->targetlong, $usersObj->users->targetandhdg );
-          }
-     }
-     printf ("POS=%s\n" , getCurrentRanking($idu, $usersObj->users->engaged) );
-     printf ("LOC=%s\n" , $usersObj->users->loch);
-     printf ("LUP=%d\n" , $usersObj->users->lastupdate ) ;
-     printf ("NUP=%d\n" , 10 * round($usersObj->users->lastupdate + 60*$racesObj->vacfreq - time())/10);
-     printf ("AVG=%02.2f\n", 3600*$usersObj->users->loch/(time() - $usersObj->users->userdeptime));
-     printf ("WPLAT=%s\n", $usersObj->users->targetlat) ;
-     printf ("WPLON=%s\n", $usersObj->users->targetlong) ;
-     printf ("H@WP=%s\n", $usersObj->users->targetandhdg) ;
-
-     // Map Preferences
-     $mapvar = array (
-            'mapAge' => 'MAG',
-            'maparea' => 'MAR',
-            'mapCenter' => 'MCR',
-            'mapEstime' => 'MES',
-            'mapLayers' => 'MLY',
-            'mapMaille' => 'MGD',
-            'mapOpponents' => 'MOP',
-            'mapPrefOpponents' => 'MPO',
-            'mapTools' => 'MTL',
-            'mapX' => 'MWD',
-            'mapY' => 'MHT',
-     );
-     $prefs=listUserPref($idu);
-     while ( $pref = current($prefs)  ) { 
-  echo $mapvar[key($prefs)] . "=" . $pref . "\n"; 
-        next($prefs);
-     }
-     
-     printf ("SRV=%s\n", SERVER_NAME ) ;
-
-
-
-     printf ("NPD=\"%s\"\n", $usersObj->users->blocnote) ; //encloser pour le blocnote
-
-     // Pilototo data
-     $rc=$usersObj->users->pilototoList();
-     $numligne=1;
-     foreach ($usersObj->users->pilototo as $pilototo_row) { /*(id,time,PIM,PIP,status)*/
-          printf("PIL%d=%d,%d,%d,%s,%s\n",$numligne,$pilototo_row[0],$pilototo_row[1],$pilototo_row[2],$pilototo_row[3],$pilototo_row[4]);
-          $numligne++;
-     }
-     while($numligne<=5)
-     {
-          printf("PIL%d=none\n",$numligne);
-          $numligne++;
-     }
-     printf ("THM=%s\n", $usersObj->users->theme) ;
-     printf ("HID=%d\n", $usersObj->users->hidepos) ;
-     // vacfreq est en minutes dans la base, mais affiché en secondes 
- 	   printf ("VAC=%d\n" , 60 * $racesObj->vacfreq ); 
+    $ETA=-1;
   }
-?>
+  printf ("ETA=%s\n", $ETA);
+  $twd=$usersObj->wheading + 180; 
+  while ( $twd > 360 ) { $twd-=360; }
+  printf ("TWD=%05.2f\n" , round($twd,2) ) ;
+
+  printf ("TWS=%5.2f\n" , $usersObj->wspeed ) ;
+
+  // Calcul du TWA signé
+  $twa=round($usersObj->boatanglewithwind,2);
+  printf ("TWA=%5.2f\n" , $twa );
+
+  printf ("PIM=%d\n" , $usersObj->users->pilotmode );
+  switch ( $usersObj->users->pilotmode ) {
+  case 1: 
+    printf ("PIP=%5.2f\n", $usersObj->users->boatheading );
+    break;
+  case 2:
+    printf ("PIP=%5.2f\n", $usersObj->users->pilotparameter );
+    break;
+  case 3:
+  case 4:
+    if ( $usersObj->users->targetlat == 0 && $usersObj->users->targetlong == 0 ) {
+      printf ("PIP=%5.4f,%5.4f\n", $usersObj->users->LatNM, $usersObj->users->LonNM );
+    } else {
+      printf ("PIP=%5.4f,%5.4f@%d\n", $usersObj->users->targetlat, $usersObj->users->targetlong, $usersObj->users->targetandhdg );
+    }
+  }
+  printf ("POS=%s\n" , getCurrentRanking($idu, $usersObj->users->engaged) );
+  printf ("LOC=%s\n" , $usersObj->users->loch);
+  printf ("LUP=%d\n" , $usersObj->users->lastupdate ) ;
+  printf ("NUP=%d\n" , 10 * round($usersObj->users->lastupdate + 60*$racesObj->vacfreq - time())/10);
+  printf ("AVG=%02.2f\n", 3600*$usersObj->users->loch/(time() - $usersObj->users->userdeptime));
+  printf ("WPLAT=%s\n", $usersObj->users->targetlat) ;
+  printf ("WPLON=%s\n", $usersObj->users->targetlong) ;
+  printf ("H@WP=%s\n", $usersObj->users->targetandhdg) ;
+
+  // Map Preferences
+  $mapvar = array (
+		   'mapAge' => 'MAG',
+		   'maparea' => 'MAR',
+		   'mapCenter' => 'MCR',
+		   'mapEstime' => 'MES',
+		   'mapLayers' => 'MLY',
+		   'mapMaille' => 'MGD',
+		   'mapOpponents' => 'MOP',
+		   'mapPrefOpponents' => 'MPO',
+		   'mapTools' => 'MTL',
+		   'mapX' => 'MWD',
+		   'mapY' => 'MHT',
+		   );
+  $prefs=listUserPref($idu);
+  while ( $pref = current($prefs)  ) { 
+    echo $mapvar[key($prefs)] . "=" . $pref . "\n"; 
+    next($prefs);
+  }
   
+  printf ("SRV=%s\n", SERVER_NAME ) ;
+  printf ("NPD=\"%s\"\n", $usersObj->users->blocnote) ; //encloser pour le blocnote
+  // Pilototo data
+  $rc=$usersObj->users->pilototoList();
+  $numligne=1;
+  foreach ($usersObj->users->pilototo as $pilototo_row) { /*(id,time,PIM,PIP,status)*/
+    printf("PIL%d=%d,%d,%d,%s,%s\n",$numligne,$pilototo_row[0],$pilototo_row[1],$pilototo_row[2],$pilototo_row[3],$pilototo_row[4]);
+    $numligne++;
+  }
+  while($numligne<=5) {
+    printf("PIL%d=none\n",$numligne);
+    $numligne++;
+  }
+  printf ("THM=%s\n", $usersObj->users->theme) ;
+  printf ("HID=%d\n", $usersObj->users->hidepos) ;
+  // vacfreq est en minutes dans la base, mais affiché en secondes 
+  printf ("VAC=%d\n" , 60 * $racesObj->vacfreq ); 
+}
+?>
+
