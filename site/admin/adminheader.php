a4353a6911a8bddd71d11fbcb571d6ab88bfb2dd
paparazzia <paparazzia@gmail.com>
2009-10-13 20:14:16 +0000

    * prise en charge correcte du langage dans phpMyEdit

--- site/admin/adminheader.php
+++ site/admin/adminheader.php
@@ -53,7 +53,7 @@ $opts['cgi']['prefix']['data']      = 'PME_data_';
 /* Get the user's default language and use it if possible or you can
    specify particular one you want to use. Refer to official documentation
    for list of available languages. */
-$opts['language'] = $_SERVER['HTTP_ACCEPT_LANGUAGE'] . '-UTF8';
+$opts['language'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) . '-UTF8';
 
 /* Table-level filter capability. If set, it is included in the WHERE clause
    of any generated SELECT statement in SQL query. This gives you ability to
