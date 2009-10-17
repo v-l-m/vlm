47b48b5decf7ba7f6e74fcba7df21a6dd5ad2fa5
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-10-17 09:59:40 +0000

    using different wrapper for read and write operations [refs #236]

--- trunk/site/admin/strange_engaged_in_unknown.php
+++ trunk/site/admin/strange_engaged_in_unknown.php
@@ -4,7 +4,7 @@
     include_once ("functions.php");
         
     if ($_REQUEST["action"] == "go" and $_REQUEST['confirm'] == "on" ) {
-        wrapper_mysql_db_query("update users SET engaged=0 where engaged != 0 and engaged not in (select distinct idraces from races)");
+        wrapper_mysql_db_query_writer("update users SET engaged=0 where engaged != 0 and engaged not in (select distinct idraces from races)");
         insertAdminChangelog($operation = "Update users engaged in unknown race");
 
         echo "<h3>Done, following results should be empty.</h3>";
