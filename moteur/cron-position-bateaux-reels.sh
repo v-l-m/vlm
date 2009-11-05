c7a97ea8f9340de995dd2c7b335e6fbd15430f85
fm <github-fm@actilis.net>
2009-11-05 17:06:21 +0000

    Apres quelques petites corrections

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-#VLMRACINE=/base/de/vlm #A configurer normalement fans le crontab
 source $VLMRACINE/conf/conf_script || exit 1
 
-
-# Ce script doit etre executable.
-if [ -x /opt/php/bin/php ]; then
-    PHP=/opt/php/bin/php
-else
-    PHP=php
-fi
-
-
-#=======
-# VOR : finished
-# $PHP $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
-
 # G3
-$PHP $VLMRACINE/vlmcode/moteur/posscripts/g3pos.php
+nice -1 $VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/g3pos.php
 
