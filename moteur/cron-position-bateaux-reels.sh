c13c760feba5498d3568dd6b68da3ea3a61bd65f
paparazzia <paparazzia@gmail.com>
2009-11-08 22:28:17 +0000

    merge from v0_11

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-#VLMRACINE=/base/de/vlm #A configurer normalement fans le crontab
 source $VLMRACINE/conf/conf_script || exit 1
 
-
-# Ce script doit etre executable.
-if [ -x /opt/php/bin/php ]; then
-    PHP=/opt/php/bin/php
-else
-    PHP=php
-fi
-
-
-#=======
-# VOR : finished
-# $PHP $VLMRACINE/vlmcode/moteur/posscripts/VolvoPos.php
-
-
+# G38
+nice -1 $VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/g3pos.php
+# KAWA !
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawa20091108.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawa20091110.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
