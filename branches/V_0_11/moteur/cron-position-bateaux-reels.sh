10e7d2397e661ee9b613c7f168b73bae87673288
fm <fm@9fd53e09-640f-4c6e-8556-f39f7746c792>
2009-11-10 09:35:09 +0000

    Log dans fichier plutot que sur stdout

--- branches/V_0_11/moteur/cron-position-bateaux-reels.sh
+++ branches/V_0_11/moteur/cron-position-bateaux-reels.sh
@@ -5,5 +5,5 @@ source $VLMRACINE/conf/conf_script || exit 1
 # G38
 nice -1 $VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/g3pos.php
 # KAWA !
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawacup20091108.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawacup20091110.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawacup20091108.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php > $VLMTEMP/position_reels.log 2>&1
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawacup20091110.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php >> $VLMTEMP/position_reels.log 2>&1
