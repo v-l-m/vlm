0c4089a86b741ae10d277200df358d57bc0c221d
fm <github-fm@actilis.net>
2009-12-30 21:32:47 +0000

    Merge from V_0_12

--- moteur/validation-WP/cron-validation.sh
+++ moteur/validation-WP/cron-validation.sh
@@ -10,5 +10,7 @@ cd $VLMJEUROOT/moteur/validation-WP
 #sh valide-20080511.sh
 #sh valide-20081109.sh
 #sh valide-81102.sh
-sh valide-81103.sh
-sh valide-81005.sh
+#sh valide-81103.sh
+#sh valide-81005.sh
+sh zoneInterdite.sh
+
