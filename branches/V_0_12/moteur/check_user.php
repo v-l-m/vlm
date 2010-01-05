3a8bea6c66a6fbb912ce85a0bcbb4598f73c3291
fm <fm@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-01-05 22:33:30 +0000

    Et bien sur, petite sensibilité invisible... le . collé au %360 !

--- branches/V_0_12/moteur/check_user.php
+++ branches/V_0_12/moteur/check_user.php
@@ -121,7 +121,7 @@
         $distVac=round($fullUsersObj->boatspeed*$fullUsersObj->hours,3);
 
         echo "\tPosition update (WSpd=" . round($fullUsersObj->wspeed,1) . 
-          ", WHdg=".(round($fullUsersObj->wheading,1)+180)%360. 
+          ", WHdg=".((round($fullUsersObj->wheading,1)+180)%360). 
           ", Hours=".round($fullUsersObj->hours,4). 
           ", ANG=".round($fullUsersObj->boatanglewithwind,2). 
           ", Hdg=" . $fullUsersObj->users->boatheading. 
