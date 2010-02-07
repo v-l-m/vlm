438bf363e59760ac2bd0358c0c274b88b356ce35
fm <github-fm@actilis.net>
2010-02-07 10:07:54 +0000

    merge vers trunk pour position  G3 et zone interdite BP5

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -2,7 +2,7 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-# G38
+# G3
 nice -1 $VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/g3pos.php
 # KAWA !
 nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawa20091108.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
