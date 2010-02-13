b045212beca53274b244d53385440bde3ab8ddbf
fm <fm@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-02-13 09:38:31 +0000

    Merge from V_0_12

--- trunk/moteur/cron-position-bateaux-reels.sh
+++ trunk/moteur/cron-position-bateaux-reels.sh
@@ -4,6 +4,8 @@ source $VLMRACINE/conf/conf_script || exit 1
 
 # G3
 nice -1 $VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/g3pos.php
+# BP5
+nice -1 $VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/bp5pos.php
 # KAWA !
 nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawa20091108.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kawa20091110.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
