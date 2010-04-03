dd7326a6136677acdcd9f88bc05d7a8d5f6cba9f
paparazzia <paparazzia@gmail.com>
2010-04-03 10:04:51 +0000

    * fix #266

--- site/ws/boatinfo.php
+++ site/ws/boatinfo.php
@@ -1,55 +1,11 @@
 <?php
 include_once("config.php");
+include_once("wslib.php");
 
 header("content-type: text/plain; charset=UTF-8");
 
 function get_output_format() {
-  $qjson = 0;
-  $sjson = false;
-  $qplain = 0.1;
-  $splain = false;
-  
-  if (isset($_REQUEST['forcefmt'])) {
-      $fmt = $_REQUEST['forcefmt'];
-      if (in_array($fmt, Array('json', 'text'))) {
-          return $fmt;
-      }
-      //else, we still try to autodetect...
-  }
-
-  if (preg_match(',application/json(;q=(d?.d+))?,i', 
-                  $_SERVER['HTTP_ACCEPT'], $res)) {
-    $qjson = (isset($res[2]))? $res[2] : 1;
-    $sjson = true;
-  }
-  
-  if (preg_match(',text/plain(;q=(d?.d+))?,i', 
-                  $_SERVER['HTTP_ACCEPT'], $res)) {
-    $qplain = (isset($res[2]))? $res[2] : 1;
-    $splain = true;
-  } else if (preg_match(',text/\*(;q=(d?.d+))?,i', 
-              $_SERVER['HTTP_ACCEPT'], $res)) {
-    $qplain = (isset($res[2]))? $res[2] : 1;
-    $splain = true;
-  }
-  if ($qplain > $qjson) {
-    return "text";
-  } else if ($qplain < $qjson) {
-    return "json";
-  }
-  if ( $splain == $sjson) {
-    // both there, take the first one :)
-    $jsonpos = stripos($_SERVER['HTTP_ACCEPT'], "text/json");
-    $plainpos = stripos($_SERVER['HTTP_ACCEPT'], "text/plain");
-    if ($plainpos === false) {
-      $plainpos = stripos($_SERVER['HTTP_ACCEPT'], "text/*");  
-    }
-    if ($plainpos > $jsonpos) {
-      return "json";
-    } 
-  }
-  return "text";
-}
+  return get_requested_output_format();
 
 function get_info_array($idu) {
   $info = array();
@@ -255,43 +211,7 @@ function ia_print($value, $key) {
 
 // now start the real work
 
-function ask_for_auth() {
-    header('WWW-Authenticate: Basic realm="VLM Access"');
-    header($_SERVER["SERVER_PROTOCOL"]." 401 Unauthorized");
-    header("Content-Type: text/plain; charset=UTF-8");
-    usage();
-}
-
-session_start();
-// do we know the user from a previous login session?
-if (array_key_exists('idu', $_SESSION) && array_key_exists('loggedin', $_SESSION) 
-    && ($_SESSION['loggedin'] == 1)) {
-  $idu = $_SESSION['idu'];
-  $pseudo = $_SESSION['login'];
-  $IP = $_SESSION['IP'];
-} else {
-  // fallback to HTTP auth
-  if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
-    ask_for_auth();
-    exit;
-  } else {
-    $pseudo=$_SERVER['PHP_AUTH_USER'];
-    $passwd=$_SERVER['PHP_AUTH_PW'];
-    $idu = checkAccount($pseudo, $passwd);
-    // FIXME, do we need to check after utf-8 transform?
-    /*
-      if ((checkAccount(htmlentities($pseudo,ENT_COMPAT), 
-      htmlentities($password, ENT_COMPAT)) != FALSE)
-      || (checkAccount(htmlentities($pseudo,ENT_COMPAT,"UTF-8"),
-      htmlentities($password, ENT_COMPAT,"UTF-8")) |= FALSE)) {
-    */
-    if ($idu == FALSE) {
-      ask_for_auth();
-      exit();
-    }
-    login($idu, $pseudo);
-  }
-}
+login_if_not();
 
 $fmt = get_output_format();
 $info_array = get_info_array($idu);
