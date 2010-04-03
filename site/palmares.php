951a7d9d19c4982399f53cf32baba42ef72b1701
paparazzia <paparazzia@gmail.com>
2010-04-03 05:27:28 +0000

    * refs #212

--- site/palmares.php
+++ site/palmares.php
@@ -11,12 +11,8 @@
         $idusers=htmlentities(quote_smart($_REQUEST['idusers']));
         echo "<h4>" ; printf ($strings[$lang]["palmares"],$idusers); echo "</h4>";
         displayPalmares($idusers);
-?>
-    <form>
-      <input type="button" value="Close" onclick="javascript:self.close();" />
-    </form>
-<?php
-}
-include_once("includes/footer.inc");
+    }
+
+    include_once("includes/footer.inc");
 ?>
 
