20f9f58828d6d1cc3e7a4ce38d9901837116dbe2
paparazzia <paparazzia@gmail.com>
2010-04-28 20:39:23 +0000

    * bout de code pour reformater $strings

---
+++ tests/reprint-translation.php
@@ -0,0 +1,15 @@
+<?php
+
+include "../site/includes/strings.inc";
+
+foreach ($strings as $k => $v) {
+    ksort($v);
+    $strings[$k] = $v;
+    }
+
+echo '<?php\n$strings = ';
+var_export($strings);
+echo ';\n\n?>';
+
+?>
+    
