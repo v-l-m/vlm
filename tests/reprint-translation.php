06a4bcca213f59d77d7579c0b43e0b5c433fe86f
paparazzia <paparazzia@gmail.com>
2010-05-02 15:21:11 +0000

    * indentation * optimisation (static flag) * ajout de get_cgi_var :   fonction utile pour rapatrier sans risque une variable cgi   (import depuis phpMyEdit)

--- tests/reprint-translation.php
+++ tests/reprint-translation.php
@@ -1,15 +1,15 @@
 <?php
 
-include "../site/includes/strings.inc";
+    include "../site/includes/strings.inc";
 
-foreach ($strings as $k => $v) {
-    ksort($v);
-    $strings[$k] = $v;
+    foreach ($strings as $k => $v) {
+        ksort($v);
+        $strings[$k] = $v;
     }
 
-echo '<?php\n$strings = ';
-var_export($strings);
-echo ';\n\n?>';
+    echo '<?php\n$strings = ';
+    var_export($strings);
+    echo ';\n\n?>';
 
 ?>
     
