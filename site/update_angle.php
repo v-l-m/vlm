5e526c18f98fccb4fc7864df0a4a021463a4eff6
paparazzia <paparazzia@gmail.com>
2010-05-09 16:10:11 +0000

    refs #316

--- site/update_angle.php
+++ site/update_angle.php
@@ -3,8 +3,8 @@
     include_once("config.php");
 
     // Controle de saisie / soumission
-    $pilotmodes=array("autopilot", "windangle", "orthodromic", "bestvmg", "vbvmg", "bestspeed");
-    if ( ! in_array($pilotmode, $pilotmodes) ) {
+    $pilotmodes = array("autopilot" => 1, "windangle" => 2, "orthodromic" => 3, "bestvmg" => 4, "vbvmg" => 5, "bestspeed" => 6);
+    if ( ! array_key_exists($pilotmode, $pilotmodes) ) {
         printf ("<h1>PILOTMODE = $pilotmode IS UNSUPPORTED.");
         printf ("<h2>Your IP address is : " . getip() . "</h2>\n");
         printf ("<h2>It has been logged. Don't try this again.</h2>\n");
@@ -23,21 +23,24 @@
     }
 
     if ($idusers != 0 ) {
-        if ( $idusers ==  getLoginId() ) {
+        if ( $idusers == getLoginId() ) {
 
+            //Contrôles amont...
             if ( $boatheading >= 360 ) $boatheading -= 360;
             if ( $boatheading < 0 ) $boatheading += 360;
             if ( $pilotparameter >= 360 ) $pilotparameter -= 360;
             if ( $pilotparameter > 180 ) $pilotparameter -= 360;
             if ( $pilotparameter < -180 ) $pilotparameter += 360;
+            // $pilotmode est lié  à l ihm, on le bascule en int pour l'interne VLM
+            $pim = $pilotmodes[$pilotmode];
 
             // Tout ça pour mettre à jour la ligne de la table users... on simplifie
             $fullUsersObj = new fullUsers($idusers);
-            if ($fullUsersObj->writeNewheading($pilotmode, $boatheading, $pilotparameter) === False) {
-                die($fullUsersObj->users->error_string);
+            if ($fullUsersObj->writeNewheading($pim, $boatheading, $pilotparameter) === False) {
+                die("<h1 class=\"error\">".$fullUsersObj->users->error_string."</h1>");
             }
 
-            echo "<h1 align=\"center\">&nbsp;<br />&nbsp;<br />" . getLocalizedString("angleupdated") . "<br />&nbsp;<br />&nbsp;</h1>";
+            echo "<h1 class=\"info\">".getLocalizedString("angleupdated")."</h1>";
         } else {
             printf ("<h1>This is not a nice thing to try this.</h1>");
             printf ("<h2>Your IP address is : " . getip() . "</h2>\n");
