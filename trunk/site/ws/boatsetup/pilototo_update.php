6448094aab41e1ae7ba085a0b7ecacd4fe64e7f1
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-06-03 12:05:21 +0000

    fixed the variable name (per KP report)

--- trunk/site/ws/boatsetup/pilototo_update.php
+++ trunk/site/ws/boatsetup/pilototo_update.php
@@ -23,7 +23,7 @@
         default :
             reply_with_error('PIM03');
     }
-    $fullusers->users->pilototoUpdate($taskid, $tasktime, $pim, $pip);
+    $ws->users->pilototoUpdate($taskid, $tasktime, $pim, $pip);
 
     $ws->finish();
 ?>
