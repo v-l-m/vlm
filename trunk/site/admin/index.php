7ffe7fb70001405fb147ae5dd49a30bd2125e820
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-06-06 12:25:41 +0000

    refs #331

--- trunk/site/admin/index.php
+++ trunk/site/admin/index.php
@@ -39,16 +39,22 @@
 
     adminlistbox($uploadpages, "Upload and import operations");
 
-    $strangepages = Array(
+    $reportpages = Array(
         "user_agents.php"                   => "User_agent statistics",
         "possible_duplicates.php"           => "Duplicates by IP",
-        "strange_engaged_in_unknown.php"    => "Engaged in unknown race.",
-        "strange_unknown_flag.php"          => "Boat with Unknown flag.",
         "racemaps_without_race.report.php"  => "Racemaps with no corresponding race",
         "races_without_racemap.report.php"  => "Races with no corresponding racemap",
         );
 
-    adminlistbox($strangepages, "Reports [Use with caution]");
+    adminlistbox($reportpages, "Reports");
+
+    $strangepages = Array(
+        "strange_results_for_unknown_races.php"    => "Results for unknown races.",
+        "strange_engaged_in_unknown.php"    => "Engaged in unknown race.",
+        "strange_unknown_flag.php"          => "Boat with Unknown flag.",
+        );
+    
+    adminlistbox($strangepages, "Report & fix [Use with caution to fix inconsistencies]");
 
     echo "<div class=\"adminbox\">";
     echo "<h3>Last operations</h3>";
