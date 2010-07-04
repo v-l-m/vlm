02e6e17a6e83c35f68f489e7e117ff126384411b
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-07-04 15:39:49 +0000

    * refs #356 : first draft pour la classe de base

--- trunk/base/scripts/upgrade-v0.14.sql
+++ trunk/base/scripts/upgrade-v0.14.sql
@@ -3,7 +3,7 @@ CREATE TABLE `players` (
   `idplayers` bigint(20) NOT NULL auto_increment,
   `email` varchar(50) default NULL,
   `password` varchar(50) default NULL,
-  `pseudo` varchar(20) default NULL,
+  `playername` varchar(20) default NULL,
   `updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `created` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`idplayers`),
