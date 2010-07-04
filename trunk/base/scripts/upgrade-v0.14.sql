e7a1ea00ccbb56fee9f6142ad85a238be24d0259
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-07-04 17:35:22 +0000

    * refs #357 : table temporaire pour gerer les players en attente de validation

--- trunk/base/scripts/upgrade-v0.14.sql
+++ trunk/base/scripts/upgrade-v0.14.sql
@@ -1,15 +1,24 @@
-#Table de gestion des PLAYERS
+#Tables de gestion des PLAYERS
+CREATE TABLE `players_pending` (
+  `idplayers_pending` bigint(20) NOT NULL auto_increment,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `playername` varchar(20) NOT NULL,
+  `updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `seed` bigint default 0,
+  PRIMARY KEY  (`idplayers_pending`),
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='players_pending validation';
+
 CREATE TABLE `players` (
   `idplayers` bigint(20) NOT NULL auto_increment,
-  `email` varchar(50) default NULL,
-  `password` varchar(50) default NULL,
-  `playername` varchar(20) default NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `playername` varchar(20) NOT NULL,
+  `description` text default NULL,
   `updated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `created` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`idplayers`),
   UNIQUE KEY `email` (`email`),
   KEY `email_password` (`email`,`password`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='players (not boats)';
-
-
 
