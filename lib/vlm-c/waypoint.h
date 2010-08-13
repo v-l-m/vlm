9534bdd4258073f9e402e76cd193d0cdbc313e7f
Yves Lafon <yves@w3.org>
2010-08-13 20:15:01 +0000

    init waypoint [See #34]

--- lib/vlm-c/waypoint.h
+++ lib/vlm-c/waypoint.h
@@ -1,5 +1,5 @@
 /**
- * $Id: waypoint.h,v 1.5 2010-08-13 16:00:22 ylafon Exp $
+ * $Id: waypoint.h,v 1.6 2010-08-13 20:03:35 ylafon Exp $
  *
  * (c) 2008 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -24,12 +24,17 @@
 #include "defs.h"
 #include "types.h"
 
-int check_waypoint_crossed PARAM8(double, double, time_t,
-				  double, double, time_t,
-				  waypoint *, time_t *);
-int check_waypoint         PARAM8(double, double,  
-				  double, double,  
-				  waypoint *, double *,
-				  double *, double *);
+void init_waypoint          PARAM9(waypoint *, int, int,
+				   double, double,
+				   double, double,
+				   double, double);
+
+int  check_waypoint_crossed PARAM8(double, double, time_t,
+				   double, double, time_t,
+				   waypoint *, time_t *);
+int  check_waypoint         PARAM8(double, double,  
+				   double, double,  
+				   waypoint *, double *,
+				   double *, double *);
 
 #endif /* _WAYPOINT_H_ */
