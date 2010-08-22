d5005698cb2fea270b29c9014e42c692f809040f
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-08-22 14:32:56 +0000

    leftover... [See #34]

--- trunk/lib/phpcommon/users.class.php
+++ trunk/lib/phpcommon/users.class.php
@@ -625,11 +625,17 @@ class fullUsers
     //     printf ("<p>Lat1=%f, Long1=%f</p>\n", $nextwaypoint[0]/1000, $nextwaypoint[1]/1000);
     //  printf ("<p>Lat2=%f, Long2=%f</p>\n", $nextwaypoint[2]/1000, $nextwaypoint[3]/1000);
     //  printf ("<p>BoatLat=%f, BoatLong=%f</p>\n", $this->lastPositions->lat/1000, $this->lastPositions->long/1000);
-
-    $xing_dist = VLM_distance_to_line_ratio_xing($this->lastPositions->lat, $this->lastPositions->long,
-             $nextwaypoint['latitude1'], $nextwaypoint['longitude1'],
-             $nextwaypoint['latitude2'], $nextwaypoint['longitude2'],
-             $lat_xing, $long_xing, $xing_ratio);
+    if (defined('MOTEUR')) {
+      $xing_dist = VLM_distance_to_line_ratio_xing($this->lastPositions->lat, $this->lastPositions->long,
+						   $nextwaypoint->latitude1, $nextwaypoint->longitude1,
+						   $nextwaypoint->latitude2, $nextwaypoint->longitude2,
+						   $lat_xing, $long_xing, $xing_ratio);
+    } else {
+      $xing_dist = VLM_distance_to_line_ratio_xing($this->lastPositions->lat, $this->lastPositions->long,
+						   $nextwaypoint['latitude1'], $nextwaypoint['longitude1'],
+						   $nextwaypoint['latitude2'], $nextwaypoint['longitude2'],
+						   $lat_xing, $long_xing, $xing_ratio);
+    }
     //  printf("Xing_dist %.3f, ratio %.3f\n", $xing_dist, doublep_value($xing_ratio));
     $coords = array( doublep_value($lat_xing) / 1000.0, doublep_value($long_xing) / 1000.0);
 
