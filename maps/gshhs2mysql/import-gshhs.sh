550810ddf168f0877d5d251f8b430209012f664e
paparazzia <paparazzia@gmail.com>
2010-08-31 17:05:22 +0000

    fix script (see #374)

--- maps/gshhs2mysql/import-gshhs.sh
+++ maps/gshhs2mysql/import-gshhs.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+#pour récupérer le VLMJEUROOT
+source $VLMRACINE/conf/conf_script
 
-PATH_TO_GSHHS=.
-DB_USERNAME=sample
-DB_DBNAME=vlmdev
+PATH_TO_GSHHS=$VLMGSHHS
+
+DB_USERNAME=$DBUSER
+DB_DBNAME=$DBPASSWORD
 
 for i in 'c' 'l' 'i' 'h' 'f'; do
-	echo "Working on $i
-	gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > coastline_$i.csv
-	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DB_USERNAME" --password "$DB_DBNAME" coastline_i$.csv
+	echo "Working on $i"
+	./gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > coastline_$i.csv
+	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DB_USERNAME" --password="$DB_DBNAME" coastline_i$.csv
 	rm -f coastline_$i.csv
 done
 
