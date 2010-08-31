c87d89329ba386ad2b5f81aa0030c1ee0ac756ae
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-08-31 18:25:46 +0000

    * add --local && --delete options   (maybe we should split csv build and csv import)

--- trunk/maps/gshhs2mysql/import-gshhs.sh
+++ trunk/maps/gshhs2mysql/import-gshhs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #pour récupérer le VLMJEUROOT
+source $VLMRACINE/conf/conf_script
 source $VLMRACINE/conf/conf_base
 
 PATH_TO_GSHHS=$VLMGSHHS
@@ -7,7 +8,7 @@ PATH_TO_GSHHS=$VLMGSHHS
 for i in 'c' 'l' 'i' 'h' 'f'; do
 	echo "Working on $i"
 	./gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > coastline_$i.csv
-	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DBPASSWORD" "$DBNAME" coastline_$i.csv
+	mysqlimport --delete --local --host="$DBSERVER" --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DBPASSWORD" "$DBNAME" coastline_$i.csv
 	rm -f coastline_$i.csv
 done
 
