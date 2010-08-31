87aab86e9ae4a673ec7256801bcc4a096710e26b
paparazzia <paparazzia@gmail.com>
2010-08-31 19:57:34 +0000

    * scripts et commentaires pour l'upgrade (see #374)

--- maps/gshhs2mysql/import-gshhs.sh
+++ maps/gshhs2mysql/import-gshhs.sh
@@ -7,9 +7,9 @@ PATH_TO_GSHHS=$VLMGSHHS
 
 for i in 'c' 'l' 'i' 'h' 'f'; do
 	echo "Working on $i"
-	./gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > coastline_$i.csv
-	mysqlimport --lock-tables --delete --local --host="$DBSERVER" --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DBPASSWORD" "$DBNAME" coastline_$i.csv
-	rm -f coastline_$i.csv
+	./gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > $VLMTEMP/coastline_$i.csv
+	mysqlimport --lock-tables --delete --local --host="$DBSERVER" --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DBPASSWORD" "$DBNAME" $VLMTEMP/coastline_$i.csv
+	rm -f $VLMTEMP/coastline_$i.csv
 done
 
 
