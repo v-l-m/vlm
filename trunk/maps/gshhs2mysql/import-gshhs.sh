c318f2b2d368a4ec92a43c9770db7750d2e08887
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-08-31 17:08:25 +0000

    typo...

--- trunk/maps/gshhs2mysql/import-gshhs.sh
+++ trunk/maps/gshhs2mysql/import-gshhs.sh
@@ -7,7 +7,7 @@ PATH_TO_GSHHS=$VLMGSHHS
 for i in 'c' 'l' 'i' 'h' 'f'; do
 	echo "Working on $i"
 	./gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > coastline_$i.csv
-	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DNPASSWORD" "$DB_DBNAME" coastline_i$.csv
+	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DBPASSWORD" "$DBNAME" coastline_i$.csv
 	rm -f coastline_$i.csv
 done
 
