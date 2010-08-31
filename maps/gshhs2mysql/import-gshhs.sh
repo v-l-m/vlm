01f2eb820542c6f57324bbbcd4d145e9755bbeaa
paparazzia <paparazzia@gmail.com>
2010-08-31 17:07:33 +0000

    * refix (see #374)

--- maps/gshhs2mysql/import-gshhs.sh
+++ maps/gshhs2mysql/import-gshhs.sh
@@ -4,13 +4,10 @@ source $VLMRACINE/conf/conf_script
 
 PATH_TO_GSHHS=$VLMGSHHS
 
-DB_USERNAME=$DBUSER
-DB_DBNAME=$DBPASSWORD
-
 for i in 'c' 'l' 'i' 'h' 'f'; do
 	echo "Working on $i"
 	./gshhs2csv $PATH_TO_GSHHS/gshhs_$i.b > coastline_$i.csv
-	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DB_USERNAME" --password="$DB_DBNAME" coastline_i$.csv
+	mysqlimport --fields-terminated-by="," --lines-terminated-by="\n" --columns=idpoint,idcoast,latitude,longitude --user="$DBUSER" --password="$DNPASSWORD" "$DB_DBNAME" coastline_i$.csv
 	rm -f coastline_$i.csv
 done
 
