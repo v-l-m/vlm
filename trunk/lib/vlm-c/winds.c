7909d3233a6aaee3898d56770ead7850fde76d74
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-10-05 21:41:43 +0000

    normalization of wind

--- trunk/lib/vlm-c/winds.c
+++ trunk/lib/vlm-c/winds.c
@@ -1,5 +1,5 @@
 /**
- * $Id: winds.c,v 1.30 2010-09-30 13:11:42 ylafon Exp $
+ * $Id: winds.c,v 1.32 2010-09-30 20:16:28 ylafon Exp $
  *
  * (c) 2008-2010 by Yves Lafon
  *      See COPYING file for copying and redistribution conditions.
@@ -1068,6 +1068,9 @@ wind_info *get_wind_info_latlong_hybrid_context(vlmc_context *context,
 #else
     c = - v - _Complex_I * u;
     angle = carg(c);
+    if (angle < 0) { 
+      angle += TWO_PI; 
+    } 
 #endif /* OLD_C_COMPILER */
 #ifdef DEBUG
     printf("time stamps: prev %ld, boat_time %ld", prev->prevision_time,
@@ -1085,6 +1088,9 @@ wind_info *get_wind_info_latlong_hybrid_context(vlmc_context *context,
 #else
     c = - vprev - _Complex_I * uprev;
     angle = carg(c);
+    if (angle < 0) { 
+      angle += TWO_PI; 
+    } 
 #endif /* OLD_C_COMPILER */
   }
 
