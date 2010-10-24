3ac92eb5f6d30cdb3200bc3dd557719fd46078f4
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-10-24 09:22:21 +0000

    fix #459

--- trunk/site/ws/serverinfo/misc.php
+++ trunk/site/ws/serverinfo/misc.php
@@ -9,6 +9,16 @@
       $info['time'] = time();
       $info['servername'] = SERVER_NAME;
       $info['warning'] = "Do not rely on this ws without asking to the dev mailing list";
+      
+      $versiontxt = "";
+      $fd = fopen("../../version.txt", 'r', True);
+      while ($line = fgetss($fd)) {
+        if (strlen(trim($line)) > 0) $versiontxt .= $line;
+      }
+      $versionarray = split("\n", $versiontxt);
+      $info['version'] = $versionarray[0];
+      $info['branch'] = $versionarray[1];
+
       return $info;
     }
 
