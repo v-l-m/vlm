375681ad41bebde773d436515879f22da0a565ef
paparazzia <paparazzia@gmail.com>
2010-11-13 19:27:20 +0000

    * evite une redirection vers le serveur s11 ou s10 dans l'url a cause de lb (see #425)

--- site/admin/htmlstart.php
+++ site/admin/htmlstart.php
@@ -18,15 +18,6 @@
 	<title><?php echo $PAGETITLE; ?></title>
     <link rel="stylesheet" type="text/css" href="../style/admin.css" />
     <link rel="icon" type="image/png" href="../images/site/favicon.png" />
-   	<!-- tinyMCE 
-		<script language="javascript" type="text/javascript" src="../scripts/tinymce.js"></script>
-		<script language="javascript" type="text/javascript">
-   		tinyMCE.init({
-      		mode : "specific_textareas",
-      		auto_reset_designmode : true
-   		});
-		</script>
-		/tinyMCE -->
     <script type="text/javascript" src="../<?php echo DIRECTORY_JSCALENDAR; ?>/calendar.js"></script>
     <script type="text/javascript" src="../<?php echo DIRECTORY_JSCALENDAR; ?>/lang/calendar-en.js"></script>
     <script type="text/javascript" src="../<?php echo DIRECTORY_JSCALENDAR; ?>/calendar-setup.js"></script>
@@ -39,7 +30,7 @@
 <?php
     echo $PAGETITLE;
     if ($_SERVER["PHP_SELF"] != "/admin/" and $_SERVER["PHP_SELF"] != "/admin/index.php") {
-        echo "&nbsp;(<a href=\"/admin\">Menu</a>)";
+        echo "&nbsp;(<a href=\"/admin/index.php\">Menu</a>)";
     }
 
 ?>
