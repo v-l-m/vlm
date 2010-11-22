f01de2baefcf21f069937ac4ee8951c34beb6645
Yves Lafon <yves@w3.org>
2010-11-22 11:12:23 +0000

    mixing languages is a bad thing [See #500]

--- moteur/dboptimize.php
+++ moteur/dboptimize.php
@@ -14,21 +14,21 @@ $verbose=0;
 
 $engine_start=microtime(true);
 
-echo "\n1- === OPTIMIZE T\n";
+echo "\n1- === OPTIMIZE TABLES ===\n";
 // we avoid coastline_* and histpos
-$alltables = { "admin_changelog", "auto_pilot", "flags", "modules_status",
-	       "players", "players_pending", "playerstousers",
-	       "positions", "races", "races_instructions", "races_ranking",
-	       "races_results", "races_waypoints", "racesmap", "updates",
-	       "user_action", "user_prefs", "users", 
-	       "waypoints", "waypoints_crossing" };
+$alltables = array("admin_changelog", "auto_pilot", "flags", "modules_status",
+		   "players", "players_pending", "playerstousers",
+		   "positions", "races", "races_instructions", "races_ranking",
+		   "races_results", "races_waypoints", "racesmap", "updates",
+		   "user_action", "user_prefs", "users", 
+		   "waypoints", "waypoints_crossing");
 
 foreach ($alltables as $table) {
   $opt_start = microtime(true); 
   $cmd    = "OPTIMIZE TABLE $table";
   $result = wrapper_mysql_db_query_writer($cmd);
   $opt_end   = microtime(true);
-  echo "Optimized table $table in ".($opt_end - $opt_start)"s\n";
+  echo "Optimized table $table in ".($opt_end - $opt_start)."s\n";
 }
 $engine_end=microtime(1);
 echo "\n  TIMINGS: total execution time - ".($engine_end-$engine_start).
