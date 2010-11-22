a796d59b709aa3950398678824ce918e40e8b8ee
kernel_panic <kernel_panic@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-11-22 23:01:48 +0000

    Adding Spectator Mode

---
+++ trunk/site/guest_map/index.php
@@ -0,0 +1,39 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>VLM - Mode Spectateur</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta http-equiv="Cache-Control" content="public"/>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="js/config.js" type="text/javascript"></script>
+<script src="js/guest_map.js" type="text/javascript"></script>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body onload="start();">
+
+<table bgcolor="#cccccc" width="1015">
+<tr><td colspan="2" bgcolor="#ffffff">
+<img src="http://www.virtual-loup-de-mer.org/images/site/banniere_haut.jpg"><br/>
+<div id="titre_carte"></div>
+
+<!-- <input type="button" name="test" value="test" class="txt1" onclick="refresh_all();" /> -->
+
+</td></tr>
+<tr>
+<td valign="top">
+
+<!-- Tableau RANKING -->
+<div id="tab_ranking"></div>
+</td>
+<td valign="top" class="txt1">
+
+<!-- CARTE ou LISTE DE COURSES-->
+<div id="tab_listrace"></div>
+<div id="map_canvas"></div>
+
+</td>
+</tr>
+</table>
+<div id="test"></div>
+</body>
+</html>
