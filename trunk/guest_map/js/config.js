cc0ae4af5d187e23636e1366ceff5e17aa71ab3b
kernel_panic <kernel_panic@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-11-24 10:44:38 +0000

    - track when getting only one boat = 12 hours (better getting 32 boats tracks of 2 hours in one shoot !! )

--- trunk/guest_map/js/config.js
+++ trunk/guest_map/js/config.js
@@ -30,13 +30,10 @@ var dyear = today.getFullYear();
 
 current_date = dday + "/" + dmonth + "/" + dyear + " " + hrs + ":" + mns + ":" + secs;
 
-
-//now()-7200
 cur_tsp = Math.ceil(new Date().getTime()/1000);
-// 2 heures
-//starttime = cur_tsp-7200;
-// 8 heures
-starttime = cur_tsp-86400;
+
+// 12 heures
+starttime = cur_tsp-43200;
 endtime = cur_tsp;
 
 user_pass_ajax = "username=la.playa@free.fr&password=la.playa";
