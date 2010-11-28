736b9227eef73d3c6c291a9b068035b16937fbba
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-11-28 16:42:42 +0000

    small adjustements for making guest_map usable in dev env

--- trunk/guest_map/js/config.js
+++ trunk/guest_map/js/config.js
@@ -1,22 +1,31 @@
 // CONFIGURATION FILE FOR SPECTATOR MODE
 
 webhost = window.location.hostname;
+baseurl = "http://virtual-loup-de-mer.org";
+
 if (webhost.match("virtual-loup-de-mer.org"))
 {
-// virtual-loup-de-mer.org
-gmap_key = "ABQIAAAAU9L35N6EdAtdkT4Cb2abDRR9fpxOiyHPEX_8YzC8CNXvq83W-hRDmTj4GD1F8DLKiaJ97BAfcB5i7w";
+    // virtual-loup-de-mer.org
+    baseurl = "";
+    gmap_key = "ABQIAAAAU9L35N6EdAtdkT4Cb2abDRR9fpxOiyHPEX_8YzC8CNXvq83W-hRDmTj4GD1F8DLKiaJ97BAfcB5i7w";
+}
+
+    if (webhost.match("paparazzia.info"))
+    {
+    baseurl = "";
+gmap_key = "ABQIAAAAt-TNu1jygAUkY20DVJC9EBQzFwCcMhfsDTv_S48r6nw_wx5DpRRymYw5m7qN4i_Kpwt-w5ZRvgYCQA";
 }
 
 if (webhost.match("caraibes.hd.free.fr"))
 {
-// caraibes.hd.free.fr
-gmap_key = "ABQIAAAAok_IBFtVMWx0xZX21zbpJRQMI8aBYvnUh4A1aH4V8c-4mNhOzxRN6Ev5QoDS02IpbpY4DsfPaT4QmQ";
+    // caraibes.hd.free.fr
+    gmap_key = "ABQIAAAAok_IBFtVMWx0xZX21zbpJRQMI8aBYvnUh4A1aH4V8c-4mNhOzxRN6Ev5QoDS02IpbpY4DsfPaT4QmQ";
 }
 
 if (webhost.match("zigszags.com"))
 {
-// zigszags.com
-gmap_key = "ABQIAAAAok_IBFtVMWx0xZX21zbpJRTlhEcjKhu1zbEPvkpXJWixh659yBThzICEnW2yhGGYfsPoLArh73nKaA";
+    // zigszags.com
+    gmap_key = "ABQIAAAAok_IBFtVMWx0xZX21zbpJRTlhEcjKhu1zbEPvkpXJWixh659yBThzICEnW2yhGGYfsPoLArh73nKaA";
 }
 
 today = new Date();
