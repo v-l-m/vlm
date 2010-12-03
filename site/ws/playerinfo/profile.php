027ee6332e0f66b3b8c35851737361566953eb65
paparazzia <paparazzia@gmail.com>
2010-12-03 08:25:36 +0000

    fix #516

--- site/ws/playerinfo/profile.php
+++ site/ws/playerinfo/profile.php
@@ -10,6 +10,7 @@ if (is_null($player = getPlayerObject($idp))) $ws->reply_with_error('PLAYER02');
 $ws->answer['profile'] = Array();
 $ws->answer['profile']['idp'] = intval($player->idplayers);
 $ws->answer['profile']['playername'] = $player->playername;
+$ws->answer['profile']['admin'] = $player->isAdmin();
 
 $ws->reply_with_success();
 
