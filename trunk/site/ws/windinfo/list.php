13739e1e6a73653ad996cb8b3757631afabbffa4
spf <spf@9fd53e09-640f-4c6e-8556-f39f7746c792>
2010-12-19 22:28:24 +0000

    usual typos [See #525]

--- trunk/site/ws/windinfo/list.php
+++ trunk/site/ws/windinfo/list.php
@@ -3,20 +3,20 @@ include_once("config.php");
 include_once("vlmc.php");
 
 function get_grib_timestamp_array() {
-  $ts_polar = array();
+  $ts_array = array();
   
   $global_vlmc_context = new vlmc_context();
   global_vlmc_context_set($global_vlmc_context);
   shm_lock_sem_construct_grib(1);
   $nb_grib = get_prevision_count();
   for ($i=0; $i < $nb_grib; $i++) { 
-    array_push(ts_polar, get_prevision_time_index($i));
+    array_push($ts_array, get_prevision_time_index($i));
   }
   shm_unlock_sem_destroy_grib(1);
-  return $ts_polar;
+  return $ts_array;
 }
 
-$answer = gt_grib_timestamp_array();
+$answer = get_grib_timestamp_array();
 // check if we have a full grib or a 
 if (count($answer) > 10) {
   $cache = $answer[0] + 34200 - time(); /* grib offset + 9h30 */ 
