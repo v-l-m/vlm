bb05e12b43e18e2f52bb40843bc7793bd705bd6d
paparazzia <paparazzia@gmail.com>
2011-01-03 06:44:30 +0000

    correct handling of negatives xy index for tiles (asked by openlayers, thus we copy/cache them as well)

--- site/gshhstiles.php
+++ site/gshhstiles.php
@@ -6,21 +6,30 @@
     $tilez = intval(get_cgi_var('z', 0));
     $force = get_cgi_var('force', 'no');
 
-    if ( $tilez < 0 && $tilez > 20 ) die("Bad z index");
+    if ( $tilez < 0 || $tilez > 20) die("Bad z index");
     $sizetile = pow(2, $tilez);
-    $tilex = $tilex % $sizetile;
-    $tiley = $tiley % $sizetile;
-    if ($tilex < 0) $tilex += $sizetile;
-    if ($tiley < 0) $tiley += $sizetile;    
-  
+
     $original = sprintf("%s/%d/%d/%d.png", DIRECTORY_GSHHSTILES, $tilez, $tilex, $tiley);
     $originaldir = sprintf("%s/%d/%d", DIRECTORY_GSHHSTILES, $tilez, $tilex);
+
+    $tilex = $tilex % $sizetile;
+    $tiley = $tiley % $sizetile;
+    while ($tilex < 0) $tilex += $sizetile;
+    while ($tiley < 0) $tiley += $sizetile;    
+
+    while ($tilex >= $sizetile) $tilex -= $sizetile;
+    while ($tiley >= $sizetile) $tiley -= $sizetile;
+  
+    $regular = sprintf("%s/%d/%d/%d.png", DIRECTORY_GSHHSTILES, $tilez, $tilex, $tiley);
+    $regulardir = sprintf("%s/%d/%d", DIRECTORY_GSHHSTILES, $tilez, $tilex);
     
     // Création et mise en cache
     if ( ( ! file_exists($original) ) ||  ($force == 'yes') ) {
         if (!is_dir($originaldir)) mkdir($originaldir, 0777, True);
         if (defined("TILES_SOURCE_SERVER")) {
             copy(sprintf("%s/%s", TILES_SOURCE_SERVER, $original), $original);
+        } else if (file_exists($regular) ) {
+            copy($regular, $original);
         } else {
             $rivers = "";
             if ($tilez > 4) $rivers = sprintf("--rivers %s ", GSHHS_CLIPPED_RIVER_FILENAME);
