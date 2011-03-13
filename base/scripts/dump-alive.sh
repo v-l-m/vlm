cb4c733070136e6eb3c8a783f66cfa4f1265a4c3
Yves Lafon <yves@w3.org>
2011-03-13 13:44:48 +0000

    one pass

--- base/scripts/dump-alive.sh
+++ base/scripts/dump-alive.sh
@@ -7,12 +7,8 @@ dumpname="vlmdump-alive.sql"
 #dont dump : histpos modules_status
 TBNAMES="admin_changelog auto_pilot flags players players_pending playerstousers positions races races_instructions races_loch races_ranking races_results races_waypoints racesmap updates user_action user_prefs users waypoint_crossing waypoints"
 
-echo "Dumping living tables sql to $dumpname"
+echo "Dumping live sql tables (gzipped) to $dumpname.gz"
 
-mysqldump -h $DBSERVER -u $DBUSER --password=$DBPASSWORD --add-locks --add-drop-table --insert-ignore --no-create-db --disable-keys $DBNAME $TBNAMES > $dumpname
-
-echo "Zipping file to $dumpname.gz"
-
-gzip $dumpname
+mysqldump -h $DBSERVER -u $DBUSER --password=$DBPASSWORD --add-locks --add-drop-table --insert-ignore --no-create-db --disable-keys $DBNAME $TBNAMES | gzip -9 -c > $dumpname.gz
 
 echo "OK"
