a01c5cf753a6cae32af5e90e92d1d1a40e816d88
paparazzia <paparazzia@gmail.com>
2011-04-09 11:37:26 +0000

    #534

--- site/maintenance.php
+++ site/maintenance.php
@@ -1,26 +1,18 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-		"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Virtual-loup-de-mer - Maintenance page - Work in progress</title>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
+ 
+ <head> 
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+   <title>Virtual-loup-de-mer - Maintenance page - Work in progress</title>
    <link rel="stylesheet" type="text/css" href="style/default/style.css" /> 
-   <link rel="icon" type="image/png" href="images/site/favicon.png" /> 
 </head>
 <body>
-   <div id="baneer"> 
-       <div id="baneerleftbox"></div> 
-       <div id="baneercenterbox"></div> 
-       <div id="baneerrightbox"> 
-       Virtual Loup de Mer est h&eacute;berg&eacute; par       <a href="http://www.actilis.net/" target="_actilis"><img src="/images/site/actilis.png" alt="Actilis" /></a> 
-       </div> 
-   </div> 
   <div>
 <h1>Site Maintenance In Progress</h1>
-<p>We are currently updating this website. It will be available again shortly. ...</p>
+<p>We are currently updating VLM. It will be available again shortly. ...</p>
 <hr />
 <h1>Maintenance du site en cours</h1>
-<p>Nous sommes actuellement en train de mettre à jour VLM. Il sera de nouveau disponible dans quelques instants</p>
+<p>Nous sommes actuellement en train de mettre &agrave; jour VLM. Il sera de nouveau disponible dans quelques instants...</p>
 <hr />
 <h2>Message:</h2>
 <p><?php include("maintenance.txt");?></p>
