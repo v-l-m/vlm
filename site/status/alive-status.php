f218fe7a7f56bd4450f8cfb98a189f24844c3fe2
paparazzia <paparazzia@gmail.com>
2011-04-25 08:59:41 +0000

    see #247

--- site/status/alive-status.php
+++ site/status/alive-status.php
@@ -1,18 +1,8 @@
 <?php
   //smallest possible code
-  function die503($msg) {
-        header("HTTP/1.1 503 Service Temporarily Unavailable");
-        header("Status: 503 Service Temporarily Unavailable");
-        die($msg);
-  }
-  function define_if_not($k, $v) {
-      if (!defined($k)) define($k, $v);
-  }
 
-  @include_once('param.php');
-  $link = @mysql_connect(DBSLAVESERVER, DBSLAVEUSER, DBSLAVEPASSWORD) or die503("Could not connect : " . mysql_error());
-  @mysql_select_db(DBNAME, $link) or die503("Could not select VLM database");
-  @mysql_query("SELECT count(*) from players", $link) or die503("KO");   //smallest table is players
-  @mysql_close($link);
+  @include_once('config-mysql.php');
+
+  @mysql_query("SELECT count(*) from players", $GLOBALS['slavedblink']) or die503("KO");   //smallest table is players
   echo "OK";
 ?>
