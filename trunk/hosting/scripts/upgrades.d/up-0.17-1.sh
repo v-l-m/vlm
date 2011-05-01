7fe375befcf9398410a8e813a84de6c8fa183619
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2011-05-01 08:20:53 +0000

    update UPGRADE NOTES (fix #587)

--- trunk/hosting/scripts/upgrades.d/up-0.17-1.sh
+++ trunk/hosting/scripts/upgrades.d/up-0.17-1.sh
@@ -4,7 +4,7 @@
 $VLMRACINE/scripts/maintenance.sh
 
 #base
-$VLMJEUROOT/base/scripts/dump-alive.sh $VLMTEMP/vlmdump-alive.sql
+$VLMJEUROOT/base/scripts/dump-alive.sh $VLMTEMP/vlmdump-alive.sql 
 $VLMJEUROOT/base/scripts/runupgrade.sh $VLMRELEASE
 
 $VLMRACINE/scripts/maj_module.sh moteur
