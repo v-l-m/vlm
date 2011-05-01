dd21251ce4e34b4bbb8495782a3b308f1e8cc84d
paparazzia <paparazzia@gmail.com>
2011-05-01 07:45:44 +0000

    see #587 (typos)

--- hosting/scripts/upgrade.sh
+++ hosting/scripts/upgrade.sh
@@ -6,7 +6,7 @@ if test "$1" = ""; then
   exit 0
 fi
 
-echo -n "Looking for $VLMRACINE/conf/conf_script"
+echo -n ">> Looking for $VLMRACINE/conf/conf_script"
 source $VLMRACINE/conf/conf_script || exit 1
 echo " : OK"
 
@@ -17,22 +17,23 @@ lockfile=$VLMTEMP/vlmup.$VLMRELEASE.$step.lock
 logfile=$VLMTEMP/vlmup.$VLMRELEASE.$step.log
 
 if test "$1" = "0"; then
-    echo "Step 0 - Upgrading scripts"
+    echo ">> Step 0 - Upgrading scripts"
     $VLMRACINE/scripts/maj_scripts.sh
 fi
 
 if [ -f $lockfile ]; then
-    echo "LOCKFILE $lockfile found. Back to manual upgrade !" 1>&2
+    echo ">> LOCKFILE $lockfile found. Back to manual upgrade !" 1>&2
     exit 0
 fi
 
 if [ -f $upfile ]; then
     touch $lockfile
-    echo "Running upgrade script $upfile"
+    echo ">> Running upgrade script $upfile"
     bash  $upfile > $logfile 
 else
     echo "Did not find $upfile" 1>&2
 fi
 
-echo "End Step $step for $VLMRELEASE, grepping logfile"
-grep _i "error" $logfile
+echo ">> Grepping logfile"
+grep -i "error" $logfile|grep -v "^A"
+echo ">> End of step $step for $VLMRELEASE"
