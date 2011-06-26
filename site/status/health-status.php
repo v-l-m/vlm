7b95bdc6f269c3f616f25857f5bfde1f584a3264
paparazzia <paparazzia@gmail.com>
2011-06-26 06:06:52 +0000

    * better status (see #614)

--- site/status/health-status.php
+++ site/status/health-status.php
@@ -4,21 +4,29 @@
   @require_once('griblib.php');
   @require_once('functions.php');
 
+  $now = time();
   $row = getLastUpdateRow();
   $lastupdate = $row2['time'];
+  $duration = max($row2['duration'],0.001);
+  $minmax = get_grib_minmax_time();
+  $min = minmax['min']
+
+  $msg  = "\n";
+  $msg .= "NOW: $now\n";
+  $msg .= "Last update: $lastupdate\n";
+  $msg .= "Time from last update: ".($now-$lastupdate)."\n";
+  $msg .= "Last engine duration: $duration\n";
+  $msg .= "Time from last weather forecast : ".($now - $min);
+
 
   #Are we late for running engine ?
-  if ((time() - $lastupdate) > 2*DELAYBETWEENUPDATE ) die503("KO");
+  if (($now - $lastupdate) > 2*DELAYBETWEENUPDATE ) die503("KO$msg");
 
   #Are we slow ?
-  $duration = max($row2['duration'],0.001);
-  if ($duration > UPDATEDURATION) die503("KO");
+  if ($duration > UPDATEDURATION) die503("KO$msg");
   
   #Are we late for grib refresh
-  $minmax = get_grib_minmax_time();
-  if ( (time() - $minmax['min']) > 10*3600 ) die503("KO");
+  if ( ($now - $min) > 10*3600 ) die503("KO$msg");
   
   echo "OK";
-  #debug
-  #echo "\n".(time()-$lastupdate)."\n".($duration)."\n".(time() - $minmax['min']);
 ?>
