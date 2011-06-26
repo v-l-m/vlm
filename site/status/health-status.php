f9f4369f3a96c31edbd785195a0ff0c61682ab61
paparazzia <paparazzia@gmail.com>
2011-06-26 05:50:31 +0000

    *typo (see #614)

--- site/status/health-status.php
+++ site/status/health-status.php
@@ -14,7 +14,7 @@
   if ($duration > UPDATEDURATION) die503("KO");
   
   #Are we late for grib refresh
-  $minmax = get_grib_minmax_time()
+  $minmax = get_grib_minmax_time();
   if ( (time() - $minmax['min']) > 10*3600 ) die503("KO");
   
   echo "OK";
