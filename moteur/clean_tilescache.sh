e589b1425485e5325ee64aba5a6c2ab620d9e729
Yves Lafon <yves@w3.org>
2011-07-16 21:06:25 +0000

    go around the limit of hard links for one file... Fixes #624

--- moteur/clean_tilescache.sh
+++ moteur/clean_tilescache.sh
@@ -9,8 +9,8 @@ FIXEDTILESDIR=f
 CHECKFILE=.lastcheck
 
 if [ ! -d ${TILESCACHEDIR} ]; then
-	echo "You must configure TILESCACHEDIR in clean_tilescache.sh" >&2
-	exit 2
+    echo "You must configure TILESCACHEDIR in clean_tilescache.sh" >&2
+    exit 2
 fi
 
 cd ${TILESCACHEDIR}
@@ -18,31 +18,44 @@ cd ${TILESCACHEDIR}
 emptysum="40d1d7d1e1b9bcaae0d69a5900dabf85"
 
 sea=0
+nblinks=0
 if [ ! -d f ]; then
-	mkdir ${FIXEDTILESDIR}
-	echo "Creating fixed dir"
+    mkdir ${FIXEDTILESDIR}
+    echo "Creating fixed dir"
 else
-	if [ -f ${FIXEDTILESDIR}/${SEAFILE} ]; then
-		sea=1
-	fi
+    if [ -f ${FIXEDTILESDIR}/${SEAFILE} ]; then
+	sea=1
+	nblinks=`ls -l ${FIXEDTILESDIR}/${SEAFILE} | awk '{ print $2}'`
+    fi
 fi
 
 findmod=""
 if [ -f ${CHECKFILE} ]; then
-	findmod="-newer ${CHECKFILE}"
+    findmod="-newer ${CHECKFILE}"
 fi
 
 find . -type f ${findmod} -print | grep -v ${SEAFILE} | while read tilename ; do
-	echo "Checking $tilename"
-        echo "$emptysum  $tilename" | md5sum -c --status
-        if [ $? -eq 0 ]; then
-        	echo "Is empty!"
-                if [ "$sea" -eq "0" ]; then
-                	mv $tilename ${FIXEDTILESDIR}/${SEAFILE}
-                        sea=1
-                fi
-                ln -f ${FIXEDTILESDIR}/${SEAFILE} $tilename
+    echo "Checking $tilename"
+    echo "$emptysum  $tilename" | md5sum -c --status
+    if [ $? -eq 0 ]; then
+        echo "Is empty!"
+        if [ "$sea" -eq "0" ]; then
+            mv $tilename ${FIXEDTILESDIR}/${SEAFILE}
+            sea=1
         fi
+	if [ $nblinks -lt 65000 ]; then
+	    nblinks=`expr $nblinks + 1`
+            ln -f ${FIXEDTILESDIR}/${SEAFILE} $tilename
+	else
+	    tstamp=`date +"%s"`
+	    mv ${FIXEDTILESDIR}/${SEAFILE} ${FIXEDTILESDIR}/${tstamp}-${SEAFILE}
+	    cp ${FIXEDTILESDIR}/${tstamp}-${SEAFILE} ${FIXEDTILESDIR}/${SEAFILE}
+	    nblinks=1
+	    echo "limit reached, storing ${SEAFILE} as ${tstamp}-${SEAFILE}"
+	fi
+	ln -f ${FIXEDTILESDIR}/${SEAFILE} $tilename
+	echo "$nblinks hard links on ${SEAFILE}"
+    fi
 done
 
 touch ${CHECKFILE}
