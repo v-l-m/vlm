d2943e4e86bffbefad0c15208182da50f8b6b301
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2011-11-20 11:03:30 +0000

    see #634

---
+++ branches/V_0_18/site/ws/raceinfo/reals.php
@@ -0,0 +1,36 @@
+<?
+    include_once("config.php");
+    include_once("wslib.php");
+
+    header("content-type: text/plain; charset=UTF-8");
+
+    //FIXME : types are badly checked
+    $ws = new WSBaseRace();
+    $now = time();
+    
+    $ws->require_idr();
+    $races = new races($ws->idr);
+    
+    $query_positions = "SELECT `time`, `idusers`, `lat`, `long` FROM `positions`" .
+      " WHERE `race` = "  . $races->idraces .
+      " AND `time` > ".($now - 24*3600) .
+      " AND `idusers` < 0" .
+      " ORDER BY `time` desc, `idusers`";
+
+    $res = $ws->queryRead($query_positions);
+
+    $ws->answer['request'] = array('idr' => $ws->idr, 'time_request' => $now);
+    $ws->answer['realpositions'] = array();
+
+    $not_started = array();
+
+    while ($row = mysql_fetch_assoc($res)) {
+        if (!isset($ws->answer['realpositions'][$row['idusers']])) $ws->answer['realpositions'][$row['idusers']] = Array();
+        $ws->answer['realpositions'][$row['idusers']][] = Array($row['time'], $row['lat'], $row['long']);
+    }
+
+    $ws->answer['nb_boats']  = $ws->answer['realpositions'];
+
+    $ws->reply_with_success();
+
+?>
