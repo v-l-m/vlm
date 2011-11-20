b175d04b31e15f99550f7f054e9d6b2debe3f295
paparazzia <paparazzia@gmail.com>
2011-11-20 09:59:57 +0000

    * REPLACE or INSERT (see #634)

--- moteur/posscripts/getpos_py_wrapper.php
+++ moteur/posscripts/getpos_py_wrapper.php
@@ -32,7 +32,7 @@
             //echo "$query\n";
 
 
-            $query ="insert into positions values ";
+            $query ="REPLACE INTO `positions` VALUES ";
             $query .= "( $time , $lonb*1000, $latb*1000, $idusers, $race ) ;";
 
             mysql_query($query) or die("KAWA : Query failed : " . mysql_error()." ".$query);
