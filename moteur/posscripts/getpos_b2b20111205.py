5758552d637413902782fc6c68d1717ca5896b82
paparazzia <paparazzia@gmail.com>
2011-12-01 20:39:38 +0000

    ready for the start (see #643)

--- moteur/posscripts/getpos_b2b20111205.py
+++ moteur/posscripts/getpos_b2b20111205.py
@@ -13,50 +13,39 @@ def unzip_file_into_dir(file, dir):
         outfile.write(zfobj.read(name))
         outfile.close()
 
-
+#get file
 urllib.urlretrieve("http://transatbtob-imoca.geovoile.org/2011/data/reports/leg1.gvz", "b2b.tmp.glz")
 unzip_file_into_dir('b2b.tmp.glz', '.')
 
-#<factors coord="1000" speed="10" distance="10" timecode="1" coef="1000"/>
-#  Indique les facteurs de conversions des coordonnées
-#<boat id="26" name="CAMPER Emirates Team NZ" sail="4" nbhulls="1" hullcolor="FFFFFF" trackcolor="FFFFFF" coef="1000">
-#<virtualboat id="126" classid="1" boatid="26"/>
-# correspondance id technique <=> numéro de dossard
-
+#parse
 tree = ElementTree.parse('b2b.tmp.gl')
+
+#date start : FIXME : timezero is currently on 7/12/2011 ???
+timezero = int(tree.getroot().attrib['timezero'])
+
+#matching realid and virtual id
+kboats = {6 : -1231, 41 : -1232, 331 : -1232, 337 : -1233, 341 : -1234, 343 : -1235, 347 : -1236, 349 : -1237}
+
+#array of boats
 boats = {}
-for outline in tree.findall(".//boat"):
+
+#base informations
+for outline in tree.findall("./boats/boat"):
   boats[int(outline.attrib['id'])] = outline.attrib
-  print outline.attrib
+  boats[int(outline.attrib['id'])]['name'] = outline.find('name').text.encode('utf-8')
+  #print boats[int(outline.attrib['id'])]
 
-sys.exit()
-page = urlopen("http://volvooceanrace.geovoile.com/2011/shared/event/update.xml")
-#Un peu de doc
-#<reports><report  id="208" date="2011/11/27 01:02:11Z"><v i="121" st="" d="5993" l="5993" s="211" c="93" o="-431"/>...
-#id = id du report
-#date = date de la publication
-#i = id (dossard+100)
-#st = status (DNF, ARV, STA (at start), "" => en course)
-#d = distance from finish (en 10ème de miles)
-#l = difference of distance to finish from nearest best opponent (en 10ème de miles)
-#s = speeed (10ème de noeuds)
-#c = cap / heading
-#o = offset de temps entre la date de publication et la mesure en secondes
-data = page.readlines()
-data = ' '.join(data)
+#location 
+for outline in tree.findall(".//location"):
+  id = int(outline.attrib['boatid'])
+  lat, lon, t = outline.text.split(',')
+  boats[id]['lat'] = float(lat)/1000.
+  boats[id]['lon'] = float(lon)/1000.
+  boats[id]['t'] = int(t) + timezero
+  boats[id]['vid'] = kboats[id]
 
-tree = ElementTree.fromstring(data)
-for outline in tree.findall(".//track"):
-  l = outline.text.split(';')
-  lat, lon, t = 0, 0, 1320498000
-  rid = int(outline.attrib['id'])
-  id = -1200-rid
-  for i in l:
-    tup = i.split(',')
-    lat += int(tup[0])
-    lon += int(tup[1])
-    t += int(tup[2])
-  if time.time() - t < 48*3600:
+  #print boats[id]
+  if time.time() - boats[id]['t'] < 48*3600: #FIXME : HARDCODED
     #20091108|1|1257681600|-729|BT|Sébastien Josse - Jean François Cuzon|50.016000|-1.891500|85.252725|4651.600000
-    print "20111105|0|%d|%d|%s|BAR|%f|%f|0.|0." % (int(t), id, boats[rid]['name'].encode('utf-8'), lat/1000., lon/1000.)
+    print "20111105|0|%d|%d|%s|BAR|%f|%f|0.|0." % (boats[id]['t'], boats[id]['vid'], boats[id]['name'], boats[id]['lat'], boats[id]['lon'])
 
