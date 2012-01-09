99f1cbce4ec4f69baabe6fb571ca3351b2c99bdd
paparazzia <paparazzia@gmail.com>
2012-01-09 20:48:26 +0000

    for safety reason

--- hosting/scripts/upgrades.d/up-0.19-0.sh
+++ hosting/scripts/upgrades.d/up-0.19-0.sh
@@ -2,5 +2,7 @@
 
 #do what can be done before maintenance mode
 $VLMRACINE/scripts/maj_module.sh base
+$VLMJEUROOT/base/scripts/dump-alive.sh $VLMTEMP/vlmdump-alive.pre.sql
+$VLMJEUROOT/base/scripts/dump-history.sh $VLMTEMP/vlmdump-history.pre.sql
 
 
