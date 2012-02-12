dbee69f97ca0c492c45927c8950e0167c5b1093c
paparazzia <paparazzia@gmail.com>
2012-02-12 21:37:37 +0000

    see #349 : Incoming races feeder

--- moteur/feed/feed.sh
+++ moteur/feed/feed.sh
@@ -3,11 +3,9 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-LOG=$VLMLOG/$(date +%Y%m%d_%H%M)-$1-vlmnotify.log
+LOG=$VLMLOG/$(date +%Y%m%d_%H%M)-vlmfeed.log
 
 #export VLMPHPPATH="/usr/bin/php --define extension=vlmc.so --define include_path=.:/usr/share/php:/home/vlmtest/svn/trunk/lib/phpcommon"
 
-media=$1
-
-$VLMPHPPATH $media.php >> $LOG 2>&1
+$VLMPHPPATH races.events.php $1 $2 >> $LOG 2>&1
 
