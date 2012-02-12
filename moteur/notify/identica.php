3fc3c04a38932bab6ee618393d8e16b1c9f725dc
paparazzia <paparazzia@gmail.com>
2012-02-12 13:16:53 +0000

    see #349

--- moteur/notify/identica.php
+++ moteur/notify/identica.php
@@ -1,5 +1,5 @@
 <?
-
+    require("config.php");
     require('notify.class.php');
     
     class VlmNotifyIdentica extends VlmNotifyCurl {
@@ -9,16 +9,14 @@
         function init_handle($message) {
             return $this->init_curl_handle_from_url(
                 VLM_NOTIFY_IDENTICA_URL,
-                Array("status" => $message),
+                Array("status" => $message['summary']),
                 Array(CURLOPT_USERPWD => VLM_NOTIFY_IDENTICA_USERPWD)
                 );
         }
     }
-    
+        
     $identica = new VlmNotifyIdentica();
     $identica->fetch();
-    //$messages = Array('test message 1', "test deuxième");
-    //$identica->messages = $messages;
     $identica->post();
     $identica->close();
 ?>
---
+++ moteur/notify/test.php
@@ -0,0 +1,19 @@
+<?
+    require("config.php");
+    require('notify.class.php');
+    
+    class VlmNotifyTest extends VlmNotify {
+        var $media = "twitter";
+        var $rate_limit = 2;
+    
+        function postone($message) {
+            print "ECHO : ${message['summary']}\n";
+            return True;
+        }
+    }
+        
+    $identica = new VlmNotifyTest();
+    $identica->fetch();
+    $identica->post();
+    $identica->close();
+?>
