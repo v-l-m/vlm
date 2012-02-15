f97b3f8fd1dc9cdc10f85f821f6113a78635af88
paparazzia <paparazzia@gmail.com>
2012-02-15 21:28:08 +0000

    fix news for incoming races (see #349)

--- moteur/feed/feed.sh
+++ moteur/feed/feed.sh
@@ -7,5 +7,5 @@ LOG=$VLMLOG/$(date +%Y%m%d_%H%M)-vlmfeed.log
 
 #export VLMPHPPATH="/usr/bin/php --define extension=vlmc.so --define include_path=.:/usr/share/php:/home/vlmtest/svn/trunk/lib/phpcommon"
 
-$VLMPHPPATH races.events.php $1 $2 >> $LOG 2>&1
+$VLMPHPPATH $VLMJEUROOT/moteur/feed/races.events.php $1 $2 >> $LOG 2>&1
 
