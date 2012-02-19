30f9df60f10dc55efb658a41f00416f1e996603a
paparazzia <paparazzia@gmail.com>
2012-02-19 11:04:24 +0000

    see #677 : trying to fix arrival detection

--- moteur/feed/feedresults.sh
+++ moteur/feed/feedresults.sh
@@ -7,5 +7,8 @@ LOG=$VLMLOG/$(date +%Y%m%d_%H%M)-cronvlm-feedresults.log
 
 #export VLMPHPPATH="/usr/bin/php --define extension=vlmc.so --define include_path=.:/usr/share/php:/home/vlmtest/svn/trunk/lib/phpcommon"
 
+#we wait to be sure crank has been computed
+sleep 10;
+
 $VLMPHPPATH $VLMJEUROOT/moteur/feed/results.events.php >> $LOG 2>&1
 
