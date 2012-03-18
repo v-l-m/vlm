988671d2430f0f2445db0956ad4fea3b71dbfae0
paparazzia <paparazzia@gmail.com>
2012-03-18 07:12:43 +0000

    see #708

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -3,6 +3,6 @@
 source $VLMRACINE/conf/conf_script || exit 1
 
 # VOR 2011
-#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vor20120318.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vor20120318.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 # SDC 2011
 nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_sdc20120311.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
