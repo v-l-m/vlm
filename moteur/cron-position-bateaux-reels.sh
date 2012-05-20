b9e7a05d638044113169776c95610396d213f702
Yves Lafon <yves@w3.org>
2012-05-20 20:22:20 +0000

    backport from trunk

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -2,7 +2,7 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-# VOR 2011
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vor20120428.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-# SDC 2011
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_sdc20120311.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+# VOR 2012
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vor20120520.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+# SDC 2012
+#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_sdc20120311.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
