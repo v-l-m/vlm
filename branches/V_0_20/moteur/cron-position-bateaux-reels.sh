791a3b9640a248a684dd4180d7de2d8c7ac3c997
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2012-06-03 11:13:15 +0000

    typo

--- branches/V_0_20/moteur/cron-position-bateaux-reels.sh
+++ branches/V_0_20/moteur/cron-position-bateaux-reels.sh
@@ -5,4 +5,4 @@ source $VLMRACINE/conf/conf_script || exit 1
 # VOR 2012
 #nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vor20120520.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 # TSR 2012
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_tsr20120603.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_twostar20120603.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
