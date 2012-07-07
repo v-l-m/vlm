f57052d51e7050b8e67f2eb5f833febced0e4d6e
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2012-07-07 17:42:32 +0000

    vlm transat positions

--- trunk/moteur/cron-position-bateaux-reels.sh
+++ trunk/moteur/cron-position-bateaux-reels.sh
@@ -2,7 +2,7 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-# VOR 2012
-#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vor20120701.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+# VlmTransat 2012
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kor20120707.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 # SDC 2012
 ##nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_sdc20120311.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
--- trunk/moteur/posscripts/getpos_kor20120707.py
+++ trunk/moteur/posscripts/getpos_kor20120707.py
@@ -28,7 +28,11 @@ basefilename = "kor%d" % vlmidrace
 addvisotree = gp.AddvisoTree("http://krysoceanrace.addviso.com/data/positions.xml", basefilename)
 coordfactor = addvisotree.factors()
 
-timezero = 1341680400 #addvisotree.timezero() #FIXME 07/07/2012 - 17:00 GMT
+#Datafile use fr locale ! Stupid !
+import locale
+locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+
+timezero = addvisotree.timezero(3600) #GMT Offset 1341680400 #addvisotree.timezero() #FIXME 07/07/2012 - 17:00 GMT
 
 boats = addvisotree.boats()
 
@@ -39,14 +43,16 @@ for rid in boats.keys() :
     boats[rid]['vlmboatname'] = "%s - %s" % (rid[0:3].upper(), boats[rid]['name'])
     boats[rid]['vlmusername'] = "%s%s" % (vlmusernameprefix, rid[0:3].upper())
 
-#FIXME : Wait for first pollings before finishing code
-print boats
-sys.exit()
-
-for b in boats:
+for bid in boats:
       #20091108|1|1257681600|-729|BT|Sébastien Josse - Jean François Cuzon|50.016000|-1.891500|85.252725|4651.600000
-    
-    t += timezero
+    t = timezero
     if time.time() - t < 48*3600 and t < time.time():
-        print "%d|0|%d|%d|%s|BAR|%f|%f|0.|0." % (vlmidrace, t, -boats[rid]['vlmid'], boats[rid]['name'].encode('utf8'), lat/coordfactor, lon/coordfactor)
+        print "%d|0|%d|%d|%s|BAR|%f|%f|0.|0." % (vlmidrace, t, -boats[bid]['vlmid'], boats[bid]['name'].encode('utf8'), float(boats[bid]['lt'])/coordfactor, float(boats[bid]['ln'])/coordfactor)
+        
+#for bid in boats:
+#    print("INSERT INTO users SET idusers = %d, username = '%s', boatname = '%s', engaged = %d ;" % (
+#        -boats[bid]['vlmid'],
+#        boats[bid]['vlmusername'],
+#        boats[bid]['vlmboatname'],
+#        vlmidrace))
 
