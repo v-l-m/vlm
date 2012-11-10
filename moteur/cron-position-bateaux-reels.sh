379afa8033115ce8766dc254d145bb3339aaf710
paparazzia <paparazzia@gmail.com>
2012-11-10 21:40:04 +0000

    add to cron

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -2,7 +2,6 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-# VlmTransat 2012
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_kor20120707.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-#TFV 2012
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_tfv20120708.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+# Vdg 2012
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vdg20121110.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+
