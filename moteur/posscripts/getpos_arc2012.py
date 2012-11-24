f3e23bf1927aecdb51d9a7e5839103af9d4b1ff7
paparazzia <paparazzia@gmail.com>
2012-11-24 11:19:01 +0000

    see #732

--- moteur/posscripts/getpos_arc2012.py
+++ moteur/posscripts/getpos_arc2012.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#YellowBrick based http://yb.tl/links/twostar2012
+#YellowBrick based
 
-import urllib
 import sys, os
-import time, calendar
 import getposlib as gp
 import sys, time
-import pprint
+
+if len(sys.argv) > 1 :
+    cmd = sys.argv[1]
+else :
+    cmd = "positions"
+    
 
 vlmfirstid = 1600    
 vlmidrace = 20121125
@@ -20,20 +23,27 @@ ybtree = gp.YellowBrickTree("http://yb.tl/Flash/arc2012/TeamSetup/", basefilenam
 
 boats = ybtree.boats()
 
-ybtree = gp.YellowBrickTree("http://yb.tl/Flash/arc2012/AllPositions/", basefilename, "pos")
+if cmd == "insert" :
+    for rid in boats:
+        b = boats[rid]
+        vlmid = vlmfirstid + int(b['marker'])
+        vlmusername = "%s%d" % (vlmusernameprefix, int(b['marker']))
+        vlmboatname = "%03d - %s" % (int(b['marker']), b['name'].encode('utf8'))
+        color = b['trackC'].lower()
+        print("INSERT INTO `users` SET idusers = %d, username = '%s', boatname = '%s', engaged = %d, color = '%s';" % (-vlmid, vlmusername, vlmboatname, vlmidrace, color))
+else :
 
-tracks = ybtree.tracks()
+    ybtree = gp.YellowBrickTree("http://yb.tl/Flash/arc2012/AllPositions/", basefilename, "pos")
+    tracks = ybtree.tracks()
 
-for p in tracks :
-      t = p[1]
-      rid = p[0]
-      lat = p[2]
-      lon = p[3]
-      if time.time() - t < 12*3600:
-          #20091108|1|1257681600|-729|BT|Sébastien Josse - Jean François Cuzon|50.016000|-1.891500|85.252725|4651.600000
-          boat = boats[rid]
-          vlmid = vlmfirstid +rid
-          print("%d|0|%d|%d|%s|%s|%f|%f|0.|0." % (vlmidrace, t, -vlmid, boat['name'].encode('utf8'), boat['skipper']['name'].encode('utf8'), lat, lon))
-          #print time.gmtime(int(t))
-
+    for p in tracks :
+          t = p[1]
+          rid = p[0]
+          lat = p[2]
+          lon = p[3]
+          if time.time() - t < 12*3600:
+              #20091108|1|1257681600|-729|BT|Sébastien Josse - Jean François Cuzon|50.016000|-1.891500|85.252725|4651.600000
+              boat = boats[rid]
+              vlmid = vlmfirstid +int(boat['marker'])
+              print("%d|0|%d|%d|%s|%s|%f|%f|0.|0." % (vlmidrace, t, -vlmid, boat['name'].encode('utf8'), boat['skipper']['name'].encode('utf8'), lat, lon))
 
