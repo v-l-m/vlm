2f56efcd7e4f2c27a760fd4698581c7bcaca9553
mesosaure <mesosaure@gmail.com>
2012-12-16 10:28:32 +0000

    See #734 : fixes 2 bugs

--- base/scripts/upgrade-v0.22.sql
+++ base/scripts/upgrade-v0.22.sql
@@ -1,8 +1,12 @@
 -- Using cursor on UserTrophyTable make it more easy for maintenance...
 drop procedure `vlm`.`sptrophycontrolconnexion`;
-
+-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: comments before and after the routine body will not be stored by the server
+-- --------------------------------------------------------------------------------
 DELIMITER $$
-CREATE DEFINER=`vlm`@`%` PROCEDURE `sptrophycontrolconnexion`(par_race INT, par_dt date)
+
+CREATE DEFINER=`vlm`@`%` PROCEDURE `sptrophycontrolconnexion`(par_dt date)
 BEGIN
   DECLARE done INT DEFAULT 0;
   DECLARE race, idu, idut INT;
@@ -11,7 +15,7 @@ BEGIN
 
 DECLARE curTrophy CURSOR FOR SELECT UT.idraces, UT.idusers, UT.idUsersTrophies FROM users_Trophies UT 
 	right join users US on UT.idusers = US.idUsers
-	WHERE UT.RefTrophy=ID_TROPHY AND UT.idusers IS NOT NULL AND UT.quitdate IS NULL AND US.engaged=par_race;
+	WHERE UT.RefTrophy=ID_TROPHY AND UT.idusers IS NOT NULL AND UT.quitdate IS NULL AND US.engaged=UT.idraces; -- par_race;
 
 DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done = 1;
 
@@ -35,21 +39,14 @@ REPEAT
     IF NOT done THEN
 		SET condOAD = (SELECT TIME_TO_SEC(TIMEDIFF(MAX(user_action.time),MIN(user_action.time))) 
 		FROM user_action 
-		WHERE user_action.idraces=par_race AND user_action.ipaddr <>'127.0.0.1' AND DATE(user_action.time)=par_dt LIMIT 1);
-		IF condOAD IS NULL OR condOAD < 3600 THEN
-			insert into racetrophycontrol (controldate,idusertrophy,Score) SELECT par_dt, idut, condOAD;
+		WHERE user_action.idraces=race AND user_action.idusers=idu AND user_action.ipaddr <>'127.0.0.1' AND DATE(user_action.time)=par_dt LIMIT 1);
+		insert into racetrophycontrol (controldate,idusertrophy,Score) SELECT par_dt, idut, condOAD;
+		IF NOT (condOAD IS NULL OR condOAD < 3600) THEN
+			-- insert into racetrophycontrol (controldate,idusertrophy,Score) SELECT par_dt, idut, condOAD;
+			UPDATE users_Trophies SET quitdate = par_dt WHERE idUsersTrophies=idut; 
 		END IF;
     END IF;
 UNTIL done END REPEAT;
 CLOSE curTrophy;
  
--- Delete all users that are above the threashold
-UPDATE users_Trophies SET quitdate=par_dt 
-	WHERE idUsersTrophies 
-	IN (SELECT idusertrophy 
-		FROM racetrophycontrol R 
-		WHERE getTrophy(idusertrophy)=ID_TROPHY AND R.Score>=3600 AND R.controldate=par_dt);
-commit ;
-
 END
-
