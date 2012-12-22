e156a1817bc06aab38da708ddc1bc98a8a119e27
mesosaure <mesosaure@gmail.com>
2012-12-22 16:34:57 +0000

    See #734 : better with title

--- site/admin/boats_OAD.report.php
+++ site/admin/boats_OAD.report.php
@@ -2,7 +2,8 @@
     $PAGETITLE = "Boats engaged on Once a day trophy";
     include ("htmlstart.php");
     include_once ("functions.php");
-        
+	echo "<P>Engaged boats on trophy</P>";
+
     $query  = "SELECT U.controldate, U.score, UT.idraces, UT.idusers, US.username ";
 	$query  .= "FROM racetrophycontrol U ";
 	$query  .= "left join users_Trophies UT on U.idusertrophy = UT.idUsersTrophies ";
@@ -11,6 +12,7 @@
 	$query  .= "ORDER BY UT.idraces DESC, U.controldate DESC, UT.idUsers ASC; ";
     htmlQuery($query);
 
+	echo "<P>Shot boats on trophy</P>";
     $query  = "SELECT U.controldate, U.score, UT.idraces, UT.idusers, US.username ";
 	$query  .= "FROM racetrophycontrol U ";
 	$query  .= "left join users_Trophies UT on U.idusertrophy = UT.idUsersTrophies AND UT.quitdate = U.controldate ";
