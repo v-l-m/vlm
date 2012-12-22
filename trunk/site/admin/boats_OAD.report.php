3f2fe2de9a0a3f4e6eb7e80c1f2608b441216be9
mesosaure <mesosaure@9fd53e09-640f-4c6e-8556-f39f7746c792>
2012-12-22 16:28:08 +0000

    See #734 : decom boatname + new list for missed trophy pixels (proof)

--- trunk/site/admin/boats_OAD.report.php
+++ trunk/site/admin/boats_OAD.report.php
@@ -3,15 +3,21 @@
     include ("htmlstart.php");
     include_once ("functions.php");
         
-    $query  = "SELECT U.controldate, U.score, UT.idraces, UT.idusers, US.boatname, US.username ";
+    $query  = "SELECT U.controldate, U.score, UT.idraces, UT.idusers, US.username ";
 	$query  .= "FROM racetrophycontrol U ";
 	$query  .= "left join users_Trophies UT on U.idusertrophy = UT.idUsersTrophies ";
 	$query  .= "right join users US on UT.idusers = US.idUsers ";
 	$query  .= "WHERE UT.idusers IS NOT NULL AND UT.quitdate IS NULL AND UT.RefTrophy=2 ";
-	$query  .= "ORDER BY U.controldate DESC, UT.idUsers ASC, UT.RefTrophy ASC; ";
-	
-	
+	$query  .= "ORDER BY UT.idraces DESC, U.controldate DESC, UT.idUsers ASC; ";
     htmlQuery($query);
 
+    $query  = "SELECT U.controldate, U.score, UT.idraces, UT.idusers, US.username ";
+	$query  .= "FROM racetrophycontrol U ";
+	$query  .= "left join users_Trophies UT on U.idusertrophy = UT.idUsersTrophies AND UT.quitdate = U.controldate ";
+	$query  .= "right join users US on UT.idusers = US.idUsers ";
+	$query  .= "WHERE UT.idusers IS NOT NULL AND NOT UT.quitdate IS NULL AND UT.RefTrophy=2 ";
+	$query  .= "ORDER BY UT.idraces DESC, U.controldate DESC, UT.idUsers ASC; ";
+    htmlQuery($query);
+	
     include ("htmlend.php");
 ?>
