bc261902f8e9da0fab7dd17bc7c8d10ce9f80618
paparazzia <paparazzia@9fd53e09-640f-4c6e-8556-f39f7746c792>
2012-12-23 09:00:43 +0000

    backport from trunk (see #730)

--- branches/V_0_21/moteur/validation-WP/trophyperday.sh
+++ branches/V_0_21/moteur/validation-WP/trophyperday.sh
@@ -4,15 +4,11 @@ source $VLMRACINE/conf/conf_base || exit 1
 
 MYSQL="mysql -u $DBUSER -h $DBSERVER -p$DBPASSWORD $DBNAME"
 
-function lauchTrophyControl()
-{
-
-        # NOW=$(date +%s -u)
-        # let NOW+=300
+function launchTrophyControl {
+  # NOW=$(date +%s -u)
+  # let NOW+=300
 
  	$MYSQL -e "call $1(NULL);"
-
-	done 
 }
 
 
