437d77b0282f01758a1f0d814db1afbaaad61da2
paparazzia <paparazzia@gmail.com>
2012-12-28 18:10:31 +0000

    see #740

--- lib/phpcommon/races.class.php
+++ lib/phpcommon/races.class.php
@@ -79,8 +79,9 @@ class races {
   function setLastRun($time = null) {
       if(is_null($time)) $time = time();
       $this->lastrun = intval($time);
-      $query = "UPDATE `races` SET `lastrun` = ".$this->lastrun." WHERE `idraces` = ". $this->idraces;
-      wrapper_mysql_db_query_writer($query);    
+      $query = "UPDATE `races` SET `lastrun` = FROM_UNIXTIME(".$this->lastrun.") WHERE `idraces` = ". $this->idraces;
+      echo "$query";
+      wrapper_mysql_db_query_writer($query);
   }
   
   function getTimeToUpdate($time = null) {
