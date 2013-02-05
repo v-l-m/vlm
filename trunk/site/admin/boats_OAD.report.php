2c532c7884b3d9c4e79417babde6629834b93db9
mesosaure <mesosaure@9fd53e09-640f-4c6e-8556-f39f7746c792>
2013-02-05 13:24:45 +0000

    See #734 : fixes an issue with standard initialisation (0 vs NULL) of OAD administration GUI

--- trunk/site/admin/boats_OAD.report.php
+++ trunk/site/admin/boats_OAD.report.php
@@ -8,7 +8,7 @@
 	$query  .= "FROM racetrophycontrol U ";
 	$query  .= "left join users_Trophies UT on U.idusertrophy = UT.idUsersTrophies ";
 	$query  .= "right join users US on UT.idusers = US.idUsers ";
-	$query  .= "WHERE UT.idusers IS NOT NULL AND UT.quitdate IS NULL AND UT.RefTrophy=2 ";
+	$query  .= "WHERE UT.idusers IS NOT NULL AND (UT.quitdate IS NULL OR UT.quitdate=0) AND UT.RefTrophy=2 ";
 	$query  .= "ORDER BY UT.idraces DESC, U.controldate DESC, UT.idUsers ASC; ";
     htmlQuery($query);
 
@@ -17,7 +17,7 @@
 	$query  .= "FROM racetrophycontrol U ";
 	$query  .= "left join users_Trophies UT on U.idusertrophy = UT.idUsersTrophies AND UT.quitdate = U.controldate ";
 	$query  .= "right join users US on UT.idusers = US.idUsers ";
-	$query  .= "WHERE UT.idusers IS NOT NULL AND NOT UT.quitdate IS NULL AND UT.RefTrophy=2 ";
+	$query  .= "WHERE UT.idusers IS NOT NULL AND NOT (UT.quitdate IS NULL OR UT.quitdate=0) AND UT.RefTrophy=2 ";
 	$query  .= "ORDER BY UT.idraces DESC, U.controldate DESC, UT.idUsers ASC; ";
     htmlQuery($query);
 	
