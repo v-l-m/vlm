11ecf2181bb28086133640be2e1520d5ca12dc2b
paparazzia <paparazzia@gmail.com>
2014-04-16 03:28:56 +0000

    backport pos ag2R

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -2,6 +2,4 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-#Route de la decouverte
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_dec20121003.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_ag2r20140406.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
