bdf3b75a109a1512f8c2577589bb129059d829f1
sbs <sbs@9fd53e09-640f-4c6e-8556-f39f7746c792>
2014-11-11 08:32:41 +0000

    Add new cron shell for live, and split RDR get pos script or update and live

--- trunk/moteur/posscripts/getpos_rdr20141102.py
+++ trunk/moteur/posscripts/getpos_rdr20141102.py
@@ -41,13 +41,3 @@ for track in geotree.tracks(tagid='id'):
      t += timezero
      if time.time() - t < 48*3600 and t < time.time():
          print "%d|0|%d|%d|%s|BAR|%f|%f|0.|0." % (vlmidrace, t, -boats[rid]['vlmid'], boats[rid]['name'].encode('utf8'), lat/coordfactor, lon/coordfactor)
-
-geotree = gp.GeovoileTree("http://routedurhum.geovoile.com/2014/_elements/data/race/live.hwz?v=" + str(int(time.time())), basefilename+"update")
-
-for track_live in geotree.tracks_live(tagid='id'):
-      #20091108|1|1257681600|-729|BT|Sébastien Josse - Jean François Cuzon|50.016000|-1.891500|85.252725|4651.600000
-     rid, t, lat, lon = track_live
-     t += timezero
-     if time.time() - t < 48*3600 and t < time.time():
-         print "%d|0|%d|%d|%s|BAR|%f|%f|0.|0." % (vlmidrace, t, -boats[rid]['vlmid'], boats[rid]['name'].encode('utf8'), lat/coordfactor, lon/coordfactor)
-
