5f464ac81ed28a2ba4d209f9ba3cafbb9e1ae15e
sbs <sbsrouteur@users.noreply.github.com>
2014-11-11 08:32:41 +0000

    Add new cron shell for live, and split RDR get pos script or update and live

--- moteur/cron-position-bateaux-reels_live.sh
+++ moteur/cron-position-bateaux-reels_live.sh
@@ -5,5 +5,5 @@ source $VLMRACINE/conf/conf_script || exit 1
 #VOR 2014
 #nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 # RDR 2014
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_rdr20141102.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_rdr20141102_live.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
