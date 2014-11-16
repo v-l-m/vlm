605f20e3113f91eb1ea0f150c32915e69a59f63a
sbs <sbs@9fd53e09-640f-4c6e-8556-f39f7746c792>
2014-11-16 13:09:38 +0000

    Remove shell for live reels (not needed)

--- trunk/moteur/cron-position-bateaux-reels_live.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source $VLMRACINE/conf/conf_script || exit 1
-
-#VOR 2014
-#nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-# RDR 2014
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_rdr20141102_live.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-
