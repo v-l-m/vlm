6e66127f0f0b6be3bf17c2bbfaf4e7522b01eeaf
sbs <sbsrouteur@users.noreply.github.com>
2015-01-04 22:00:31 +0000

    FIX : 759 . Add code to interpolate segments at map border to avoid rounding error when using high map scale on very long segments.

--- lib/phpcommon/exclusionzone.class.php
+++ lib/phpcommon/exclusionzone.class.php
@@ -30,6 +30,12 @@ class exclusionZone
       $p13=array(26.315,56.795);			//	26º 18.900N 56º 47.700E
       $p14=array(26.606667,56.301667);		//	26º 36.400N 56º 18.100E
       $p15=array(26.71,56.558333);			//	26º 42.600N 56º 33.500E 
+      
+      // 60°S Exclusion Line
+      $p16=array(-62,-45);			
+      $p17=array(-58,45);			
+      
+      
 
 
       // Vor 2 Strait of hormuz obstruction
@@ -42,7 +48,7 @@ class exclusionZone
       $this->activeZoneName="VOR Leg 2";
       $this->Exclusions = array ( array($p1,$p2), array($p2,$p3),
                   array($p4,$p5), array($p5,$p6), array($p6,$p7),array($p7,$p8), array($p8,$p9), array($p9,$p10),
-                  array($p10,$p11), array($p11,$p12), array($p12,$p13),array($p13,$p14), array($p14,$p15)
+                  array($p10,$p11), array($p11,$p12), array($p12,$p13),array($p13,$p14), array($p14,$p15), array($p16,$p17)
                 );
     }
   }
