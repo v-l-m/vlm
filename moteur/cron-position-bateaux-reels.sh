04c06455c4c30fa633ee5c609df5573a3fe817e4
sbsrouteur <sbsrouteur@users.noreply.github.com>
2015-03-01 00:02:33 +0100

    Remove VOR4 from cron, and update script for Leg5.

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -3,7 +3,7 @@
 source $VLMRACINE/conf/conf_script || exit 1
 
 #VOR 2014
-nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+#nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 # RDR 2014
 nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_b2b20141231.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
