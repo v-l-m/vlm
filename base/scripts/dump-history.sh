e2d14b04f2a486819f9f5b9047ab000908d1559e
hb <paparazzia@gmail.com>
2015-03-01 09:51:53 +0100

    Do NOT dump histpos schema (see #770) - dump only records - histpos has to be created by hand on the target server

--- base/scripts/dump-history.sh
+++ base/scripts/dump-history.sh
@@ -12,6 +12,6 @@ TBNAMES="histpos"
 
 echo "Dumping big archive sql tables (gzipped) to $dumpname.gz"
 
-mysqldump -h $DBSERVER -u $DBUSER --password=$DBPASSWORD --add-locks --add-drop-table --insert-ignore --no-create-db --disable-keys $DBNAME $TBNAMES | gzip -9 -c > $dumpname.gz
+mysqldump -h $DBSERVER -u $DBUSER --password=$DBPASSWORD --add-locks --no-create-info --insert-ignore --no-create-db --disable-keys $DBNAME $TBNAMES | gzip -9 -c > $dumpname.gz
 
 echo "OK"
