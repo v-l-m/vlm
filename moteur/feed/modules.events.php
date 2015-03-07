737cfc7005666fee29a790df94021ae38aebc3cd
pzia <paparazzia@gmail.com>
2015-03-07 10:03:45 +0100

    fix #790

--- moteur/feed/modules.events.php
+++ moteur/feed/modules.events.php
@@ -12,27 +12,26 @@
     class NewModulesEvents extends baseClass {
         var $timedelta = 60;
         var $media = 'irc';
-        
+
         function __construct($media, $timedelta = 60) {
             $this->timedelta = intval($timedelta);
-            $this->media = $media
+            $this->media = $media;
         }
 
         function feed() {
             $query = "SELECT * FROM modules_status ".
-                     "AND ABS(updated - UNIX_TIMESTAMP(NOW()) ) < ".$this->timedelta." ";
+                     "WHERE ABS(UNIX_TIMESTAMP(updated) - UNIX_TIMESTAMP(NOW()) ) < ".$this->timedelta." ;";
+
             $res = $this->queryRead($query);
-            
+
             while ($line = mysql_fetch_assoc($res)) {
                 $this->feedone($line);
             }
         }
-        
+
         function feedone($line) {
-            $timedelay = $this->timetoevent; #$line['deptime'] - intval(time());
-            $timetarget = $line['deptime'] - $this->timetoevent;
-            $t = sprintf("Deploiement de %s (%s) sur %s", $line['moduleid'], $line['revid'], $line['serverid']);
-            $sql = sprintf("INSERT IGNORE INTO `news` SET media='%s', summary='%s', timetarget=%d ;", $this>media, $t, $timetarget);
+            $t = sprintf("Deploiement du module VLM %s (r%s) sur %s", $line['moduleid'], $line['revid'], $line['serverid']);
+            $sql = sprintf("INSERT IGNORE INTO `news` SET media='%s', summary='%s', timetarget=UNIX_TIMESTAMP(NOW()) ;", $this->media, $t);
             $this->queryWrite($sql);
             print $t."\n";
         }
@@ -42,11 +41,10 @@
     if ($_SERVER['argc'] != 3) die("Bad arguments");
     eval("\$media = \"".$_SERVER['argv'][1]."\";");
     eval("\$timedelta = intval(".$_SERVER['argv'][2].");");
-  
-    print "Updates for modules to $media\n";
 
-    
+    print "Updates for modules to $media ($timedelta)\n";
+
     $re = new NewModulesEvents($media, $timedelta);
     $re->feed();
-          
+
 ?>
