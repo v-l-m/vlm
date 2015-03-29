2dd11189d55d5d983bb89f386eaa0f3242edd416
pzia <paparazzia@gmail.com>
2015-03-29 13:36:17 +0200

    Prepare release
    * doc for chat configuration #793
    * script for 0.27

---
+++ hosting/scripts/upgrades.d/up-0.27-0.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#do what can be done before maintenance mode
+$VLMRACINE/scripts/maj_module.sh base #Do we need this for this release ?
+
---
+++ hosting/scripts/upgrades.d/up-0.27-1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#maintenance
+$VLMRACINE/scripts/maintenance.sh
+
+#base
+$VLMJEUROOT/base/scripts/dump-alive.sh $VLMTEMP/vlmdump-alive.sql 
+$VLMJEUROOT/base/scripts/runupgrade.sh $VLMRELEASE #do we need this for this release ?
+
+#core
+$VLMRACINE/scripts/maj_module.sh moteur
+$VLMRACINE/scripts/maj_module.sh lib/phpcommon
