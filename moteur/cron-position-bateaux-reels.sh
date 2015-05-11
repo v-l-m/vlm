0174c7f7f451ccd16d64fc84a1d16cb947fcc6f1
sbsrouteur <sbsrouteur@users.noreply.github.com>
2015-05-11 08:27:52 +0200

    Remove VOR update script until next leg.

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -3,7 +3,7 @@
 source $VLMRACINE/conf/conf_script || exit 1
 
 #VOR 2014
-nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+#nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
 # l'hermione 2015
 nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_hermione.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
