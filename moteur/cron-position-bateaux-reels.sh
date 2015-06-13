15ae90b0089406faa48bf744ae39cba19385faa9
sbsrouteur <sbsrouteur@users.noreply.github.com>
2015-06-13 17:14:27 +0200

    Update Pos Script for VOR9 and Hermione end of race

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -6,5 +6,5 @@ source $VLMRACINE/conf/conf_script || exit 1
 #nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
 # l'hermione 2015
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_hermione.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_hermione.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
