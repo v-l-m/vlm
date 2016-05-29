c264ef7266a1ba93da2c3b1727888e618f556244
sbsrouteur <sbsrouteur@users.noreply.github.com>
2016-05-29 23:20:53 +0200

    Add Script for NY Vendée race. Update cron for real accordingly.

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -5,10 +5,5 @@ source $VLMRACINE/conf/conf_script || exit 1
 #VOR 2014
 #nice -1 $VLMRACINE/vlmcode/moteur/posscripts/GetVOR2014|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
-# l'hermione 2015
-#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_hermione.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-
-# Minitransat
-#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_SpinDrift.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_IDEC.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-
+#New-York Vendée 2016
+#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_NewYorkVendee.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
