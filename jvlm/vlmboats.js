ad74d63d7185db6c95898fd767a7667bcc5e05e8
sbsrouteur <sbsrouteur@users.noreply.github.com>
2016-05-31 23:28:24 +0200

    Add OLPopup to boat position.

--- jvlm/vlmboats.js
+++ jvlm/vlmboats.js
@@ -85,6 +85,31 @@ function DrawBoat(Boat)
       )
     );
     VLMBoatsLayer.addFeatures(Boat.OLBoatFeatures[BOAT_ICON]);
+    //Add a selector control to the vectorLayer with popup functions
+    var controls = {
+      selector: new OpenLayers.Control.SelectFeature(VLMBoatsLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+    };
+
+    function createPopup(feature) {
+      feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+          feature.geometry.getBounds().getCenterLonLat(),
+          null,
+          '<div class="markerContent">'+"let's have fun"+'</div>',
+          null,
+          true,
+          function() { controls['selector'].unselectAll(); }
+      );
+      //feature.popup.closeOnMove = true;
+      map.addPopup(feature.popup);
+    }
+
+    function destroyPopup(feature) {
+      feature.popup.destroy();
+      feature.popup = null;
+    }
+    
+    map.addControl(controls['selector']);
+    controls['selector'].activate();
   }
   else
   {
