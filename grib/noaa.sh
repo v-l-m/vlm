97c038ec54f32451efc43f112e60498f28cb7481
sbsrouteur <sbsrouteur@free.fr>
2016-06-11 23:34:00 +0200

    Fix noaa.sh minsize test and retries.

--- grib/noaa.sh
+++ grib/noaa.sh
@@ -96,37 +96,29 @@ for TSTAMP in `echo $allindexes` ; do
       else
         actualsize=0
       fi
+
+      #Check for minimumsize
       if [ $actualsize -le $minimumsize ]; then
-          echo "grib file too small $actualsize < $minimumsize bytes" >>$LOG
-          rm -f $GRIBFILE
-          let retry=1
+        # file too small
+        echo "grib file too small $actualsize < $minimumsize bytes" >>$LOG
+        rm -f $GRIBFILE
+        let retry++
+      else
+        # file size looks good
+        echo  $DAT $GRIBFILE downloaded... >> $LOG 2>&1
+        cnvgrib -g21 $GRIBFILE $GRIBFILE.grib1
+        echo $GRIBFILE converted >> $LOG 2>&1
+        actualsize=$(wc -c "$GRIBFILE.grib1" | cut -f 1 -d ' ')
+        # if [ $actualsize -ne 844908 ]; then
+          # let retry++
+          # echo "Invalid grib file size, retry #" $retry  >> $LOG 2>&1
+          
+        # else
+          echo "Grib1 size ok. File complete"  >> $LOG 2>&1
+          let retry=0
+        # fi
       fi
-#    if [ $retry == 4 ]; then
-#     while [ $retry -gt 0 ]; do
-#       wget --waitretry 600 -nc -c ${NOAA_SERVICE_BACKUP_URI}/gfs.$DAT$HH/$GRIBFILE >>$LOG 2>&1
-#       let retry=$?
-#       if [ $retry -gt 0 ] ; then
-#         sleep 30
-#       else
-#	  let serv=${NOAA_SERVICE_BACKUP_URI}
-#          NOAA_SERVICE_BACKUP_URI=${NOAA_SERVICE_MAIN_URI}
-#          NOAA_SERVICE_MAIN_URI=$serv
-#        fi
-#      done
-#    fi
-      echo  $DAT $GRIBFILE downloaded... >> $LOG 2>&1
-      cnvgrib -g21 $GRIBFILE $GRIBFILE.grib1
-      echo $GRIBFILE converted >> $LOG 2>&1
-      actualsize=$(wc -c "$GRIBFILE.grib1" | cut -f 1 -d ' ')
-      # if [ $actualsize -ne 844908 ]; then
-        # let retry++
-        # echo "Invalid grib file size, retry #" $retry  >> $LOG 2>&1
-        
-      # else
-        echo "Grib1 size ok. File complete"  >> $LOG 2>&1
-        let retry=0
-      # fi
-      
+
       if [ $retry -eq 4 ]; then
         #Enough retries for this file pray for the best
         let retry=0
