9f6dfaaee8f155c555310f59a2077ba9b96a5922
sbsrouteur <sbsrouteur@free.fr>
2016-06-11 12:04:59 +0200

    Update config.js for code, remove bing APIs keys

--- jvlm/config.js
+++ jvlm/config.js
@@ -1,12 +1,13 @@
+/* global webhost */
 //webhost
 webhost = window.location.hostname;
 
 //Url for the cache
 //tilesUrlArray = "/cache/gshhstiles/${z}/${x}/${y}.png";
 //You may put an array here, example :
-tilesUrlArray = [ "http://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
+var tilesUrlArray = [ "http://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
 
-if (webhost.match("virtual-loup-de-mer.org"))
+/*if (webhost.match("virtual-loup-de-mer.org"))
 {
     // virtual-loup-de-mer.org
     bingApiKey = "AkBdZQ5s2ay67yXs9xruMVOPDAikz_CycgDXLoFwJGrfi2rjsGNq8ijo4DddYF7t";
@@ -16,5 +17,5 @@ if (webhost.match("v-l-m.org") || webhost.match("vlm-dev"))
 {
     // v-l-m.org
     bingApiKey = "AmcAzdaZ8u4cWpQo9qPF7_U4q-vW_u3bsprZ4rqu1kJppx_porBOvOELFXWZiHLE";
-}
+}*/
 
