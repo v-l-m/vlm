41ddd0217999c6e695dce32852f19f196c32c183
sbsrouteur <sbsrouteur@free.fr>
2016-06-11 12:17:19 +0200

    Definitely remove bing

--- jvlm/config.js
+++ jvlm/config.js
@@ -7,15 +7,5 @@ webhost = window.location.hostname;
 //You may put an array here, example :
 var tilesUrlArray = [ "http://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
 
-/*if (webhost.match("virtual-loup-de-mer.org"))
-{
-    // virtual-loup-de-mer.org
-    bingApiKey = "AkBdZQ5s2ay67yXs9xruMVOPDAikz_CycgDXLoFwJGrfi2rjsGNq8ijo4DddYF7t";
-}
 
-if (webhost.match("v-l-m.org") || webhost.match("vlm-dev"))
-{
-    // v-l-m.org
-    bingApiKey = "AmcAzdaZ8u4cWpQo9qPF7_U4q-vW_u3bsprZ4rqu1kJppx_porBOvOELFXWZiHLE";
-}*/
 
