76493a2d8dddabb249f826e535fd0a1dd61b25d5
sbsrouteur <sbsrouteur@free.fr>
2016-06-26 16:46:57 +0200

    Fix Green Marker in dashord to point to current VMG angle. Remove black marker since not used

--- jvlm/windangle.php
+++ jvlm/windangle.php
@@ -140,7 +140,7 @@ $wheading=get_cgi_var('wheading');
 $wspeed=get_cgi_var('wspeed');
 $roadtoend=get_cgi_var('roadtoend');
 $boattype=get_cgi_var('boattype');
-/* $vmg=quote_smart($_REQUEST['vmg']); */
+//$vmg=quote_smart($_REQUEST['vmg']);
 
 
 header("Cache-Control: no-store, no-cache, must-revalidate");
@@ -174,12 +174,12 @@ drawWindPolar($im, $color, $maxcolor, $boattype, $wspeed, 1, 0);
 $windcolor = windspeedtocolorbeaufort($wspeed, $im);
 drawWindVector($im, $windcolor, 50, geographic2drawingforwind(180), 5);
 
-$color = imagecolorallocate($im, 0, 0, 0);
-drawWindVector($im, $color, 15,  720 + $roadtoend - $wheading, 3);
+//$color = imagecolorallocate($im, 0, 0, 0);
+//drawWindVector($im, $color, 15,  720 + $roadtoend - $wheading, 3);
 
  
 $color = imagecolorallocate($im, 0, 255, 0);
-drawWindVector($im, $color, 15, geographic2drawingforwind($vmg - $wheading)-180, 2);
+drawWindVector($im, $color, 15, geographic2drawingforwind($roadtoend - $wheading)+180, 3);
 
 
 imagepng ($im);
