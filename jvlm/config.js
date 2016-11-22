b60413a549bfe949fccd72e5ccc03ade13fec0ff
ChrisCatta <madachri@campcatta.com>
2016-11-22 07:11:47 +0300

    Remise config js

--- jvlm/config.js
+++ jvlm/config.js
@@ -7,7 +7,7 @@ webhost = window.location.hostname;
 //You may put an array here, example :
 var tilesUrlArray = [ "http://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
 
-if (webhost == 'vlm-d1v')
+if (webhost == 'vlm-dev')
 {
     tilesUrlArray='/cache/gshhstiles/${z}/${x}/${y}.png'
 }
