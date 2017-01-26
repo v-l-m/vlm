67df6a5d86854cf4c299412b62f54f6cb427ce5f
sbsrouteur <sbsrouteur@free.fr>
2017-01-26 21:53:21 +0100

    fix indent

--- jvlm/position.js
+++ jvlm/position.js
@@ -13,172 +13,179 @@ const VLM_DIST_ORTHO = 1;
 
 function Deg2Rad(v)
 {
-    return v/180.0*Math.PI;
+  return v/180.0*Math.PI;
 }
 
 function Rad2Deg(v)
 {
-    return v/Math.PI*180.0;
+  return v/Math.PI*180.0;
 }
 
 function RoundPow(v,P)
 {
-    if(P)
-    {
-        var Div = Math.pow(10,P);
-        return Math.round(v*Div)/Div;
-    }
-    else
-    {
-        return v
-    }
+  if(P)
+  {
+    var Div = Math.pow(10,P);
+    return Math.round(v*Div)/Div;
+  }
+  else
+  {
+    return v
+  }
 }
 
 
 // Constructor
 function VLMPosition(lon, lat,  format)
 {   
-    if (typeof format == 'undefined' || format == POS_FORMAT_DEFAULT)
+  if (typeof format == 'undefined' || format == POS_FORMAT_DEFAULT)
+  {
+    // Default constructor, lon and lat in degs flaoting format
+    this.Lon=new Coords(lon,1);
+    this.Lat=new Coords(lat,0);
+  }
+
+  // Default string formating
+  this.ToString=function()
+  {
+    return this.Lat.ToString() + " " + this.Lon.ToString();
+  }
+
+  // function GetLoxoDist
+  // Returns the loxodromic distance to another point
+  this.GetLoxoDist= function(P,Precision)
+  {
+
+    var Lat1  = Deg2Rad(this.Lat.Value);
+    var Lat2  = Deg2Rad(P.Lat.Value);
+    var Lon1  = -Deg2Rad(this.Lon.Value);
+    var Lon2  = -Deg2Rad(P.Lon.Value);
+
+
+    var TOL  = 0.000000000000001;
+    var d =0;
+    var q=0;
+    if (Math.abs(Lat2 - Lat1) < Math.sqrt(TOL)) 
     {
-        // Default constructor, lon and lat in degs flaoting format
-        this.Lon=new Coords(lon,1);
-        this.Lat=new Coords(lat,0);
+      q = Math.cos(Lat1);
+    }
+    else
+    {
+      q = (Lat2 - Lat1) / Math.log(Math.tan(Lat2 / 2 + Math.PI / 4) / Math.tan(Lat1 / 2 +Math.PI / 4));
     }
 
-    // Default string formating
-    this.ToString=function()
+    d= Math.sqrt(Math.pow(Lat2 - Lat1, 2) + q * q * (Lon2 - Lon1) * (Lon2 - Lon1) );
+    var RetVal = EARTH_RADIUS *d;
+    
+    
+    return  RoundPow(RetVal,Precision);
+  }
+
+  // Reaches a point from position using rhumbline.
+  // Compute the position of point at r * distance to point P is 1st param is a Position
+  // Computes the position at Distance P, and heading r if P is a number
+  // Along loxodrome from this to P
+  this.ReachDistLoxo = function(P, r)
+  {
+    var d = 0;
+    var tc= 0;
+
+    if (isNaN(r))
     {
-        return this.Lat.ToString() + " " + this.Lon.ToString();
+      throw "unsupported reaching NaN distance"
+    }    
+
+    if (typeof P == "number")
+    {
+      d=P/EARTH_RADIUS;
+      tc=Deg2Rad(r % 360);
+    }
+    else
+    {
+      d=this.GetLoxoDist(P)/EARTH_RADIUS*r;
+      tc  = Deg2Rad(this.GetLoxoCourse(P));
+    }
+      
+    var Lat1  = Deg2Rad(this.Lat.Value);
+    var Lon1  = -Deg2Rad(this.Lon.Value);
+    var Lat =0; 
+    var Lon =0;
+    var TOL  = 0.000000000000001;
+    var q =0;
+    var dPhi =0;
+    var dlon =0;
+
+    Lat = Lat1 + d * Math.cos(tc);
+    if (Math.abs(Lat) > Math.PI / 2) 
+    {
+      //'"d too large. You can't go this far along this rhumb line!"
+      throw "Invalid distance, can't go that far";
     }
 
-    // function GetLoxoDist
-    // Returns the loxodromic distance to another point
-    this.GetLoxoDist= function(P,Precision)
+    if (Math.abs(Lat - Lat1) < Math.sqrt(TOL))
     {
+        q = Math.cos(Lat1);
+    }
+    else
+    {
+      dPhi = Math.log(Math.tan(Lat / 2 + Math.PI / 4) / Math.tan(Lat1 / 2 +Math.PI / 4));
+      q = (Lat - Lat1) / dPhi;
+    }
+    dlon = -d * Math.sin(tc) / q;
+    Lon = -(((Lon1 + dlon +Math.PI) % (2 *Math.PI) - Math.PI));
 
-        var Lat1  = Deg2Rad(this.Lat.Value);
-        var Lat2  = Deg2Rad(P.Lat.Value);
-        var Lon1  = -Deg2Rad(this.Lon.Value);
-        var Lon2  = -Deg2Rad(P.Lon.Value);
-
-
-        var TOL  = 0.000000000000001;
-        var d =0;
-        var q=0;
-        if (Math.abs(Lat2 - Lat1) < Math.sqrt(TOL)) 
-        {
-            q = Math.cos(Lat1);
-        }
-        else
-        {
-        	 q = (Lat2 - Lat1) / Math.log(Math.tan(Lat2 / 2 + Math.PI / 4) / Math.tan(Lat1 / 2 +Math.PI / 4));
-        }
-
-        d= Math.sqrt(Math.pow(Lat2 - Lat1, 2) + q * q * (Lon2 - Lon1) * (Lon2 - Lon1) );
-        var RetVal = EARTH_RADIUS *d;
-        
-        
-        return  RoundPow(RetVal,Precision);
+    if (isNaN(Lon) || isNaN(Lat))
+    {
+        throw "Reached Nan Position!!!"
     }
 
-    // Reaches a point from position using rhumbline.
-    // Compute the position of point at r * distance to point P is 1st param is a Position
-    // Computes the position at Distance P, and heading r if P is a number
-    // Along loxodrome from this to P
-    this.ReachDistLoxo = function(P, r)
+    return new VLMPosition(Rad2Deg(Lon),Rad2Deg(Lat));
+
+
+  };
+
+  //
+  // Return loxodromic course from this to P in °
+  //
+  this.GetLoxoCourse = function(P,Precision)
+  {
+    var Lon1  = -Deg2Rad(this.Lon.Value);
+    var Lon2  = -Deg2Rad(P.Lon.Value);
+    var Lat1  = Deg2Rad(this.Lat.Value);
+    var Lat2  = Deg2Rad(P.Lat.Value);
+
+    if (typeof Precision == "undefined" || typeof Precision != "number")
     {
-        var d = 0;
-        var tc= 0;
+      Precision = 17
+    }
 
-       if (typeof P == "number")
-        {
-            d=P/EARTH_RADIUS;
-            tc=Deg2Rad(r % 360);
-        }
-        else
-        {
-            d=this.GetLoxoDist(P)/EARTH_RADIUS*r;
-            tc  = Deg2Rad(this.GetLoxoCourse(P));
-        }
-        
-        var Lat1  = Deg2Rad(this.Lat.Value);
-        var Lon1  = -Deg2Rad(this.Lon.Value);
-        var Lat =0; 
-        var Lon =0;
-        var TOL  = 0.000000000000001;
-        var q =0;
-        var dPhi =0;
-        var dlon =0;
-
-        Lat = Lat1 + d * Math.cos(tc);
-        if (Math.abs(Lat) > Math.PI / 2) 
-        {
-            //'"d too large. You can't go this far along this rhumb line!"
-            throw "Invalid distance, can't go that far";
-        }
-
-        if (Math.abs(Lat - Lat1) < Math.sqrt(TOL))
-        {
-            q = Math.cos(Lat1);
-        }
-        else
-        {
-            dPhi = Math.log(Math.tan(Lat / 2 + Math.PI / 4) / Math.tan(Lat1 / 2 +Math.PI / 4));
-            q = (Lat - Lat1) / dPhi;
-        }
-        dlon = -d * Math.sin(tc) / q;
-        Lon = -(((Lon1 + dlon +Math.PI) % (2 *Math.PI) - Math.PI));
-
-        return new VLMPosition(Rad2Deg(Lon),Rad2Deg(Lat));
-
-
-    };
-
-    //
-    // Return loxodromic course from this to P in °
-    //
-    this.GetLoxoCourse = function(P,Precision)
+    /*if (Lon1 > 0)
     {
-        var Lon1  = -Deg2Rad(this.Lon.Value);
-        var Lon2  = -Deg2Rad(P.Lon.Value);
-        var Lat1  = Deg2Rad(this.Lat.Value);
-        var Lat2  = Deg2Rad(P.Lat.Value);
+        Lon2 += 2 * Math.PI
+    }
+    else
+    {   
+        Lon2 -= 2 * Math.PI
+    }*/;
+    var dlon_w  = (Lon2 - Lon1) % (2 * Math.PI);
+    var dlon_e  = (Lon1 - Lon2) % (2 * Math.PI);
+    var dphi  = Math.log(Math.tan(Lat2 / 2 + Math.PI / 4) / Math.tan(Lat1 / 2 + Math.PI / 4));
+    var tc ;
 
-        if (typeof Precision == "undefined" || typeof Precision != "number")
-        {
-            Precision = 17
-        }
+      
+    if (dlon_w < dlon_e) 
+    { // Westerly rhumb line is the shortest
+      tc = Math.atan2(dlon_w, dphi) % (2 * Math.PI); 
+    }
+    else
+    {
+      tc = Math.atan2(-dlon_e, dphi) % (2 * Math.PI);
+    }
 
-        /*if (Lon1 > 0)
-        {
-            Lon2 += 2 * Math.PI
-        }
-        else
-        {   
-            Lon2 -= 2 * Math.PI
-        }*/;
-        var dlon_w  = (Lon2 - Lon1) % (2 * Math.PI);
-        var dlon_e  = (Lon1 - Lon2) % (2 * Math.PI);
-        var dphi  = Math.log(Math.tan(Lat2 / 2 + Math.PI / 4) / Math.tan(Lat1 / 2 + Math.PI / 4));
-        var tc ;
+    var ret  = (720 - (tc / Math.PI * 180)) % 360;
 
-        
-        if (dlon_w < dlon_e) 
-        { // Westerly rhumb line is the shortest
-            tc = Math.atan2(dlon_w, dphi) % (2 * Math.PI);
-            
-        }
-        else
-        {
-            tc = Math.atan2(-dlon_e, dphi) % (2 * Math.PI);
-           
-
-        }
-
-        var ret  = (720 - (tc / Math.PI * 180)) % 360;
-
-        return RoundPow( ret,Precision);
-    };
+    return RoundPow( ret,Precision);
+  };
 
 if (VLM_DIST_ORTHO)
 {
@@ -224,14 +231,25 @@ if (VLM_DIST_ORTHO)
       tc1=2*pi-acos((sin(lat2)-sin(lat1)*cos(d))/(sin(d)*cos(lat1)))    
     ENDIF*/
     var d = Deg2Rad(this.GetOrthoDist(P)/60);
-    var retval
-    if (Math.sin(lon2-lon1)<0)       
+    var retval = (Math.sin(lat2)-Math.sin(lat1)*Math.cos(d))/(Math.sin(d)*Math.cos(lat1))
+    if ((retval >= -1)&&(retval<=1))
     {
-      retval=Math.acos((Math.sin(lat2)-Math.sin(lat1)*Math.cos(d))/(Math.sin(d)*Math.cos(lat1)))    
+      if (Math.sin(lon2-lon1)<0)       
+      {
+        retval=Math.acos(retval)    
+      }
+      else       
+      {
+        retval=2*Math.PI-Math.acos(retval)    
+      }
     }
-    else       
+    else if (lat1 < lat2)
     {
-      retval=2*Math.PI-Math.acos((Math.sin(lat2)-Math.sin(lat1)*Math.cos(d))/(Math.sin(d)*Math.cos(lat1)))    
+      retval = 0;
+    }
+    else
+    {
+      retval = Math.PI;
     }
     
     retval = Rad2Deg( retval % (2 * Math.PI));
@@ -285,26 +303,26 @@ else
     return RoundPow( retval,Precision);
   }
 }
-    this.ReachDistOrtho=function(dist,bearing)
-    {
-        var lat;
-        var dlon;
-        var d=dist/EARTH_RADIUS;
-        var tc = Deg2Rad(bearing);
-        var CurLat = Deg2Rad(this.Lat.Value);
-        var CurLon = Deg2Rad(-this.Lon.Value);
+  this.ReachDistOrtho=function(dist,bearing)
+  {
+    var lat;
+    var dlon;
+    var d=dist/EARTH_RADIUS;
+    var tc = Deg2Rad(bearing);
+    var CurLat = Deg2Rad(this.Lat.Value);
+    var CurLon = Deg2Rad(-this.Lon.Value);
 
-        lat =Math.asin(Math.sin(CurLat)*Math.cos(d)+Math.cos(CurLat)*Math.sin(d)*Math.cos(tc))
-        dlon=Math.atan2(Math.sin(tc)*Math.sin(d)*Math.cos(CurLat),Math.cos(d)-Math.sin(CurLat)*Math.sin(lat))
-        lon=(( CurLon-dlon +Math.PI)%(2*Math.PI ))-Math.PI;
-        return new VLMPosition(Rad2Deg(-lon), Rad2Deg(lat));
+    lat =Math.asin(Math.sin(CurLat)*Math.cos(d)+Math.cos(CurLat)*Math.sin(d)*Math.cos(tc))
+    dlon=Math.atan2(Math.sin(tc)*Math.sin(d)*Math.cos(CurLat),Math.cos(d)-Math.sin(CurLat)*Math.sin(lat))
+    lon=(( CurLon-dlon +Math.PI)%(2*Math.PI ))-Math.PI;
+    return new VLMPosition(Rad2Deg(-lon), Rad2Deg(lat));
 
-    }
+  }
 
-    this.GetVLMString=function()
-    {
-        return lat.ToString() +','+lon.ToString();
-    }
+  this.GetVLMString=function()
+  {
+    return lat.ToString() +','+lon.ToString();
+  }
 };
 
 
