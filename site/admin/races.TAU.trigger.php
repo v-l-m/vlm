702a4a30618ba4c547b3f4c528d3b946e6d75129
sbsrouteur <sbsrouteur@free.fr>
2017-04-22 11:20:02 +0200

    Fix #843. Add update trigger to replace boat on not started races.

--- site/admin/races.TAU.trigger.php
+++ site/admin/races.TAU.trigger.php
@@ -4,5 +4,10 @@
         return False;
     }
     $res = $this->myQuery("UPDATE races SET updated = NOW() WHERE idraces = '".$_REQUEST['PME_data_idraces']."';");
+
+    // if races is not started, then update all users which position is not WP0
+    $res = $this->myQuery("select idraces, startlong, startlat from races where started=0 and idraces='".$this->rec."';");
+    echo "getting changed raceid (if any)".$this->rec."-".$_REQUEST['PME_data_idraces']."\n";
+    echo $res;
     return True;
 ?>
