77d7842166b23f1fa7ad0feaf7c7c3c998af8011
sbsrouteur <sbsrouteur@free.fr>
2017-05-27 16:03:54 +0200

    Fix alert autoclose timer issue
    reduce autoclose to 5s

--- jvlm/GUI.js
+++ jvlm/GUI.js
@@ -1868,10 +1868,15 @@ function VLMAlertInfo(Text)
   VLMAlert(Text,"alert-Info");
 }
 
+let AlertIntervalId = null;
+
 function VLMAlert(Text,Style)
 {
-  // Force closing the previous alert if any
-  AutoCloseVLMAlert();
+  if (AlertIntervalId)
+  {
+    clearInterval(AlertIntervalId);
+  } 
+
   if (typeof Style === "undefined" || !Style)
   {
     Style="alert-info";
@@ -1887,8 +1892,12 @@ function VLMAlert(Text,Style)
   $("#AlertBox").addClass(Style);
   $("#AlertBox").show();
   $("#AlertCloseBox").unbind().on('click',AutoCloseVLMAlert)
-  
-  setTimeout(AutoCloseVLMAlert,15000); 
+
+  if (AlertIntervalId)
+  {
+    clearInterval(AlertIntervalId);
+  }  
+  AlertIntervalId = setTimeout(AutoCloseVLMAlert,5000); 
 }
 
 function AutoCloseVLMAlert()
