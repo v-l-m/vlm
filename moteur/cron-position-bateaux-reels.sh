7e9fbdffebef7101a3285c8a8375291faeae7764
sbsrouteur <sbsrouteur@free.fr>
2017-06-25 16:06:13 +0200

    Add script for the bridge

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -2,14 +2,6 @@
 
 source $VLMRACINE/conf/conf_script || exit 1
 
-#VDG 2016
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_vdg_2016.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+#The Bridge 2017
+nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_bridge_2017.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
-#KJV 2016
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_Joyon_JV2016.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-
-#coville
-#$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_coville_2016.php > $VLMTEMP/coville.json
-#python $VLMRACINE/vlmcode/moteur/posscripts/getpos_coville_2016.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
-#New-York Vendée 2016
-#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_NewYorkVendee.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
