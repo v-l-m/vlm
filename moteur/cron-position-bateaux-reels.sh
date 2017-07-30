deacb56e5dc6198befeeee00db24eca59d96b97b
sbsrouteur <sbsrouteur@free.fr>
2017-07-30 17:02:21 +0200

    Disable all real trackers

--- moteur/cron-position-bateaux-reels.sh
+++ moteur/cron-position-bateaux-reels.sh
@@ -3,5 +3,5 @@
 source $VLMRACINE/conf/conf_script || exit 1
 
 #The Bridge 2017
-nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_bridge_2017.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
+#nice -1 python $VLMRACINE/vlmcode/moteur/posscripts/getpos_bridge_2017.py|$VLMPHPPATH $VLMRACINE/vlmcode/moteur/posscripts/getpos_py_wrapper.php
 
