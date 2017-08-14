13222ffaead1f5e6f870f0e887e5f56cb6488a4d
sbsrouteur <sbsrouteur@free.fr>
2017-08-14 09:16:26 +0200

    Add directive to avoid buffering

--- site/admin/ranking_2.php
+++ site/admin/ranking_2.php
@@ -2,8 +2,23 @@
   include_once("config.php");
   include_once("wslib.php");
 
+  // Turn off output buffering
+  ini_set('output_buffering', 'off');
+  // Turn off PHP output compression
+  ini_set('zlib.output_compression', false);
+          
+  //Flush (send) the output buffer and turn off output buffering
+  //ob_end_flush();
+  while (@ob_end_flush());
+          
+  // Implicitly flush the buffer(s)
+  ini_set('implicit_flush', true);
+  ob_implicit_flush(true);
+  
+  //prevent apache from buffering it for deflate/gzip
   header("content-type: text/plain; charset=UTF-8");
-
+  header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+  
   //FIXME : types are badly checked
   $ws = new WSBasePlayer();
 
