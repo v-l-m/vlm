5bb2ae83a98d1c55943e5154285dc318b8bf2a89
sbsrouteur <sbsrouteur@free.fr>
2017-10-01 17:21:20 +0200

    fix cache URL for https

--- jvlm/config.js
+++ jvlm/config.js
@@ -5,7 +5,7 @@ webhost = window.location.hostname;
 //Url for the cache
 //tilesUrlArray = "/cache/gshhstiles/${z}/${x}/${y}.png";
 //You may put an array here, example :
-var tilesUrlArray = [ "http://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "http://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
+var tilesUrlArray = [ "https://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
 
 if (webhost == 'vlm-dev')
 {
