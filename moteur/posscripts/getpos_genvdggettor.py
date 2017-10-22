4a47e532ec0d9a2d47ad956a0fcd30b4cde776a1
Unknown <sbsrouteur@free.fr>
2017-10-22 19:07:43 +0200

    Add script for VOR Leg1

---
+++ moteur/posscripts/getpos_genvdggettor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import sys, zipfile, os
+import time
+#import xml.etree.ElementTree as ElementTree
+import json
+#import requests
+import getposlib as gp
+import sys, time
+
+trackfile = 'pos'
+vlmtmp = gp.vlm_get_tmp()
+
+gp.geturl("http://testing.v-l-m.org/jvlm/pos0",trackfile)
+with open(os.path.join(vlmtmp,trackfile+".static.tmp.xml")) as data_file:    
+  print data_file.read()
+  data_file.close()
