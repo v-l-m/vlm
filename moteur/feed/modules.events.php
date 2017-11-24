93830a52bf8f3640db4cda65f0d71e279ef04c0d
sbsrouteur <sbsrouteur@free.fr>
2017-11-24 23:08:35 +0100

    retab 2 spaces.

--- moteur/feed/modules.events.php
+++ moteur/feed/modules.events.php
@@ -9,34 +9,38 @@
   `revid` int(11) DEFAULT NULL,
 */
 
-    class NewModulesEvents extends baseClass {
-        var $timedelta = 60;
-        var $media = 'irc';
+  class NewModulesEvents extends baseClass 
+  {
+    var $timedelta = 60;
+    var $media = 'irc';
 
-        function __construct($media, $timedelta = 60) {
-            $this->timedelta = intval($timedelta);
-            $this->media = $media;
-        }
-
-        function feed() {
-            $query = "SELECT * FROM modules_status ".
-                     "WHERE ABS(UNIX_TIMESTAMP(updated) - UNIX_TIMESTAMP(NOW()) ) < ".$this->timedelta." ;";
-
-            $res = $this->queryRead($query);
-
-            while ($line = mysql_fetch_assoc($res)) {
-                $this->feedone($line);
-            }
-        }
-
-        function feedone($line) {
-            $t = sprintf("Deploiement du module VLM %s (r%s) sur %s", $line['moduleid'], $line['revid'], $line['serverid']);
-            $sql = sprintf("INSERT IGNORE INTO `news` SET media='%s', summary='%s', timetarget=UNIX_TIMESTAMP(NOW()) ;", $this->media, $t);
-            $this->queryWrite($sql);
-            print $t."\n";
-        }
-
+    function __construct($media, $timedelta = 60) 
+    {
+      $this->timedelta = intval($timedelta);
+      $this->media = $media;
     }
+
+    function feed() 
+    {
+      $query = "SELECT * FROM modules_status ".
+                "WHERE ABS(UNIX_TIMESTAMP(updated) - UNIX_TIMESTAMP(NOW()) ) < ".$this->timedelta." ;";
+
+      $res = $this->queryRead($query);
+
+      while ($line = mysql_fetch_assoc($res)) 
+      {
+        $this->feedone($line);
+      }
+    }
+
+    function feedone($line) {
+        $t = sprintf("Deploiement du module VLM %s (r%s) sur %s", $line['moduleid'], $line['revid'], $line['serverid']);
+        $sql = sprintf("INSERT IGNORE INTO `news` SET media='%s', summary='%s', timetarget=UNIX_TIMESTAMP(NOW()) ;", $this->media, $t);
+        $this->queryWrite($sql);
+        print $t."\n";
+    }
+
+  }
 
     if ($_SERVER['argc'] != 3) die("Bad arguments");
     eval("\$media = \"".$_SERVER['argv'][1]."\";");
