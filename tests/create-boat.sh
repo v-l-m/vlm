a211d5fb86aa5bb6d1e56db6c97e99af8df70eda
sbsrouteur <sbsrouteur@free.fr>
2018-04-01 22:08:33 +0200

    Fix remaining old domain name.

--- tests/create-boat.sh
+++ tests/create-boat.sh
@@ -10,25 +10,25 @@ PASSWORD=`echo stress$i`
 echo "Caring for $PSEUDO"
  
 # create boat 
-#curl -d myboat="myboat" -d pseudo="$PSEUDO" -d password="$PASSWORD" -d type="create" -d lang="en" -d submit="submit" http://testing.virtual-loup-de-mer.org/myboat.php  > foo
+#curl -d myboat="myboat" -d pseudo="$PSEUDO" -d password="$PASSWORD" -d type="create" -d lang="en" -d submit="submit" http://testing.v-l-m.org/myboat.php  > foo
 
 # login
-curl -c vlm-cookiejar.ck -d pseudo="$PSEUDO" -d password="$PASSWORD" -d type="login" -d lang="en" http://testing.virtual-loup-de-mer.org/myboat.php  > foo
+curl -c vlm-cookiejar.ck -d pseudo="$PSEUDO" -d password="$PASSWORD" -d type="login" -d lang="en" http://testing.v-l-m.org/myboat.php  > foo
 
 idu=`grep idu foo | grep value | head -1 | sed 's/.*value=\"\([0-9]*\).*/\1/'`
 echo "IDU is $idu"
 
-#curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d type="unsubscribe" -d idusers="$idu" -d lang="en" http://testing.virtual-loup-de-mer.org/subscribe.php > foo
+#curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d type="unsubscribe" -d idusers="$idu" -d lang="en" http://testing.v-l-m.org/subscribe.php > foo
 
 # subscribe to the mamba race
-#curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d idraces="20081101" -d idusers="" -d type="subscribe" -d lang="en" -d submit="Subscribe to this race" http://testing.virtual-loup-de-mer.org/myboat.php > foo
+#curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d idraces="20081101" -d idusers="" -d type="subscribe" -d lang="en" -d submit="Subscribe to this race" http://testing.v-l-m.org/myboat.php > foo
 
 # adjust WP
-curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d targetlat="43" -d targetlong="5" -d targetandhdg="118.5" -d type="savemywp" http://testing.virtual-loup-de-mer.org/myboat.php > foo
+curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d targetlat="43" -d targetlong="5" -d targetandhdg="118.5" -d type="savemywp" http://testing.v-l-m.org/myboat.php > foo
 
 # set ortho mode
-#curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d idusers="$idu" -d lang="en" -d pilotmode="orthodromic" http://testing.virtual-loup-de-mer.org/update_angle.php > foo
-curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d idusers="$idu" -d lang="en" -d pilotmode="bestvmg" http://testing.virtual-loup-de-mer.org/update_angle.php > foo
+#curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d idusers="$idu" -d lang="en" -d pilotmode="orthodromic" http://testing.v-l-m.org/update_angle.php > foo
+curl -b vlm-cookiejar.ck -c vlm-cookiejar.ck -d idusers="$idu" -d lang="en" -d pilotmode="bestvmg" http://testing.v-l-m.org/update_angle.php > foo
 
 rm -f foo
 done
