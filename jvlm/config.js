462f8d1c8e296d006b01c3552d2738b0976d33f7
sbsrouteur <sbsrouteur@free.fr>
2018-05-10 23:07:19 +0200

    Beautify and jshint

--- jvlm/config.js
+++ jvlm/config.js
@@ -5,23 +5,22 @@ webhost = window.location.hostname;
 //Url for the cache
 //tilesUrlArray = "/cache/gshhstiles/${z}/${x}/${y}.png";
 //You may put an array here, example :
-var tilesUrlArray = [ "https://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png" ];
+var tilesUrlArray = ["https://c1.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c2.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c3.v-l-m.org/gshhstiles/${z}/${x}/${y}.png", "https://c4.v-l-m.org/gshhstiles/${z}/${x}/${y}.png"];
 
 // URI to distribute windgridrequest amount servers
-var WindGridServers = ["https://c1.v-l-m.org","https://c2.v-l-m.org","https://c3.v-l-m.org","https://c4.v-l-m.org"]
+var WindGridServers = ["https://c1.v-l-m.org", "https://c2.v-l-m.org", "https://c3.v-l-m.org", "https://c4.v-l-m.org"];
 
 if (webhost == 'vlm-dev.ddns.net')
 {
-    tilesUrlArray='/cache/gshhstiles/${z}/${x}/${y}.png'
+  tilesUrlArray = '/cache/gshhstiles/${z}/${x}/${y}.png';
 
-    var WindGridServers = ['https://vlm-dev.ddns.net','https://vlm-dev.ddns.net','https://vlm-dev.ddns.net','https://vlm-dev.ddns.net'];
-    
+  var WindGridServers = ['https://vlm-dev.ddns.net', 'https://vlm-dev.ddns.net', 'https://vlm-dev.ddns.net', 'https://vlm-dev.ddns.net'];
+
 }
 if (webhost == 'vlm-dev')
 {
-    tilesUrlArray='/cache/gshhstiles/${z}/${x}/${y}.png'
+  tilesUrlArray = '/cache/gshhstiles/${z}/${x}/${y}.png';
 
-    var WindGridServers = ['https://vlm-dev','https://vlm-dev','https://vlm-dev','https://vlm-dev'];
-    
+  var WindGridServers = ['https://vlm-dev', 'https://vlm-dev', 'https://vlm-dev', 'https://vlm-dev'];
+
 }
-
