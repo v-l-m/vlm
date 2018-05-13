e5fc08390ea6c0fc49cd2ccd5118812ff88625e3
sbsrouteur <sbsrouteur@free.fr>
2018-05-13 20:04:45 +0200

    Update gulp for uploading after build

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ const gulp = require('gulp'),
   uglifyes = require('uglify-es'),
   composer = require('gulp-uglify/composer'),
   uglify = composer(uglifyes, console),
-  gulpDeployFtp = require('gulp-deploy-ftp');
+  ftp = require('vinyl-ftp'),
+  runsequence = require('run-sequence');
 
 gulp.task('scripts', function()
 {
@@ -38,4 +39,40 @@ gulp.task('scripts', function()
     {
       message: 'Scripts task complete'
     }));
+});
+
+
+
+gulp.task('deploy', function()
+{
+
+  var conn = ftp.create(
+  {
+    host: 'vlm-dev.ddns.net',
+    user: 'vlm',
+    password: 'vlm',
+    parallel: 10,
+    log: gutil.log
+  });
+
+  var globs = [
+    'jvlm/dist/*'
+  ];
+
+  // using base = '.' will transfer everything to /public_html correctly
+  // turn off buffering in gulp.src for best performance
+
+  return gulp.src(globs,
+    {
+      cwd: '/home/vlm/vlmcode',
+      buffer: false
+    })
+    .pipe(conn.newer('jvlm/dist')) // only upload newer files
+    .pipe(conn.dest('jvlm/dist'));
+
+});
+
+gulp.task('default', function()
+{
+  return runsequence('scripts', 'deploy');
 });
