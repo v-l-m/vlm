fb578b7e912b494e16ba3de2d2b88d178c747085
sbsrouteur <sbsrouteur@free.fr>
2018-05-19 11:06:44 +0200

    Update gulp script for libs. Try ignoring JS in dist

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ const gulp = require('gulp'),
   composer = require('gulp-uglify/composer'),
   uglify = composer(uglifyes, console),
   ftp = require('vinyl-ftp'),
+  log= require('fancy-log'),
   runsequence = require('run-sequence');
 
 gulp.task('scripts', function()
@@ -41,6 +42,39 @@ gulp.task('scripts', function()
     }));
 });
 
+gulp.task('libs', function()
+{
+  return gulp.src(['jvlm/external/jquery/jquery-3.2.1.min.js',
+  'jvlm/external/jquery-ui/jquery-ui.js','jvlm/external/bootstrap-master/js/bootstrap.js',
+  'jvlm/external/jquery.csv.js','jvlm/external/bootstrap-colorpicker-master/js/bootstrap-colorpicker.js',
+  'jvlm/external/footable-bootstrap/js/footable.js','jvlm/jquery.ui.touch-punch.js',
+  'jvlm/external/store/store.min.js',
+  'jvlm/external/verimail/verimail.jquery.min.js','jvlm/external/PasswordStrength/jquery.pstrength-min.1.2.js',
+  'jvlm/external/moments/moment.min.js','jvlm/externals/fullcalendar/fullcalendar.min.js',
+  'jvlm/externals/fullcalendar/locale-all.js','jvlm/external/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js'
+])
+    //.pipe(jshint('.jshintrc'))
+    //.pipe(jshint.reporter('default'))
+    .pipe(concat('jvlm_libs.js'))
+    .pipe(gulp.dest('jvlm/dist'))
+    .pipe(rename(
+    {
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .on('error', function(err)
+    {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
+    //.pipe(gulpDeployFtp('./vlmcode', 'vlm-dev.ddns.net', 21, 'vlm', 'vlm'))
+    .pipe(gulp.dest('jvlm/dist'))
+    .pipe(notify(
+    {
+      message: 'Minify Libs task complete'
+    }));
+});
+
+
 
 
 gulp.task('deploy', function()
@@ -51,8 +85,9 @@ gulp.task('deploy', function()
     host: 'vlm-dev.ddns.net',
     user: 'vlm',
     password: 'vlm',
-    parallel: 10,
-    log: gutil.log
+    parallel: 1,
+    reload: true,
+    log: log
   });
 
   var globs = [
@@ -67,8 +102,12 @@ gulp.task('deploy', function()
       cwd: '/home/vlm/vlmcode',
       buffer: false
     })
-    .pipe(conn.newer('jvlm/dist')) // only upload newer files
-    .pipe(conn.dest('jvlm/dist'));
+    .pipe(conn.newerOrDifferentSize('jvlm/dist')) // only upload newer files
+    .pipe(conn.dest('jvlm/dist')
+    .pipe(notify(
+      {
+        message: 'Upload task complete'
+      })));
 
 });
 
