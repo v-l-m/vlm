be79fc77ebd56d950480924a38fe1cb3dc46fb64
Unknown <sbsrouteur@free.fr>
2018-09-27 23:03:09 +0200

    Fix trigger to not use new and old as tables.

--- base/scripts/AddRaceInfoTriggers.sql
+++ base/scripts/AddRaceInfoTriggers.sql
@@ -3,49 +3,49 @@ drop trigger if exists TRG_RACE_WAYPOINT_INSERT;
 drop trigger if exists TRG_RACE_WAYPOINT_DELETE;
 
 create trigger TRG_RACE_WAYPOINT_INSERT after INSERT on races_waypoints 
-  for each row update races set updated = now() where idraces=new.idraces;
+  for each row update races set updated = now() where idraces=NEW.idraces;
 
 create trigger TRG_RACE_WAYPOINT_UPDATE after UPDATE on races_waypoints 
-  for each row update races set updated = now() where idraces=new.idraces;
+  for each row update races set updated = now() where idraces=NEW.idraces;
 
 create trigger TRG_RACE_WAYPOINT_DELETE before DELETE on races_waypoints 
-  for each row update races set updated = now() where idraces=old.idraces;
+  for each row update races set updated = now() where idraces=OLD.idraces;
 
 drop trigger if exists TRG_WAYPOINT_UPDATE;
 drop trigger if exists TRG_WAYPOINT_INSERT;
 drop trigger if exists TRG_WAYPOINT_DELETE;
 
-#create trigger TRG_WAYPOINT_INSERT after INSERT on waypoints 
-#  for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints, new  where new.idwaypoint = races_waypoints.idwaypoint);
+create trigger TRG_WAYPOINT_INSERT after INSERT on waypoints 
+  for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints  where NEW.idwaypoint = races_waypoints.idwaypoint);
 
 create trigger TRG_WAYPOINT_UPDATE after UPDATE on waypoints 
-  for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints, new  where new.idwaypoint = races_waypoints.idwaypoint);
+  for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints  where NEW.idwaypoint = races_waypoints.idwaypoint);
 
 create trigger TRG_WAYPOINT_DELETE before DELETE on waypoints 
-  for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints, old  where old.idwaypoint = races_waypoints.idwaypoint);
+  for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints  where OLD.idwaypoint = races_waypoints.idwaypoint);
 
 drop trigger if exists TRG_SEGMENT_UPDATE;
 drop trigger if exists TRG_SEGMENT_INSERT;
 drop trigger if exists TRG_SEGMENT_DELETE;
 
 create trigger TRG_SEGMENT_INSERT after INSERT on nszsegment 
-  for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment, new  where new.idsegment = nszracesegment.idsegment);
+  for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment  where NEW.idsegment = nszracesegment.idsegment);
 
 create trigger TRG_SEGMENT_UPDATE after UPDATE on nszsegment 
-  for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment, new  where new.idsegment = nszracesegment.idsegment);
+  for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment  where NEW.idsegment = nszracesegment.idsegment);
 
 create trigger TRG_SEGMENT_DELETE before DELETE on nszsegment 
-  for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment, old  where old.idsegment = nszracesegment.idsegment);
+  for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment  where OLD.idsegment = nszracesegment.idsegment);
 
 drop trigger if exists TRG_RACESEGMENT_UPDATE;
 drop trigger if exists TRG_RACESEGMENT_INSERT;
 drop trigger if exists TRG_RACESEGMENT_DELETE;
 
 create trigger TRG_RACESEGMENT_INSERT after INSERT on nszracesegment 
-  for each row update races set updated = now() where idraces=new.idraces;
+  for each row update races set updated = now() where idraces=NEW.idraces;
 
 create trigger TRG_RACESEGMENT_UPDATE after UPDATE on nszracesegment 
-  for each row update races set updated = now() where idraces=new.idraces;
+  for each row update races set updated = now() where idraces=NEW.idraces;
 
 create trigger TRG_RACESEGMENT_DELETE after DELETE on nszracesegment 
-  for each row update races set updated = now() where idraces=old.idraces;
+  for each row update races set updated = now() where idraces=OLD.idraces;
