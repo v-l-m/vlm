c33d5705550923d0f46786e0fa716d8799cc9df3
Unknown <sbsrouteur@free.fr>
2018-09-27 21:24:02 +0200

    Fix delete triggers.

--- base/scripts/AddRaceInfoTriggers.sql
+++ base/scripts/AddRaceInfoTriggers.sql
@@ -8,7 +8,7 @@ create trigger TRG_RACE_WAYPOINT_INSERT after INSERT on races_waypoints
 create trigger TRG_RACE_WAYPOINT_UPDATE after UPDATE on races_waypoints 
   for each row update races set updated = now() where idraces=new.idraces;
 
-create trigger TRG_RACE_WAYPOINT_DELETE after DELETE on races_waypoints 
+create trigger TRG_RACE_WAYPOINT_DELETE before DELETE on races_waypoints 
   for each row update races set updated = now() where idraces=old.idraces;
 
 drop trigger if exists TRG_WAYPOINT_UPDATE;
@@ -21,7 +21,7 @@ create trigger TRG_WAYPOINT_INSERT after INSERT on waypoints
 create trigger TRG_WAYPOINT_UPDATE after UPDATE on waypoints 
   for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints, new  where new.idwaypoint = races_waypoints.idwaypoint);
 
-create trigger TRG_WAYPOINT_DELETE after DELETE on waypoints 
+create trigger TRG_WAYPOINT_DELETE before DELETE on waypoints 
   for each row update races set updated = now() where idraces in (select distinct idraces from races_waypoints, old  where old.idwaypoint = races_waypoints.idwaypoint);
 
 drop trigger if exists TRG_SEGMENT_UPDATE;
@@ -34,7 +34,7 @@ create trigger TRG_SEGMENT_INSERT after INSERT on nszsegment
 create trigger TRG_SEGMENT_UPDATE after UPDATE on nszsegment 
   for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment, new  where new.idsegment = nszracesegment.idsegment);
 
-create trigger TRG_SEGMENT_DELETE after DELETE on nszsegment 
+create trigger TRG_SEGMENT_DELETE before DELETE on nszsegment 
   for each row update races set updated = now() where idraces in (select distinct idraces from nszracesegment, old  where old.idsegment = nszracesegment.idsegment);
 
 drop trigger if exists TRG_RACESEGMENT_UPDATE;
