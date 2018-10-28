9bee2e74ec10469525d62f900f725368a16fec41
Unknown <sbsrouteur@free.fr>
2018-10-28 14:07:52 +0100

    Revert "Add Babel compatibility from IE7"
    This reverts commit 6ec3388bef05eb9bcc39c912315a2d117d831df8.

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,8 @@ const gulp = require('gulp'),
   gulpif = require('gulp-if'),
   babel = require('gulp-babel');
 
-const VLMVersion = 19.3;
-var BuildTypeProd = false;
+const VLMVersion = 19.2;
+var BuildTypeProd = true;
 
 
 gulp.task('scripts', function()
