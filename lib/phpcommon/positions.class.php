0d7ca3a4dba45e0d4c00c1e6c2c71c07cbfa5160
sbs <sbsrouteur@free.fr>
2019-06-23 17:08:31 +0200

    Rel #1044 Migrate MySql API to Mysqli
    Fix search&replace typo. Fix one more API call

--- lib/phpcommon/positions.class.php
+++ lib/phpcommon/positions.class.php
@@ -40,7 +40,7 @@ class positions
     if (!$result) {
       return;
     }
-    $row = mysqlifetch_array($result, MYSQL_ASSOC);
+    $row =  mysqli_fetch_array($result, MYSQL_ASSOC);
     if (!$row) {
       return;
     }
@@ -58,11 +58,11 @@ class positions
     if (!$result) {
       return;
     }
-    $row = mysqlifetch_array($result, MYSQL_NUM);//we are not taking the last one
+    $row =  mysqli_fetch_array($result, MYSQL_NUM);//we are not taking the last one
     if (!$row) {
       return;
     }
-    $row = mysqlifetch_array($result, MYSQL_ASSOC);
+    $row =  mysqli_fetch_array($result, MYSQL_ASSOC);
     if (!$row) {
       return;
     }
@@ -81,7 +81,7 @@ class positions
     //echo "REQUEST: $query \n";
 
     $result = wrapper_mysql_db_query_reader($query);
-    $row = mysqlifetch_array($result, MYSQL_ASSOC);
+    $row =  mysqli_fetch_array($result, MYSQL_ASSOC);
 
     return array($row['time'], $row['long'], $row['lat']);
 
@@ -189,7 +189,7 @@ class positionsIterator extends baseClass {
         $result = $this->queryRead($this->getQuery());
         if ($this->error_status) return;
         $this->start();
-        while ($row = mysqlifetch_array($result, MYSQL_NUM) ) $this->onerow($row);
+        while ($row =  mysqli_fetch_array($result, MYSQL_NUM) ) $this->onerow($row);
         $this->end();
     }
 
--- lib/phpcommon/racesgroups.class.php
+++ lib/phpcommon/racesgroups.class.php
@@ -26,7 +26,7 @@ class racesgroups extends baseClass {
         $query= "SELECT * FROM racesgroups WHERE ".$where;
         $result = $this->queryRead($query);
         if ($result && mysqlinum_rows($result) === 1)  {
-            $row = mysqlifetch_array($result, MYSQL_ASSOC);
+            $row =  mysqli_fetch_array($result, MYSQL_ASSOC);
             return $this->constructFromRow($row);
         } else {
             $this->set_error("FAILED : Construct racesgroups object from query");
@@ -56,7 +56,7 @@ class racesgroups extends baseClass {
         if (!is_null($where)) $query = $query." ".$where;
         $result = $this->queryRead($query);
         if ($result) {
-            while($row = mysqlifetch_array($result, MYSQL_ASSOC)) $raceslist[$row['idraces']] = $row;
+            while($row =  mysqli_fetch_array($result, MYSQL_ASSOC)) $raceslist[$row['idraces']] = $row;
         } 
         return $raceslist;
     }
