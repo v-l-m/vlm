efe5a22b7c659b491835eecb3a5833d622bd6be1
sbs <sbsrouteur@free.fr>
2019-10-10 00:22:20 +0200

    Bump version and build for alpha publish v21.3

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ const gulp = require('gulp'),
   gulpif = require('gulp-if'),
   babel = require('gulp-babel');
 
-const VLMVersion = 21.2;
+const VLMVersion = 21.3;
 var BuildTypeProd = false; // This is automatically set to true in prod build chains
 
 
