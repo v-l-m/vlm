229f7a78f03c340ebd31c730bb1f8f5763f08ea1
sbs <sbsrouteur>
2020-03-07 18:38:16 +0100

    Bump version & alpha build

--- jvlm/GUI.js
+++ jvlm/GUI.js
@@ -47,6 +47,26 @@ var RC_PwdResetReq = null;
 var RC_PwdResetConfirm = null;
 var OnPlayerLoadedCallBack = null;
 
+/*// FindIndex for IE
+'use strict';
+
+Array.prototype.findIndex = Array.prototype.findIndex || function(callback) {
+  if (this === null) {
+    throw new TypeError('Array.prototype.findIndex called on null or undefined');
+  } else if (typeof callback !== 'function') {
+    throw new TypeError('callback must be a function');
+  }
+  var list = Object(this);
+  // Makes sures is always has an positive integer as length.
+  var length = list.length >>> 0;
+  var thisArg = arguments[1];
+  for (var i = 0; i < length; i++) {
+    if ( callback.call(thisArg, list[i], i, list) ) {
+      return i;
+    }
+  }
+  return -1;
+};*/
 
 // On ready get started with vlm management
 $(document).ready(
