7d049d9fb5c82ab21e659422b750bdfb8263540b
sbs <sbsrouteur>
2020-05-21 14:03:31 +0200

    Fix estimator color

--- jvlm/estimator.js
+++ jvlm/estimator.js
@@ -635,7 +635,7 @@ function Estimator(Boat)
 
 
 
-    let TrackStartTick = new moment().utc().startOf('hour');
+    let TrackStartTick = new moment.utc().startOf('hour');
     let GribOffset = TrackStartTick.hour() % 6;
     let TrackIndexStartTime = TrackStartTick.add('hour', - GribOffset).add('hour', - 2.5 ).add('minute',5);
     let TrackIndexStartTick = TrackIndexStartTime.unix()*1000;
