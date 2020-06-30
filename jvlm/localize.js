8282e9464773223bb5854bbade55fcf4f8848bf2
sbsrouteur <sbsrouteur@free.fr>
2020-06-30 23:02:21 +0200

    Add polyfill for missing IE function

--- jvlm/localize.js
+++ jvlm/localize.js
@@ -68,6 +68,20 @@
    return true;
  }
 
+ // IE polyfill for includes function
+ // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes#Polyfill
+ if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    } 
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
+
  function InitLocale(Lang)
  {
    var query = "/ws/serverinfo/translation.php";
